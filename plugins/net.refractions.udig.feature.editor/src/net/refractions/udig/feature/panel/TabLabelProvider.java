42a166586a0a757a97df818a9f8e124894a4ae12
jgarnett <jgarnett@e5c1c795-43da-0310-a71f-fac65c449510>
2010-05-05 01:23:15 +0000

    the file files
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31740 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ plugins/net.refractions.udig.feature.editor/src/net/refractions/udig/feature/panel/TabLabelProvider.java
@@ -0,0 +1,37 @@
+package net.refractions.udig.feature.panel;
+
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+
+
+public class TabLabelProvider extends LabelProvider {
+
+    @Override
+    public String getText( Object element ) {
+        if( element instanceof StructuredSelection ){
+            StructuredSelection sel = (StructuredSelection) element;
+            if (sel.isEmpty()){
+                return "Please select a feature";
+            }
+            element = sel.getFirstElement();
+        }
+        if( element instanceof SimpleFeature ){
+            SimpleFeature feature = (SimpleFeature) element;
+            return feature.getID();
+        }
+        if( element instanceof FeaturePanelTabDescriptor ){
+            FeaturePanelTabDescriptor tabDescriptor = (FeaturePanelTabDescriptor) element;
+            String title = tabDescriptor.getEntry().getTitle();
+            if( title == null || title.length() == 0 ){
+                title = tabDescriptor.getLabel();
+            }
+            return title;
+        }
+        else {
+            String text = element.toString();
+            return text;
+        }
+    }
+}
