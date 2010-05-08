d25c9dbfb26e910c0a4069acf74713bb0025196d
mleslie <mleslie@e5c1c795-43da-0310-a71f-fac65c449510>
2010-05-08 09:06:43 +0000

    Adding crs-based estimation of bounds.
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31754 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.catalog.geotools/src/net/refractions/udig/catalog/geotools/data/FeatureSourceGeoResourceInfo.java
+++ plugins/net.refractions.udig.catalog.geotools/src/net/refractions/udig/catalog/geotools/data/FeatureSourceGeoResourceInfo.java
@@ -1,8 +1,7 @@
 package net.refractions.udig.catalog.geotools.data;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 
 import net.refractions.udig.catalog.IGeoResourceInfo;
 import net.refractions.udig.catalog.ui.CatalogUIPlugin;
@@ -10,20 +9,78 @@ import net.refractions.udig.catalog.ui.ISharedImages;
 
 import org.geotools.data.ResourceInfo;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.geometry.Envelope;
+import org.opengis.metadata.extent.BoundingPolygon;
 import org.opengis.metadata.extent.Extent;
+import org.opengis.metadata.extent.GeographicBoundingBox;
 import org.opengis.metadata.extent.GeographicExtent;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class FeatureSourceGeoResourceInfo extends IGeoResourceInfo {
 
-    private ResourceInfo info;
+	private ResourceInfo info;
+
+
+	private ReferencedEnvelope getCRSBounds() {
+		CoordinateReferenceSystem crs = info.getCRS();
+		if(crs == null)
+			return new ReferencedEnvelope();
+		Extent extent = crs.getDomainOfValidity();
+		Collection<? extends GeographicExtent> elem = extent.getGeographicElements();
+		double xmin = Double.MAX_VALUE, ymin = Double.MAX_VALUE;
+		double xmax = Double.MIN_VALUE, ymax = Double.MIN_VALUE;
+		try {
+			for(GeographicExtent ext : elem) {
+				if(ext instanceof BoundingPolygon) {
+					BoundingPolygon bp = (BoundingPolygon)ext;
+					Collection<? extends org.opengis.geometry.Geometry> geoms = bp.getPolygons();
+					for(org.opengis.geometry.Geometry geom : geoms) {
+						Envelope env = geom.getEnvelope();
+						if(env.getMinimum(0) < xmin)
+							xmin = env.getMinimum(0);
+						if(env.getMaximum(0) > xmax)
+							xmax = env.getMaximum(0);
+						if(env.getMinimum(1) < ymin)
+							ymin = env.getMinimum(1);
+						if(env.getMaximum(1) > ymax)
+							ymax = env.getMaximum(1);
+					}
+				} else if(ext instanceof GeographicBoundingBox) {
+					GeographicBoundingBox gbb = (GeographicBoundingBox)ext;
+					ReferencedEnvelope env = new ReferencedEnvelope(DefaultGeographicCRS.WGS84);
+					env.expandToInclude(gbb.getWestBoundLongitude(), gbb.getNorthBoundLatitude());
+					env.expandToInclude(gbb.getEastBoundLongitude(), gbb.getSouthBoundLatitude());
+					env = env.transform(info.getCRS(), true);
+					if(env.getMinX() < xmin)
+						xmin = env.getMinX();
+					if(env.getMaxX() > xmax)
+						xmax = env.getMaxX();
+					if(env.getMinY() < ymin)
+						ymin = env.getMinY();
+					if(env.getMaxY() > ymax)
+						ymax = env.getMaxY();
+				}
+			}
+			if(xmin == Double.MAX_VALUE || 
+					ymin == Double.MAX_VALUE ||
+					xmax == Double.MIN_NORMAL ||
+					ymax == Double.MAX_VALUE) {
+				System.out.println("No sensible extents generated.");
+				return new ReferencedEnvelope(info.getCRS());
+			}
+			return new ReferencedEnvelope(xmin, xmax, ymin, ymax, info.getCRS());
+		} catch(Throwable ex) {
+			ex.printStackTrace(System.out);
+			return new ReferencedEnvelope(info.getCRS());
+		}
+	}
 
     public FeatureSourceGeoResourceInfo( ResourceInfo info ) {
         this.info = info;
         
-        CoordinateReferenceSystem crs = info.getCRS();
-        this.bounds = new ReferencedEnvelope(crs);
-        //this.bounds = info.getBounds();
+        System.out.println("Getting a bounds: " + new GregorianCalendar().getTimeInMillis());
+        this.bounds = getCRSBounds();
         
         this.description = info.getDescription();
         this.keywords = info.getKeywords().toArray(new String[0]);
@@ -50,5 +107,4 @@ public class FeatureSourceGeoResourceInfo extends IGeoResourceInfo {
     public ResourceInfo getInfo() {
         return info;
     }
-    
 }
