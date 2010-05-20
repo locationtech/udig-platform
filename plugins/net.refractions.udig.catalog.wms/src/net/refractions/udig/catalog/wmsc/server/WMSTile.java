2e122033d75fb1e1ef675a59cefea93c5ef12742
Jody Garnett <Jody.Garnett@lisasoft.com>
2010-05-20 13:41:09 +0000

    Include version (another manditory parameter) for UDIG1664
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31825 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.catalog.wms/src/net/refractions/udig/catalog/wmsc/server/WMSTile.java
+++ plugins/net.refractions.udig.catalog.wms/src/net/refractions/udig/catalog/wmsc/server/WMSTile.java
@@ -35,40 +35,38 @@ import org.geotools.ows.ServiceException;
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
- * Represents one tile in a tileset from a WMS Server.  Has a unique id, bounds, scale and
- * a BufferedImage.  All other data such as the format, crs, etc are
- * stored as part of the tileset.
+ * Represents one tile in a tileset from a WMS Server. Has a unique id, bounds, scale and a
+ * BufferedImage. All other data such as the format, crs, etc are stored as part of the tileset.
  * 
  * @author GDavis
  * @since 1.2.0
  */
 public class WMSTile implements Tile {
 
-	private final static boolean testing = false;  // for testing output
-	
+    private final static boolean testing = false; // for testing output
+
     public static final int INERROR = 1;
     public static final int OK = 0;
-    
-    private String id;  // id = "<scale>_<minx>_<maxy>_<maxy>_<miny>"
-    private String position;  // pos = x_y tile position within tilerange grid for this scale
+
+    private String id; // id = "<scale>_<minx>_<maxy>_<maxy>_<miny>"
+    private String position; // pos = x_y tile position within tilerange grid for this scale
     private static String ID_DIVIDER = "_"; //$NON-NLS-1$
     private WMSTileSet tileset;
     private TiledWebMapServer server;
-	private BufferedImage image;
+    private BufferedImage image;
     private Object imageLock = new Object();
     private Envelope bounds;
     private double scale;
-    
+
     /**
-     * The state of the tile; if something happens while fetching the image and the image
-     * isn't returned properly we set the state to error.  If another request is
-     * made for this tile we'll try re-requesting the tile.
+     * The state of the tile; if something happens while fetching the image and the image isn't
+     * returned properly we set the state to error. If another request is made for this tile we'll
+     * try re-requesting the tile.
      */
     private int state = OK;
-    
-    public WMSTile(TiledWebMapServer server, WMSTileSet tileset, 
-    		Envelope bounds, double scale) {
-    	this.server = server;
+
+    public WMSTile( TiledWebMapServer server, WMSTileSet tileset, Envelope bounds, double scale ) {
+        this.server = server;
         this.tileset = tileset;
         this.bounds = bounds;
         this.scale = scale;
@@ -76,175 +74,188 @@ public class WMSTile implements Tile {
     }
 
     /**
-     * Builds a tile id in the form of:
-     * id = "<scale>_<minx>_<maxy>_<maxy>_<miny>"
-     *
+     * Builds a tile id in the form of: id = "<scale>_<minx>_<maxy>_<maxy>_<miny>"
+     * 
      * @param bbox
      * @param scale
      * @return tile id string
      */
-    public static String buildId(Envelope bbox, double scale) {
+    public static String buildId( Envelope bbox, double scale ) {
         if (bbox == null) {
             return null;
         }
-        return scale+ID_DIVIDER+bbox.getMinX()+ID_DIVIDER+bbox.getMaxY()
-                +ID_DIVIDER+bbox.getMaxY()+ID_DIVIDER+bbox.getMinY(); 
+        return scale + ID_DIVIDER + bbox.getMinX() + ID_DIVIDER + bbox.getMaxY() + ID_DIVIDER
+                + bbox.getMaxY() + ID_DIVIDER + bbox.getMinY();
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getId()
-	 */
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getId()
+     */
     public String getId() {
         if (id == null) {
             id = buildId(bounds, scale);
         }
         return id;
     }
-    
+
     public TileSet getTileSet() {
         return tileset;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getBounds()
-	 */
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getBounds()
+     */
     public Envelope getBounds() {
         return bounds;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getScale()
-	 */
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getScale()
+     */
     public double getScale() {
         return scale;
     }
-    
+
     /**
      * Set the buffered image while locking and set the state of the tile.
      * 
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#setBufferedImage(java.awt.image.BufferedImage)
-	 */
-    public void setBufferedImage(BufferedImage im) {
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#setBufferedImage(java.awt.image.BufferedImage)
+     */
+    public void setBufferedImage( BufferedImage im ) {
         Object lock = getTileLock();
-        synchronized (lock) {    	
-        	setBufferedImageInternal(im);
-        	if (getBufferedImage() != null) {
-        		setTileState(WMSTile.OK);
-        	}
-        	else {
-        		setTileState(WMSTile.INERROR);
-        	}
+        synchronized (lock) {
+            setBufferedImageInternal(im);
+            if (getBufferedImage() != null) {
+                setTileState(WMSTile.OK);
+            } else {
+                setTileState(WMSTile.INERROR);
+            }
         }
-    }    
-    
+    }
+
     /**
      * See the buffered image without locking.
      */
-    private void setBufferedImageInternal(BufferedImage im) {
+    private void setBufferedImageInternal( BufferedImage im ) {
         this.image = im;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getBufferedImage()
-	 */
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getBufferedImage()
+     */
     public BufferedImage getBufferedImage() {
         return image;
     }
-    
+
     public Object getTileLock() {
         return imageLock;
     }
 
     public int compareTo( Tile other ) {
         // id contains scale and bounds so compare with that
-        return getId().compareTo( other.getId() );
+        return getId().compareTo(other.getId());
     }
-    
-	public boolean equals(Object arg0) {
-		if (arg0 instanceof Tile) {
-			Tile tile = (Tile) arg0;
-			// id contains scale and bounds so compare with that
-			if (getId().equals(tile.getId())) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		return super.equals(arg0);
-	}
-      
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getTileState()
-	 */
-    public int getTileState(){
+
+    public boolean equals( Object arg0 ) {
+        if (arg0 instanceof Tile) {
+            Tile tile = (Tile) arg0;
+            // id contains scale and bounds so compare with that
+            if (getId().equals(tile.getId())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return super.equals(arg0);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getTileState()
+     */
+    public int getTileState() {
         return this.state;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#setTileState(int)
-	 */
-    public void setTileState(int state){
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#setTileState(int)
+     */
+    public void setTileState( int state ) {
         this.state = state;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#setPosition(java.lang.String)
-	 */
-    public void setPosition(String pos) {
-    	this.position = pos;
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#setPosition(java.lang.String)
+     */
+    public void setPosition( String pos ) {
+        this.position = pos;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#getPosition()
-	 */
+
+    /*
+     * (non-Javadoc)
+     * @see net.refractions.udig.catalog.wmsc.server.Tile#getPosition()
+     */
     public String getPosition() {
-    	return this.position;
+        return this.position;
     }
-    
-    /* (non-Javadoc)
-	 * @see net.refractions.udig.catalog.wmsc.server.Tile#loadTile(org.eclipse.core.runtime.IProgressMonitor)
-	 */
-    public boolean loadTile(IProgressMonitor monitor) {
-    	// build request
-    	if (server == null && tileset != null) {
-    		// try getting server from the tileset
-    		setServer(tileset.getServer());
-    	}
-    	if (server == null || tileset == null) {
-    		WmsPlugin.log("error, tile not setup with a server and/or tileset", null); //$NON-NLS-1$
-    		createErrorImage();
-    		return false;
-    	}
-    	String baseUrl = server.buildBaseTileRequestURL();
-    	Envelope env = getBounds();
-    	URL req = null;
-		try {
-			req = new URL(baseUrl + tileset.createQueryString(env));
-		} catch (MalformedURLException e2) {
-			WmsPlugin.log("error building request URL:", e2); //$NON-NLS-1$
-			return false;
-		}
-        GetMapRequest request = new GetMapRequest(req); 
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * net.refractions.udig.catalog.wmsc.server.Tile#loadTile(org.eclipse.core.runtime.IProgressMonitor
+     * )
+     */
+    public boolean loadTile( IProgressMonitor monitor ) {
+        // build request
+        if (server == null && tileset != null) {
+            // try getting server from the tileset
+            setServer(tileset.getServer());
+        }
+        if (server == null || tileset == null) {
+            WmsPlugin.log("error, tile not setup with a server and/or tileset", null); //$NON-NLS-1$
+            createErrorImage();
+            return false;
+        }
+        String baseUrl = server.buildBaseTileRequestURL();
+        Envelope env = getBounds();
+        URL req = null;
+        try {
+            String version = server.getCapabilities().getVersion();
+            req = new URL(baseUrl + "version="+version+"&"+tileset.createQueryString(env));
+        } catch (MalformedURLException e2) {
+            WmsPlugin.log("error building request URL:", e2); //$NON-NLS-1$
+            return false;
+        } catch (IOException e) {
+            WmsPlugin.log("Error building request URL:", e); //$NON-NLS-1$
+            return false;
+        }
+        GetMapRequest request = new GetMapRequest(req);
 
         // get a lock for this tile and only fetch a new image if
         // it does not have one set already when the lock is obtained.
-        // TODO:  add support for re-fetching expired images too
+        // TODO: add support for re-fetching expired images too
         Object lock = getTileLock();
         BufferedImage bufImage = null;
         if (testing) {
-            System.out.println("getting lock: "+getId()); //$NON-NLS-1$
+            System.out.println("getting lock: " + getId()); //$NON-NLS-1$
         }
         synchronized (lock) {
             if (testing) {
-                System.out.println("got lock: "+getId()); //$NON-NLS-1$
+                System.out.println("got lock: " + getId()); //$NON-NLS-1$
             }
-            if ((getBufferedImage() != null && getTileState() != WMSTile.INERROR) || monitor.isCanceled()) {
+            if ((getBufferedImage() != null && getTileState() != WMSTile.INERROR)
+                    || monitor.isCanceled()) {
                 // tile image already set
                 monitor.setCanceled(true);
                 if (testing) {
-                    System.out.println("REQUEST CANCELLED - REMOVING lock: "+getId()); //$NON-NLS-1$
-                }                    
+                    System.out.println("REQUEST CANCELLED - REMOVING lock: " + getId()); //$NON-NLS-1$
+                }
                 return true;
             }
             InputStream inputStream = null;
@@ -252,28 +263,28 @@ public class WMSTile implements Tile {
                 inputStream = server.issueRequest(request).getInputStream();
                 // simulate latency if testing
                 if (testing) {
-                	Random rand = new Random();	
+                    Random rand = new Random();
                     long delay = rand.nextInt(5000); // delay 1-5 secs
-                    System.out.println("request delaying for: "+delay); //$NON-NLS-1$
-                    Thread.sleep(delay);  // simulate latency
+                    System.out.println("request delaying for: " + delay); //$NON-NLS-1$
+                    Thread.sleep(delay); // simulate latency
                 }
-                WmsPlugin.log("WMSC GetMap: "+ req.toString(), null);  //$NON-NLS-1$
+                WmsPlugin.log("WMSC GetMap: " + req.toString(), null); //$NON-NLS-1$
                 bufImage = ImageIO.read(inputStream);
                 if (bufImage != null) {
                     setBufferedImageInternal(bufImage);
                     setTileState(WMSTile.OK);
-                }else{
-                	// create an error buffered image
+                } else {
+                    // create an error buffered image
                     setBufferedImageInternal(createErrorImage());
                     setTileState(WMSTile.INERROR);
                 }
             } catch (Exception e1) {
-            	// create an error buffered image
+                // create an error buffered image
                 setBufferedImageInternal(createErrorImage());
                 setTileState(WMSTile.INERROR);
                 WmsPlugin.log("error loading tile, placeholder created:", e1); //$NON-NLS-1$
-            } catch( Throwable t){
-            	// create an error buffered image
+            } catch (Throwable t) {
+                // create an error buffered image
                 setBufferedImageInternal(createErrorImage());
                 setTileState(WMSTile.INERROR);
                 WmsPlugin.log("error loading tile, placeholder created:", t); //$NON-NLS-1$
@@ -287,37 +298,38 @@ public class WMSTile implements Tile {
                 }
             }
         } // end synchronized block
-        
+
         if (testing) {
-            System.out.println("REMOVING lock: "+getId()); //$NON-NLS-1$
+            System.out.println("REMOVING lock: " + getId()); //$NON-NLS-1$
         }
-        
+
         // if we successfully set the buffered image to something, return true
         if (getBufferedImage() != null) {
-        	return true;
+            return true;
         }
-        
+
         // if we get here, something prevented us from setting an image
         return false;
-    }    
+    }
 
-	private BufferedImage createErrorImage() {
-	    BufferedImage bf = new BufferedImage(tileset.getWidth(), tileset.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	    Graphics2D g = bf.createGraphics();
-	    g.setColor(Color.RED);
-	    g.drawLine(0, 0, tileset.getWidth(), tileset.getHeight());
-	    g.drawLine(0, tileset.getHeight(), tileset.getWidth(), 0);
-	    return bf;
-	}
-	
+    private BufferedImage createErrorImage() {
+        BufferedImage bf = new BufferedImage(tileset.getWidth(), tileset.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = bf.createGraphics();
+        g.setColor(Color.RED);
+        g.drawLine(0, 0, tileset.getWidth(), tileset.getHeight());
+        g.drawLine(0, tileset.getHeight(), tileset.getWidth(), 0);
+        return bf;
+    }
+
     public TiledWebMapServer getServer() {
-		return server;
-	}
+        return server;
+    }
 
-	public void setServer(TiledWebMapServer server) {
-		this.server = server;
-	}
-	
+    public void setServer( TiledWebMapServer server ) {
+        this.server = server;
+    }
+
     /**
      * Class for sending map requests
      * 
@@ -326,20 +338,21 @@ public class WMSTile implements Tile {
      */
     static class GetMapRequest extends AbstractRequest {
         private URL url;
-        
-        public GetMapRequest(URL url){
+
+        public GetMapRequest( URL url ) {
             super(url, null);
             this.url = url;
         }
         public Response createResponse( String contentType, InputStream inputStream )
                 throws ServiceException, IOException {
-            return new Response(contentType, inputStream){};
+            return new Response(contentType, inputStream){
+            };
         }
 
         public URL getFinalURL() {
             return this.url;
         }
-        
+
         @Override
         protected void initRequest() {
         }
@@ -348,8 +361,6 @@ public class WMSTile implements Tile {
         }
         @Override
         protected void initVersion() {
-        }        
-    }	
+        }
+    }
 }
-
-
