d012798aff825f0ad77541508cb257a18b975cb1
Jody Garnett <Jody.Garnett@lisasoft.com>
2010-05-24 09:12:33 +0000

    Rollback p2 and restore InstallWizardAction; also cleaning up a few more udig_language references
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31826 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.ui/src/net/refractions/udig/InstallWizardAction.java
+++ plugins/net.refractions.udig.ui/src/net/refractions/udig/InstallWizardAction.java
@@ -6,7 +6,7 @@ import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
-//import org.eclipse.update.ui.UpdateManagerUI;
+import org.eclipse.update.ui.UpdateManagerUI;
 
 public class InstallWizardAction implements IWorkbenchWindowActionDelegate {
 
@@ -27,7 +27,7 @@ public class InstallWizardAction implements IWorkbenchWindowActionDelegate {
     private void openInstaller(final IWorkbenchWindow window) {
         BusyIndicator.showWhile(window.getShell().getDisplay(), new Runnable() {
             public void run() {
-//                UpdateManagerUI.openInstaller(window.getShell());
+                UpdateManagerUI.openInstaller(window.getShell());
             }
         });
     }
