929e64f1b76bf42e9b23fb682ac6b9f8d8b3de4c
Jody Garnett <Jody.Garnett@lisasoft.com>
2010-07-12 04:58:17 +0000

    cleanup
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31877 e5c1c795-43da-0310-a71f-fac65c449510

--- deploy/uDig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-JRE=$JAVA_HOME/jre/lib/ext
-JAI_CP=$JRE/clibwrapper_jiio.jar:$JRE/jai_codec.jar:$JRE/jai_core.jar:$JRE/jai_imageio.jar:$JRE/mlibwrapper_jai.jar
-
-java -Xbootclasspath/a:$JAI_CP -Dosgi.splashLocation=splash/splash.bmp -classpath startup.jar org.eclipse.core.launcher.Main -application net.refractions.udig.ui.uDig -os linux -ws gtk -arch x86 -nl en_US
--- deploy/udig.bat
+++
@@ -1,5 +0,0 @@
-echo on
-setlocal
-cd %~dp0
-start javaw -Xbootclasspath/a:%JAVA_HOME%\jre\lib\ext\clibwrapper_jiio.jar;%JAVA_HOME%\jre\lib\ext\jai_codec.jar;%JAVA_HOME%\jre\lib\ext\jai_core.jar;%JAVA_HOME%\jre\lib\ext\jai_imageio.jar;%JAVA_HOME%\jre\lib\ext\mlibwrapper_jai.jar -cp startup.jar org.eclipse.core.launcher.Main -application net.refractions.udig.ui.uDig %* 
-endlocal
