273cf8401639eb691377b5cd4325a998c516832d
Andrea Antonello <andrea.antonello@hydrologis.com>
2010-08-14 09:36:08 +0000

    changed externalgraphics path absolutizer to StyleVisitor
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31945 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.catalog.shp/src/net/refractions/udig/catalog/internal/shp/ShpGeoResourceImpl.java
+++ plugins/net.refractions.udig.catalog.shp/src/net/refractions/udig/catalog/internal/shp/ShpGeoResourceImpl.java
@@ -20,46 +20,59 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.List;
-
-import javax.swing.Icon;
 
 import net.refractions.udig.catalog.ID;
 import net.refractions.udig.catalog.IGeoResource;
 import net.refractions.udig.catalog.IGeoResourceInfo;
 import net.refractions.udig.catalog.IService;
 import net.refractions.udig.catalog.URLUtils;
-import net.refractions.udig.catalog.util.GeotoolsResourceInfoAdapter;
-import net.refractions.udig.ui.graphics.AWTSWTImageUtils;
-import net.refractions.udig.ui.graphics.Glyph;
-import net.refractions.udig.ui.graphics.SLDs;
 
-import org.apache.commons.io.FilenameUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.FeatureStore;
 import org.geotools.data.shapefile.indexed.IndexedShapefileDataStore;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.factory.GeoTools;
+import org.geotools.styling.AnchorPoint;
+import org.geotools.styling.ChannelSelection;
+import org.geotools.styling.ColorMap;
+import org.geotools.styling.ColorMapEntry;
+import org.geotools.styling.ContrastEnhancement;
+import org.geotools.styling.Displacement;
 import org.geotools.styling.ExternalGraphic;
+import org.geotools.styling.FeatureTypeConstraint;
 import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Fill;
 import org.geotools.styling.Graphic;
+import org.geotools.styling.Halo;
+import org.geotools.styling.ImageOutline;
+import org.geotools.styling.LinePlacement;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.Mark;
+import org.geotools.styling.NamedLayer;
+import org.geotools.styling.OverlapBehavior;
+import org.geotools.styling.PointPlacement;
 import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.RasterSymbolizer;
 import org.geotools.styling.Rule;
 import org.geotools.styling.SLD;
 import org.geotools.styling.SLDParser;
+import org.geotools.styling.SelectedChannelType;
+import org.geotools.styling.ShadedRelief;
+import org.geotools.styling.Stroke;
 import org.geotools.styling.Style;
 import org.geotools.styling.StyleFactory;
+import org.geotools.styling.StyleVisitor;
+import org.geotools.styling.StyledLayerDescriptor;
 import org.geotools.styling.Symbolizer;
+import org.geotools.styling.TextSymbolizer;
+import org.geotools.styling.UserLayer;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.metadata.citation.OnLineResource;
 import org.opengis.style.GraphicalSymbol;
-
 
 /**
  * Connect to a shapefile.
@@ -72,25 +85,25 @@ public class ShpGeoResourceImpl extends IGeoResource {
     String typename = null;
     private URL identifier;
     private ID id;
-    
+
     /**
      * Construct <code>ShpGeoResourceImpl</code>.
      *
      * @param parent
      * @param typename
      */
-    public ShpGeoResourceImpl(ShpServiceImpl parent, String typename){
+    public ShpGeoResourceImpl( ShpServiceImpl parent, String typename ) {
         this.service = parent;
         this.parent = parent;
         this.typename = typename;
         try {
-            identifier= new URL(parent.getIdentifier().toString()+"#"+typename); //$NON-NLS-1$
-            id = new ID( parent.getID(), typename );
+            identifier = new URL(parent.getIdentifier().toString() + "#" + typename); //$NON-NLS-1$
+            id = new ID(parent.getID(), typename);
         } catch (MalformedURLException e) {
-            identifier= parent.getIdentifier();
+            identifier = parent.getIdentifier();
         }
     }
-    
+
     public URL getIdentifier() {
         return identifier;
     }
@@ -111,7 +124,7 @@ public class ShpGeoResourceImpl extends IGeoResource {
     public Throwable getMessage() {
         return parent.getMessage();
     }
-    
+
     /*
      * Required adaptions:
      * <ul>
@@ -121,43 +134,42 @@ public class ShpGeoResourceImpl extends IGeoResource {
      * @see net.refractions.udig.catalog.IResolve#resolve(java.lang.Class, org.eclipse.core.runtime.IProgressMonitor)
      */
     public <T> T resolve( Class<T> adaptee, IProgressMonitor monitor ) throws IOException {
-        if(adaptee == null){
+        if (adaptee == null) {
             return null;
         }
-        if (adaptee.isAssignableFrom(IGeoResource.class)){
-            return adaptee.cast( this );
+        if (adaptee.isAssignableFrom(IGeoResource.class)) {
+            return adaptee.cast(this);
         }
-        if(adaptee.isAssignableFrom(IGeoResourceInfo.class)){
-            return adaptee.cast( createInfo(monitor) );
+        if (adaptee.isAssignableFrom(IGeoResourceInfo.class)) {
+            return adaptee.cast(createInfo(monitor));
         }
-        if(adaptee.isAssignableFrom(FeatureStore.class)){
+        if (adaptee.isAssignableFrom(FeatureStore.class)) {
             FeatureSource<SimpleFeatureType, SimpleFeature> fs = featureSource(monitor);
-            if(fs instanceof FeatureStore){
-                return adaptee.cast( fs );
+            if (fs instanceof FeatureStore) {
+                return adaptee.cast(fs);
             }
         }
-        if(adaptee.isAssignableFrom(FeatureSource.class)){
-            return adaptee.cast( featureSource(monitor) );
+        if (adaptee.isAssignableFrom(FeatureSource.class)) {
+            return adaptee.cast(featureSource(monitor));
         }
-        if(adaptee.isAssignableFrom(IndexedShapefileDataStore.class)){
-            return adaptee.cast( parent.getDS(monitor) );
+        if (adaptee.isAssignableFrom(IndexedShapefileDataStore.class)) {
+            return adaptee.cast(parent.getDS(monitor));
         }
-        if(adaptee.isAssignableFrom(Style.class)){
-        	Style style = style(monitor);
-        	if( style != null ){
-                return adaptee.cast( style(monitor));
-        	}
-        	// proceed to ask the super class, someone may
-        	// of written an IResolveAdapaterFactory providing us
-        	// with a style ...
+        if (adaptee.isAssignableFrom(Style.class)) {
+            Style style = style(monitor);
+            if (style != null) {
+                return adaptee.cast(style(monitor));
+            }
+            // proceed to ask the super class, someone may
+            // of written an IResolveAdapaterFactory providing us
+            // with a style ...
         }
         return super.resolve(adaptee, monitor);
     }
 
-    private  FeatureSource<SimpleFeatureType, SimpleFeature> featureSource( IProgressMonitor monitor ) throws IOException {
+    private FeatureSource<SimpleFeatureType, SimpleFeature> featureSource( IProgressMonitor monitor ) throws IOException {
         return parent.getDS(monitor).getFeatureSource();
     }
-    
 
     public Style style( IProgressMonitor monitor ) {
         URL url = parent.getIdentifier();
@@ -185,7 +197,7 @@ public class ShpGeoResourceImpl extends IGeoResource {
             }
             Style[] styles = parser.readXML();
 
-             FeatureSource<SimpleFeatureType, SimpleFeature> source;
+            FeatureSource<SimpleFeatureType, SimpleFeature> source;
             try {
                 source = featureSource(null);
             } catch (IOException e) {
@@ -198,7 +210,7 @@ public class ShpGeoResourceImpl extends IGeoResource {
                 if (style == null) {
                     style = styles[0];
                 }
-                
+
                 makeGraphicsAbsolute(file, style);
                 return style;
             }
@@ -218,35 +230,165 @@ public class ShpGeoResourceImpl extends IGeoResource {
      */
     private void makeGraphicsAbsolute( File relatedFile, Style style ) {
         File parentFolder = relatedFile.getParentFile();
-        Rule[] rules = SLDs.rules(style);
-        for( Rule rule : rules ) {
-            Symbolizer[] onlineResource = rule.getSymbolizers();
-            
-            for( Symbolizer symbolizer : onlineResource ) {
-                if (symbolizer instanceof PointSymbolizer) {
-                    PointSymbolizer pointSymbolizer = (PointSymbolizer) symbolizer;
-                    Graphic graphic = SLDs.graphic(pointSymbolizer);
-                    List<GraphicalSymbol> graphicalSymbols = graphic.graphicalSymbols();
-                    for( GraphicalSymbol graphicalSymbol : graphicalSymbols ) {
-                        if (graphicalSymbol instanceof ExternalGraphic) {
-                            ExternalGraphic externalGraphic = (ExternalGraphic) graphicalSymbol;
-                            try {
-                                URL location = externalGraphic.getLocation();
-                                File urlToFile = URLUtils.urlToFile(location);
-                                if (urlToFile != null && !urlToFile.exists()) {
-                                     File newFile = new File(parentFolder, urlToFile.getPath());
-                                     if (newFile.exists()) {
-                                         externalGraphic.setLocation(newFile.toURI().toURL());
-                                    }
-                                }
-                            } catch (MalformedURLException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+        
+        ExternalGraphicsAbsolutePathMaker visitor = new ExternalGraphicsAbsolutePathMaker(parentFolder);
+        visitor.visit(style);
+    }
+
+    private class ExternalGraphicsAbsolutePathMaker implements StyleVisitor {
+
+        private final File parentFolder;
+
+        public ExternalGraphicsAbsolutePathMaker( File parentFolder ) {
+            this.parentFolder = parentFolder;
+        }
+
+        public void visit( StyledLayerDescriptor sld ) {
+        }
+
+        public void visit( NamedLayer layer ) {
+        }
+
+        public void visit( UserLayer layer ) {
+        }
+
+        public void visit( Style style ) {
+            List<FeatureTypeStyle> fts = style.featureTypeStyles();
+            for( FeatureTypeStyle featureTypeStyle : fts ) {
+                featureTypeStyle.accept(this);
+            }
+        }
+
+        public void visit( Rule rule ) {
+            List<Symbolizer> syms = rule.symbolizers();
+            for( Symbolizer symbolizer : syms ) {
+                symbolizer.accept(this);
+            }
+        }
+
+        public void visit( FeatureTypeStyle fts ) {
+            List<Rule> rules = fts.rules();
+            for( Rule rule : rules ) {
+                rule.accept(this);
+            }
+        }
+
+        public void visit( Fill fill ) {
+            Graphic graphicFill = fill.getGraphicFill();
+            if (graphicFill != null) {
+                graphicFill.accept(this);
+            }
+        }
+
+        public void visit( Stroke stroke ) {
+            Graphic graphicFill = stroke.getGraphicFill();
+            if (graphicFill != null) {
+                graphicFill.accept(this);
+            }
+            Graphic graphicStroke = stroke.getGraphicStroke();
+            if (graphicStroke != null) {
+                graphicStroke.accept(this);
+            }
+        }
+
+        public void visit( Symbolizer sym ) {
+            if (sym instanceof RasterSymbolizer) {
+                visit((RasterSymbolizer) sym);
+            } else if (sym instanceof LineSymbolizer) {
+                visit((LineSymbolizer) sym);
+            } else if (sym instanceof PolygonSymbolizer) {
+                visit((PolygonSymbolizer) sym);
+            } else if (sym instanceof PointSymbolizer) {
+                visit((PointSymbolizer) sym);
+            } else if (sym instanceof TextSymbolizer) {
+                visit((TextSymbolizer) sym);
+            } else
+                throw new RuntimeException("visit(Symbolizer) unsupported");
+        }
+
+        public void visit( PointSymbolizer ps ) {
+            Graphic graphic = ps.getGraphic();
+            if (graphic != null) {
+                graphic.accept(this);
+            }
+        }
+
+        public void visit( LineSymbolizer line ) {
+            Stroke stroke = line.getStroke();
+            if (stroke != null) {
+                stroke.accept(this);
+            }
+        }
+
+        public void visit( PolygonSymbolizer poly ) {
+            Stroke stroke = poly.getStroke();
+            if (stroke != null) {
+                stroke.accept(this);
+            }
+            Fill fill = poly.getFill();
+            if (fill != null) {
+                fill.accept(this);
+            }
+        }
+
+        public void visit( TextSymbolizer text ) {}
+
+        public void visit( RasterSymbolizer raster ) {}
+
+        public void visit( Graphic gr ) {
+            List<GraphicalSymbol> graphicalSymbols = gr.graphicalSymbols();
+            for( GraphicalSymbol graphicalSymbol : graphicalSymbols ) {
+                if (graphicalSymbol instanceof ExternalGraphic) {
+                    ExternalGraphic ext = (ExternalGraphic) graphicalSymbol;
+                    ext.accept(this);
                 }
             }
         }
+
+        public void visit( Mark mark ) {}
+
+        public void visit( ExternalGraphic exgr ) {
+            try {
+                URL location = exgr.getLocation();
+                File urlToFile = URLUtils.urlToFile(location);
+                if (urlToFile != null && !urlToFile.exists()) {
+                    File newFile = new File(parentFolder, urlToFile.getPath());
+                    if (newFile.exists()) {
+                        exgr.setLocation(newFile.toURI().toURL());
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void visit( PointPlacement pp ) {}
+
+        public void visit( AnchorPoint ap ) {}
+
+        public void visit( Displacement dis ) {}
+
+        public void visit( LinePlacement lp ) {}
+
+        public void visit( Halo halo ) {}
+
+        public void visit( FeatureTypeConstraint ftc ) {}
+
+        public void visit( ColorMap colorMap ) {}
+
+        public void visit( ColorMapEntry colorMapEntry ) {}
+
+        public void visit( ContrastEnhancement contrastEnhancement ) {}
+
+        public void visit( ImageOutline outline ) {}
+
+        public void visit( ChannelSelection cs ) {}
+
+        public void visit( OverlapBehavior ob ) {}
+
+        public void visit( SelectedChannelType sct ) {}
+
+        public void visit( ShadedRelief sr ) {}
     }
 
     /**
@@ -256,28 +398,25 @@ public class ShpGeoResourceImpl extends IGeoResource {
      * @return ShpServiceImpl responsible for this ShpGeoResourceImpl
      */
     public ShpServiceImpl service() {
-    	return parent;
+        return parent;
     }
-    
+
     /*
      * @see net.refractions.udig.catalog.IResolve#canResolve(java.lang.Class)
      */
     public <T> boolean canResolve( Class<T> adaptee ) {
-        if(adaptee == null){
+        if (adaptee == null) {
             return false;
         }
-        return (adaptee.isAssignableFrom(IGeoResourceInfo.class) || 
-                adaptee.isAssignableFrom(FeatureStore.class) || 
-                adaptee.isAssignableFrom(FeatureSource.class) || 
-                adaptee.isAssignableFrom(IService.class) ||
-                adaptee.isAssignableFrom(Style.class)) ||
-                super.canResolve(adaptee);
+        return (adaptee.isAssignableFrom(IGeoResourceInfo.class) || adaptee.isAssignableFrom(FeatureStore.class)
+                || adaptee.isAssignableFrom(FeatureSource.class) || adaptee.isAssignableFrom(IService.class) || adaptee
+                .isAssignableFrom(Style.class)) || super.canResolve(adaptee);
     }
     @Override
     public ShpGeoResourceInfo getInfo( IProgressMonitor monitor ) throws IOException {
         return (ShpGeoResourceInfo) super.getInfo(monitor);
     }
-    protected ShpGeoResourceInfo createInfo(IProgressMonitor monitor) throws IOException{
+    protected ShpGeoResourceInfo createInfo( IProgressMonitor monitor ) throws IOException {
         return new ShpGeoResourceInfo(this);
     }
 }
