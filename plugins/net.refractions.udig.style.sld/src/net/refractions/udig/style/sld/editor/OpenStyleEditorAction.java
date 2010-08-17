5c223fef8e2d2bc2b4d60ef8a2427f9b1acde8b0
Andrea Antonello <andrea.antonello@hydrologis.com>
2010-08-17 16:14:20 +0000

    fix that doens't change the actual state of things, but will enable properly the new style editors if the plugin is available.
    git-svn-id: http://svn.refractions.net/udig/udig/trunk@31970 e5c1c795-43da-0310-a71f-fac65c449510

--- plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/sld/editor/OpenStyleEditorAction.java
+++ plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/sld/editor/OpenStyleEditorAction.java
@@ -1,7 +1,9 @@
 package net.refractions.udig.style.sld.editor;
 
+import java.lang.reflect.Field;
 
 import net.refractions.udig.project.internal.Layer;
+import net.refractions.udig.style.sld.SLD;
 
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.jface.action.Action;
@@ -27,9 +29,9 @@ public class OpenStyleEditorAction extends Action implements IWorkbenchWindowAct
     public static final String STYLE_ID = "net.refractions.udig.style.sld"; //$NON-NLS-1$
 
     private Layer selectedLayer;
-    
+
     private Plugin plugin;
-    
+
     /**
      * The workbench window; or <code>null</code> if this
      * action has been <code>dispose</code>d.
@@ -49,30 +51,52 @@ public class OpenStyleEditorAction extends Action implements IWorkbenchWindowAct
      * from the given resource bundle.
      * @param window
      */
-    public OpenStyleEditorAction(IWorkbenchWindow window) {
-        super();  
+    public OpenStyleEditorAction( IWorkbenchWindow window ) {
+        super();
         if (window == null) {
             throw new IllegalArgumentException();
         }
         this.workbenchWindow = window;
         setActionDefinitionId("net.refractions.style.sld.editor"); //$NON-NLS-1$
-        
-		//setToolTipText(WorkbenchMessages.OpenPreferences_toolTip); 
-		//window.getWorkbench().getHelpSystem().setHelp(this,
-		//       IWorkbenchHelpContextIds.OPEN_PREFERENCES_ACTION);
+
+        // setToolTipText(WorkbenchMessages.OpenPreferences_toolTip);
+        // window.getWorkbench().getHelpSystem().setHelp(this,
+        // IWorkbenchHelpContextIds.OPEN_PREFERENCES_ACTION);
     }
 
     public void run( IAction action ) {
         if (workbenchWindow == null) {
             return; // action has been disposed
         }
-        
+
         Shell shell = workbenchWindow.getShell();
-        //the page to select by default
-        String pageId = "simple";  //$NON-NLS-1$
-        //the filter to apply, if defined
-        //String[] displayedIds = null; 
-        
+        // the page to select by default
+        String pageId = "simple"; //$NON-NLS-1$
+        // the filter to apply, if defined
+        // String[] displayedIds = null;
+
+        try {
+            if (SLD.POINT.supports(selectedLayer)) {
+                Class< ? > pointClass = Class.forName("eu.hydrologis.jgrass.style.editorpages.SimplePointEditorPage"); //$NON-NLS-1$
+                Field idField = pointClass.getField("ID"); //$NON-NLS-1$
+                Object value = idField.get(null);
+                pageId = value.toString();
+            } else if (SLD.LINE.supports(selectedLayer)) {
+                Class< ? > pointClass = Class.forName("eu.hydrologis.jgrass.style.editorpages.SimpleLineEditorPage"); //$NON-NLS-1$
+                Field idField = pointClass.getField("ID"); //$NON-NLS-1$
+                Object value = idField.get(null);
+                pageId = value.toString();
+            } else if (SLD.POLYGON.supports(selectedLayer)) {
+                Class< ? > pointClass = Class.forName("eu.hydrologis.jgrass.style.editorpages.SimplePolygonEditorPage"); //$NON-NLS-1$
+                Field idField = pointClass.getField("ID"); //$NON-NLS-1$
+                Object value = idField.get(null);
+                pageId = value.toString();
+            }
+        } catch (Exception e) {
+            // fallback on simple
+            pageId = "simple"; //$NON-NLS-1$
+        }
+
         final EditorPageManager manager = EditorPageManager.loadManager(plugin, selectedLayer);
 
         StyleEditorDialog dialog = StyleEditorDialog.createDialogOn(shell, pageId, selectedLayer, manager);
@@ -80,18 +104,18 @@ public class OpenStyleEditorAction extends Action implements IWorkbenchWindowAct
     }
 
     public void selectionChanged( IAction action, ISelection selection ) {
-        if (selection.isEmpty() || !(selection instanceof IStructuredSelection)) 
+        if (selection.isEmpty() || !(selection instanceof IStructuredSelection))
             return;
-        
-        IStructuredSelection sselection = (IStructuredSelection)selection;
+
+        IStructuredSelection sselection = (IStructuredSelection) selection;
         if (sselection.getFirstElement() instanceof Layer) {
             selectedLayer = (Layer) sselection.getFirstElement();
         }
     }
 
     public void dispose() {
-    	plugin = null;
-    	workbenchWindow = null;
+        plugin = null;
+        workbenchWindow = null;
     }
 
     public void init( IWorkbenchWindow window ) {
