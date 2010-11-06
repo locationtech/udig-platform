f80a230560a3f0123fbdd5788289b6fa93af9c08
Andrea Antonello H <andrea.antonello@gmail.com>
2010-11-06 22:31:28 +0100

    adding raster legend part here

---
+++ plugins/eu.udig.style.jgrass/src/eu/udig/style/jgrass/legend/VectorLegendStyle.java
@@ -0,0 +1,35 @@
+/*
+ * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * (C) HydroloGIS - www.hydrologis.com 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ package eu.udig.style.jgrass.legend;
+
+import java.awt.Color;
+
+public class VectorLegendStyle {
+    public Color foregroundColor;
+    public Color backgroundColor;
+    public Color fontColor;
+    public int xPos;
+    public int yPos;
+    public int legendHeight;
+    public int legendWidth;
+    public int boxWidth;
+    public int bAlpha;
+    public int fAlpha;
+    public String titleString;
+    public boolean isRoundedRectangle;
+}
