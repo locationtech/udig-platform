d4796c28b8186082222a6d8002ec35e6b115965a
levi-putna <levi.putna@gmail.com>
2011-06-20 15:21:09 +1000

    Added getMetric override.

--- plugins/net.refractions.udig.catalog.rasterings/src/net/refractions/udig/catalog/rasterings/AbstractRasterServiceInfo.java
+++ plugins/net.refractions.udig.catalog.rasterings/src/net/refractions/udig/catalog/rasterings/AbstractRasterServiceInfo.java
@@ -1,11 +1,21 @@
 package net.refractions.udig.catalog.rasterings;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
+import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.referencing.crs.DefaultEngineeringCRS;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
 import net.refractions.udig.catalog.ID;
 import net.refractions.udig.catalog.IServiceInfo;
 
 public class AbstractRasterServiceInfo extends IServiceInfo {
 
     private final AbstractRasterService service;
+    
+    private static double UNCACHED = -1.0;
+    private double completeness = UNCACHED;
 
     public AbstractRasterServiceInfo( AbstractRasterService service, String... keywords ) {
         this.service = service;
@@ -34,6 +44,43 @@ public class AbstractRasterServiceInfo extends IServiceInfo {
     @Override
     public String getDescription() {
         return service.getIdentifier().toString();
+    }
+    
+    @Override
+    public double getMetric() {
+        if (!(completeness == UNCACHED)){
+            return completeness;
+        }
+        
+        int numberOfexpectedInfoFields = 2;
+        int numberOfAvailableInfoFields = 0;
+
+        AbstractGridCoverage2DReader reader = null;
+
+        try {
+            IProgressMonitor monitor = new NullProgressMonitor();
+            monitor.setTaskName("Checking availability of metadata for " + service.getTitle());
+            
+            reader = service.getReader(monitor);
+
+            CoordinateReferenceSystem CRS = reader.getCrs();
+
+            if (!CRS.equals(DefaultEngineeringCRS.GENERIC_2D))
+                numberOfAvailableInfoFields++;
+            
+            GeneralEnvelope env = reader.getOriginalEnvelope();
+            
+            if (!(env.isEmpty() || env.isNull() || env.isInfinite()))
+                numberOfAvailableInfoFields++;
+
+            completeness = (double) numberOfexpectedInfoFields
+                    / (double) numberOfAvailableInfoFields;
+
+        } finally {
+            reader.dispose();//clean up render.
+        }
+        
+        return completeness;
     }
     
 }
