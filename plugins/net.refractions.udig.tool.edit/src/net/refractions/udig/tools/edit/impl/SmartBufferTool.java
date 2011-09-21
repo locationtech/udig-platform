26be350207a7eef56546d8ae687623dee93e81b3
Levi Putna <levi.putna@gmail.com>
2011-09-21 15:13:22 +1000

    Initial setup for Smart Buffer Tool

---
+++ plugins/net.refractions.udig.tool.edit/src/net/refractions/udig/tools/edit/impl/SmartBufferTool.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package net.refractions.udig.tools.edit.impl;
+
+import net.refractions.udig.project.ui.tool.SimpleTool;
+
+/**
+ * @author leviputna
+ *
+ */
+public class SmartBufferTool extends SimpleTool {
+
+	/**
+	 * 
+	 */
+	public SmartBufferTool() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param targets
+	 */
+	public SmartBufferTool(int targets) {
+		super(targets);
+		// TODO Auto-generated constructor stub
+	}
+
+}
