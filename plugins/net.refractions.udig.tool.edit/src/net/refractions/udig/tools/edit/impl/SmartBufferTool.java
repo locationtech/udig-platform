0072045f438a5ade24bb720a6925f11c7ebda801
Levi Putna <levi.putna@gmail.com>
2011-09-21 15:24:44 +1000

    Smart Buffer tool behaviour and preferences added

--- plugins/net.refractions.udig.tool.edit/src/net/refractions/udig/tools/edit/impl/SmartBufferTool.java
+++ plugins/net.refractions.udig.tool.edit/src/net/refractions/udig/tools/edit/impl/SmartBufferTool.java
@@ -1,29 +1,125 @@
-/**
- * 
- */
 package net.refractions.udig.tools.edit.impl;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import java.util.List;
+import java.util.Set;
 
-import net.refractions.udig.project.ui.tool.SimpleTool;
+import org.eclipse.swt.SWT;
+import org.opengis.filter.spatial.BBOX;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+import net.refractions.udig.tool.edit.internal.Messages;
+import net.refractions.udig.tools.edit.AbstractEditTool;
+import net.refractions.udig.tools.edit.Activator;
+import net.refractions.udig.tools.edit.Behaviour;
+import net.refractions.udig.tools.edit.DefaultEditToolBehaviour;
+import net.refractions.udig.tools.edit.EditToolConfigurationHelper;
+import net.refractions.udig.tools.edit.EditToolHandler;
+import net.refractions.udig.tools.edit.EnablementBehaviour;
+import net.refractions.udig.tools.edit.activator.AdvancedBehaviourCommandHandlerActivator;
+import net.refractions.udig.tools.edit.activator.ClearCurrentSelectionActivator;
+import net.refractions.udig.tools.edit.activator.DeleteGlobalActionSetterActivator;
+import net.refractions.udig.tools.edit.activator.DrawGeomsActivator;
+import net.refractions.udig.tools.edit.activator.EditStateListenerActivator;
+import net.refractions.udig.tools.edit.activator.GridActivator;
+import net.refractions.udig.tools.edit.activator.ResetHandlerActivator;
+import net.refractions.udig.tools.edit.activator.SetRenderingFilter;
+import net.refractions.udig.tools.edit.activator.SetSnapBehaviourCommandHandlerActivator;
+import net.refractions.udig.tools.edit.activator.DrawGeomsActivator.DrawType;
+import net.refractions.udig.tools.edit.behaviour.AcceptBehaviour;
+import net.refractions.udig.tools.edit.behaviour.AcceptOnDoubleClickBehaviour;
+import net.refractions.udig.tools.edit.behaviour.BufferGeometryBehaviour;
+import net.refractions.udig.tools.edit.behaviour.CreateShapeBehaviour;
+import net.refractions.udig.tools.edit.behaviour.CursorControlBehaviour;
+import net.refractions.udig.tools.edit.behaviour.DefaultCancelBehaviour;
+import net.refractions.udig.tools.edit.behaviour.DrawCreateVertexSnapAreaBehaviour;
+import net.refractions.udig.tools.edit.behaviour.MoveGeometryBehaviour;
+import net.refractions.udig.tools.edit.behaviour.MoveVertexBehaviour;
+import net.refractions.udig.tools.edit.behaviour.SelectFeatureBehaviour;
+import net.refractions.udig.tools.edit.behaviour.SelectVertexBehaviour;
+import net.refractions.udig.tools.edit.behaviour.SelectVertexOnMouseDownBehaviour;
+import net.refractions.udig.tools.edit.behaviour.SetSnapSizeBehaviour;
+import net.refractions.udig.tools.edit.behaviour.StartEditingBehaviour;
+import net.refractions.udig.tools.edit.behaviour.accept.AcceptChangesBehaviour;
+import net.refractions.udig.tools.edit.behaviour.accept.DeselectEditShapeAcceptBehaviour;
+import net.refractions.udig.tools.edit.enablement.ValidToolDetectionActivator;
+import net.refractions.udig.tools.edit.enablement.WithinLegalLayerBoundsBehaviour;
+import net.refractions.udig.tools.edit.support.ShapeType;
+import net.refractions.udig.tools.edit.behaviour.CreateShapeBehaviour.ShapeFactory;
+
+/* uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2004-2011, Refractions Research Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 
 /**
+ * Create a Polygon from a Point or Line input usually involves applying a 
+ * Buffer to the Point or Line, the tool will only work on a layer that supports 
+ * the Polygon geometry type.
+ * 
+ * @see BufferGeometryBehaviour
  * @author leviputna
- *
+ * @since 1.2.0
  */
-public class SmartBufferTool extends SimpleTool {
+public class SmartBufferTool extends AbstractEditTool {
 
-	/**
-	 * 
-	 */
-	public SmartBufferTool() {
-		// TODO Auto-generated constructor stub
-	}
+    protected void initEnablementBehaviours( List<EnablementBehaviour> helper ) {
+        helper.add(new WithinLegalLayerBoundsBehaviour());
+        helper.add(new ValidToolDetectionActivator(new Class[]{Geometry.class, Polygon.class, MultiPolygon.class}));
+    }
 
-	/**
-	 * @param targets
-	 */
-	public SmartBufferTool(int targets) {
-		super(targets);
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    protected void initActivators( Set<Activator> activators ) {
+
+        activators.add(new EditStateListenerActivator());
+        activators.add(new ResetHandlerActivator());
+        activators.add(new SetRenderingFilter());
+        activators.add(new ClearCurrentSelectionActivator());
+    }
+
+    @Override
+    protected void initAcceptBehaviours( List<Behaviour> acceptBehaviours ) {
+
+        acceptBehaviours.add( new DeselectEditShapeAcceptBehaviour() );
+        
+        acceptBehaviours.add( new AcceptChangesBehaviour(Polygon.class, true){
+            @Override
+            public boolean isValid( EditToolHandler handler ) {
+                return super.isValid(handler) && handler.getCurrentGeom().getShapeType()==ShapeType.POLYGON;
+            }
+        });
+        
+    }
+
+    @Override
+    protected void initCancelBehaviours( List<Behaviour> cancelBehaviours ) {
+        cancelBehaviours.add(new DefaultCancelBehaviour());
+    }
+
+    @Override
+    protected void initEventBehaviours( EditToolConfigurationHelper helper ) {
+        
+        helper.add(new BufferGeometryBehaviour(getContext()));
+        helper.done();
+    }
 
 }
