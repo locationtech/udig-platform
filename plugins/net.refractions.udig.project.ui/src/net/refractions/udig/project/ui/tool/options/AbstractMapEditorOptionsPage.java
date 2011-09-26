af14de9f738ea3294f6c01cd32e3428a3e16e854
Levi Putna <levi.putna@gmail.com>
2011-09-26 13:47:19 +1000

    Add tool options attributes to the modal tool extension point

---
+++ plugins/net.refractions.udig.project.ui/src/net/refractions/udig/project/ui/tool/options/AbstractMapEditorOptionsPage.java
@@ -0,0 +1,29 @@
+/* uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2004-2011, Refractions Research Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+package net.refractions.udig.project.ui.tool.options;
+
+/**
+ * TODO Purpose of 
+ * <p>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * </p>
+ * @author leviputna
+ * @since 1.2.0
+ */
+public abstract class AbstractMapEditorOptionsPage {
+
+}
