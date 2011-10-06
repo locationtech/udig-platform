1d75bff8c8cef54b1c4fc9a5f96e876e885452f3
Gabriel Roldan <gabriel.roldan@gmail.com>
2011-10-06 02:01:25 -0300

    Adapt net.refractions.udig.catalog.wms to the geotools wms and ows API changes made by GEOT-3860

--- plugins/net.refractions.udig.catalog.tests.wms/src/net/refractions/udig/catalog/wmsc/server/WMSCParserTest.java
+++ plugins/net.refractions.udig.catalog.tests.wms/src/net/refractions/udig/catalog/wmsc/server/WMSCParserTest.java
@@ -2,15 +2,52 @@ package net.refractions.udig.catalog.wmsc.server;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.geotools.data.ows.HTTPResponse;
 import org.geotools.data.ows.WMSCapabilities;
 import org.junit.Test;
 
 public class WMSCParserTest {
 
+    private static class MockHttpResponse implements HTTPResponse{
+
+        private final InputStream in;
+        private final String contentType;
+
+        public MockHttpResponse(final InputStream in, final String contentType){
+            this.in = in;
+            this.contentType = contentType;
+        }
+        
+        @Override
+        public void dispose() {
+            try{
+                in.close();
+            }catch(Exception e){
+                //ignore, dispose() could be called multiple times
+            }
+        }
+
+        @Override
+        public String getContentType() {
+            return contentType;
+        }
+
+        @Override
+        public String getResponseHeader( String arg0 ) {
+            return null;
+        }
+
+        @Override
+        public InputStream getResponseStream() throws IOException {
+            return in;
+        }
+        
+    }
     @Test
     public void testGeoWebCache() throws Exception {
         URL url = WMSCParserTest.class.getResource("wmscCapabilities3.xml");
@@ -20,7 +57,7 @@ public class WMSCParserTest {
         // InputStream is = new ByteArrayInputStream(caps_xml.getBytes());
         WMSCCapabilitiesResponse response;
 
-        response = new WMSCCapabilitiesResponse("txt/xml", stream); //$NON-NLS-1$
+        response = new WMSCCapabilitiesResponse(new MockHttpResponse(stream, "text/xml")); //$NON-NLS-1$
         WMSCCapabilities capabilities = (WMSCCapabilities) response.getCapabilities();
 
         assertNotNull(capabilities.getCapability().getVSCapabilities());
@@ -40,7 +77,7 @@ public class WMSCParserTest {
         // InputStream is = new ByteArrayInputStream(caps_xml.getBytes());
         WMSCCapabilitiesResponse response;
 
-        response = new WMSCCapabilitiesResponse("txt/xml", stream); //$NON-NLS-1$
+        response = new WMSCCapabilitiesResponse(new MockHttpResponse(stream, "text/xml")); //$NON-NLS-1$
         WMSCCapabilities capabilities = (WMSCCapabilities) response.getCapabilities();
 
         assertNotNull(capabilities.getCapability().getVSCapabilities());
@@ -70,7 +107,7 @@ public class WMSCParserTest {
         // InputStream is = new ByteArrayInputStream(caps_xml.getBytes());
         WMSCCapabilitiesResponse response;
 
-        response = new WMSCCapabilitiesResponse("txt/xml", stream); //$NON-NLS-1$
+        response = new WMSCCapabilitiesResponse(new MockHttpResponse(stream, "text/xml")); //$NON-NLS-1$
         WMSCCapabilities capabilities = (WMSCCapabilities) response.getCapabilities();
 
         assertNotNull(capabilities.getCapability().getVSCapabilities());
@@ -87,7 +124,7 @@ public class WMSCParserTest {
         // InputStream is = new ByteArrayInputStream(caps_xml.getBytes());
         WMSCCapabilitiesResponse response;
 
-        response = new WMSCCapabilitiesResponse("txt/xml", stream); //$NON-NLS-1$
+        response = new WMSCCapabilitiesResponse(new MockHttpResponse(stream, "text/xml")); //$NON-NLS-1$
         WMSCCapabilities capabilities = (WMSCCapabilities) response.getCapabilities();
 
         assertNotNull(capabilities.getCapability().getVSCapabilities());
