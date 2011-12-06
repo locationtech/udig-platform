25c82e4ed47809bde4fa46dc6f918ee94305b30e
Cliff Broadbent <djseverin@gmail.com>
2011-12-06 22:04:24 +1000

    Fix up some of the tests in net.refractions.udig.tool.info.tests ... Still working on a few.

--- plugins/net.refractions.udig.tool.info.tests/src/net/refractions/udig/tool/info/tests/TestRenderManager.java
+++ plugins/net.refractions.udig.tool.info.tests/src/net/refractions/udig/tool/info/tests/TestRenderManager.java
@@ -4,12 +4,17 @@
 package net.refractions.udig.tool.info.tests;
 
 import java.awt.image.RenderedImage;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+
+import org.geotools.geometry.jts.ReferencedEnvelope;
 
 import net.refractions.udig.project.ILayer;
 import net.refractions.udig.project.IMap;
 import net.refractions.udig.project.render.IRenderManager;
 import net.refractions.udig.project.render.IRenderer;
+import net.refractions.udig.project.render.Tile;
 import net.refractions.udig.project.render.displayAdapter.IMapDisplay;
 
 import com.vividsolutions.jts.geom.Envelope;
@@ -73,6 +78,26 @@ public class TestRenderManager implements IRenderManager {
 	public void stopRendering() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public boolean areLayersRelatedByContext(ILayer layer, ILayer contained) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Map<ReferencedEnvelope, Tile> getTiles(
+			Collection<ReferencedEnvelope> bounds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<ReferencedEnvelope> computeTileBounds(
+			ReferencedEnvelope viewBounds, double worldunitsperpixel) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- plugins/net.refractions.udig.tool.info.tests/src/net/refractions/udig/tool/info/tests/WMSLayer.java
+++ plugins/net.refractions.udig.tool.info.tests/src/net/refractions/udig/tool/info/tests/WMSLayer.java
@@ -18,9 +18,9 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.geotools.data.Query;
 import org.geotools.data.ows.Layer;
 import org.geotools.data.wms.WebMapServer;
-import org.geotools.feature.FeatureType;
 import org.geotools.filter.Filter;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.type.FeatureType;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
 
@@ -61,7 +61,8 @@ public class WMSLayer implements ILayer {;
 		return null;
 	}
 
-	public ReferencedEnvelope getBounds(IProgressMonitor monitor, CoordinateReferenceSystem crs) throws IOException {
+	// public ReferencedEnvelope getBounds(IProgressMonitor monitor, CoordinateReferenceSystem crs) throws IOException {
+	public ReferencedEnvelope getBounds(IProgressMonitor monitor, CoordinateReferenceSystem crs) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -210,6 +211,38 @@ public class WMSLayer implements ILayer {;
 	public int compareTo(ILayer arg0) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+
+
+	@Override
+	public <T> IGeoResource findGeoResource(Class<T> clazz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public <T> boolean hasResource(Class<T> resourceType) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	@Override
+	public boolean isApplicable(Interaction interaction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+	@Override
+	public IBlackboard getBlackboard() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
