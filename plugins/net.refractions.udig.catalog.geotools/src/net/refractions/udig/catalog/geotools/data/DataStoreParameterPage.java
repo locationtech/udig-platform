70a279320e6d966e976e37f1733a1f8e398839dc
Jody Garnett <jody@Liberi.local>
2012-01-13 17:14:44 +1000

    Code review; adding headers and generally making a mess of things

--- plugins/net.refractions.udig.catalog.geotools/src/net/refractions/udig/catalog/geotools/data/DataStoreParameterPage.java
+++ plugins/net.refractions.udig.catalog.geotools/src/net/refractions/udig/catalog/geotools/data/DataStoreParameterPage.java
@@ -1,3 +1,17 @@
+/* uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2010-2012, Refractions Research Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 package net.refractions.udig.catalog.geotools.data;
 
 import java.io.File;
@@ -31,10 +45,9 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.PlatformUI;
 import org.geotools.data.DataAccessFactory;
-import org.geotools.data.DataUtilities;
 import org.geotools.data.DataAccessFactory.Param;
+import org.geotools.data.DataUtilities;
 
 public class DataStoreParameterPage extends AbstractUDIGImportPage implements UDIGConnectionPage {
 
