1f9de59924cf5ed8e2fefd69ae04aa7aef6f50e7
Paul Pfeiffer <nightdrift@gmail.com>
2012-02-10 19:01:08 +1000

    initial additions for Area of Interest Style Filter from pull request 99
    Conflicts:
    	plugins/net.refractions.udig.project/src/net/refractions/udig/project/internal/interceptor/ShowViewInterceptor.java
    	plugins/net.refractions.udig.style.sld/META-INF/MANIFEST.MF
    	plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/filter/FilterConfigurator.java
    
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/filter/FilterContent.java
+++ plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/filter/FilterContent.java
@@ -33,15 +33,29 @@ import org.geotools.filter.text.cql2.CQLException;
 import org.opengis.filter.Filter;
 
 /**
- * FilterContent is responsible for saving a filter for the current layer;
- * offers the ability to filter out content "as a view" prior to SLD
- * getting a hold of it.
+ * FilterContent is responsible for saving query information used to preprocess the current layer.
+ * filter for the current layer.
+ * <p>
+ * Currently there are three ways to do this!
+ * <ul>
+ * <li>Filter: if the value is a filter it is combined with the featureSource name and turned into a
+ * Query (this is usually supplied by a programmer).</li>
+ * <li>Query: if the value is a Query it is used directly. This is usually supplied by a
+ * programmer).</li>
+ * <li>FilterStyle: Complete filter settings supplied by user using the FilterConfigurator user
+ * interface. The number of settings here may grow in the future.</li>
+ * </ul>
+ * These settings offer the ability to "filter" out content; or using Query pre process the columns
+ * or sort information prior to the FeatureSource being used (this is accomplished using
+ * ShowViewInterceptor).
+ * 
  * @see ShowViewInterceptor
  */
 public final class FilterContent extends StyleContent {
+
     /** style id, used to identify query on a blackboard */
-    public static String STYLE_ID = ProjectBlackboardConstants.LAYER__DATA_QUERY;
-    
+    public static String STYLE_ID = ProjectBlackboardConstants.LAYER__STYLE_FILTER;
+
     /**
      * FilterContent constructor.
      */
@@ -51,71 +65,95 @@ public final class FilterContent extends StyleContent {
 
     /*
      * (non-Javadoc)
-     * 
      * @see net.refractions.udig.project.StyleContent#getStyleClass()
      */
-    public Class<?> getStyleClass() {
+    public Class< ? > getStyleClass() {
         return Boolean.class;
     }
-    
+
     /*
      * (non-Javadoc)
-     * 
      * @see net.refractions.udig.project.StyleContent#save(org.eclipse.ui.IMemento,
-     *      java.lang.Object)
+     * java.lang.Object)
      */
     public void save( IMemento memento, Object value ) {
         Filter filter = null;
-        if( value instanceof Filter ){
+        Boolean aoiFilter = null;
+        if (value instanceof Filter) {
             filter = (Filter) value;
-        }
-        else if( value instanceof Query ){
+        } else if (value instanceof Query) {
             Query query = (Query) value;
             filter = query.getFilter();
-        }      
-        if( filter != null && filter != Filter.INCLUDE ){
-            String cql = CQL.toCQL( filter );
-            memento.putString("cql", cql );
+        } else if (value instanceof FilterStyle) {
+            filter = ((FilterStyle) value).getFilter();
+            aoiFilter = ((FilterStyle) value).isAoiFilter();
         }
-        memento.putString("type", "ViewStle"); //$NON-NLS-1$ //$NON-NLS-2$
+        if (filter != null && filter != Filter.INCLUDE) {
+            String cql = CQL.toCQL(filter);
+            memento.putString("cql", cql);
+        }
+        if (aoiFilter == true) {
+            memento.putString("aoi", "true");
+        }
+        memento.putString("type", "FilterContent"); //$NON-NLS-1$ //$NON-NLS-2$
         memento.putString("version", "1.0"); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
-    /*
+    /**
+     * Retrieves a {@link FilterStyle}, or null if not provided.
      * (non-Javadoc)
-     * 
      * @see net.refractions.udig.project.StyleContent#load(org.eclipse.ui.IMemento)
      */
-    public Object load( IMemento momento ) {
-        String cql = momento.getString("cql");
-        if( cql == null || cql.length() == 0 ){
-            return null;
-            // return Filter.INCLUDE;
+    public Object load( IMemento memento ) {
+        String type = memento.getString("type");
+        String version = memento.getString("version");
+        if( "FilterContent".equals(type) && "1.0".equals(version)){
+            String cql = memento.getString("cql");
+            Filter filter = null;
+            if (cql != null && cql.length() != 0) {
+                try {
+                    filter = CQL.toFilter(cql);
+                } catch (CQLException eek) {
+                    SLDPlugin.log("Could not restore filter:" + eek, eek);
+                }
+            }
+            String aoi = memento.getString("aoiFilter"); //$NON-NLS-1$
+            boolean isAOI = "true".equalsIgnoreCase(aoi);
+    
+            if (isAOI) {
+                FilterStyle styleFilter = new FilterStyle();
+                styleFilter.setAoiFilter(true);
+                if (filter != null) {
+                    styleFilter.setFilter(filter);
+                }
+                return styleFilter;
+            } else {
+                if (filter == null) {
+                    return null;
+                } else {
+                    return new FilterStyle(filter);
+                }
+            }
         }
-        try {
-            Filter filter = CQL.toFilter(cql);
-            return filter;
-        }
-        catch( CQLException eek ){
-            SLDPlugin.log("Could not restore filter:"+eek, eek);
-            return Filter.EXCLUDE; // something is the matter
-        }
+        return null; // not a supported memento format
     }
 
     /*
      * (non-Javadoc)
-     * 
      * @see net.refractions.udig.project.StyleContent#load(java.net.URL)
      */
     public Object load( URL url, IProgressMonitor m ) throws IOException {
         return null;
     }
-    
+
     /**
-     * This will need to know the "scheme."
+     * Default FilterStyle for the provided resource.
+     * <p>
+     * Note some resources may have a good default value here; as an example background layers
+     * probably never want to have AOI turned on by default.
      */
-    public Object createDefaultStyle( IGeoResource resource, Color colour, 
-            IProgressMonitor m ) throws IOException {
-        return Filter.INCLUDE;
-    }    
+    public Object createDefaultStyle( IGeoResource resource, Color colour, IProgressMonitor m )
+            throws IOException {
+        return new FilterStyle();
+    }
 }
