b46103903256357a63cb58bc60277fdd60513f4a
Jody Garnett <jody.garnett@gmail.com>
2012-04-03 08:48:47 +1000

    Create an empty dropins folder
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- deploy/support_functions.sh
+++ deploy/support_functions.sh
@@ -46,10 +46,11 @@ function prepare_resources () {
     for opt in `find ../plugins/ -name .options` ; do cat $opt >> ${BUILD}/${PLATFORM}/udig/debug-options ; done
     cp ${BASE}/udig-1.3.x.html ${BUILD}/${PLATFORM}/udig/udig-${VERSION}.html
     cat ../plugins/net.refractions.udig.libs/.options >> ${BUILD}/${PLATFORM}/udig/.options
+    mkdir -p ${BUILD}/${PLATFORM}/udig/dropins
     if [[ $PLATFORM == linux* ]] ; then
         cp udig.sh ${BUILD}/${PLATFORM}/udig
         cp udig-clean.sh ${BUILD}/${PLATFORM}/udig
-        cp udig-debug.sh ${BUILD}/${PLATFORM}/udig        
+        cp udig-debug.sh ${BUILD}/${PLATFORM}/udig
     fi
     if [[ $PLATFORM == win* ]] ; then
         cp *.bat ${BUILD}/${PLATFORM}/udig
