e5a720de390f164d319312e93566f3080a60cf7d
moovida <andrea.antonello@gmail.com>
2012-04-15 11:22:26 +0200

    basic movie is working

--- plugins/eu.udig.tools.jgrass/src/eu/udig/tools/jgrass/featuremovieview/FeatureMovieView.java
+++ plugins/eu.udig.tools.jgrass/src/eu/udig/tools/jgrass/featuremovieview/FeatureMovieView.java
@@ -17,6 +17,18 @@
  */
 package eu.udig.tools.jgrass.featuremovieview;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.refractions.udig.project.ILayer;
+import net.refractions.udig.project.IMap;
+import net.refractions.udig.project.internal.command.navigation.ZoomCommand;
+import net.refractions.udig.project.ui.ApplicationGIS;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -28,11 +40,24 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
 
 import eu.udig.tools.jgrass.JGrassToolsPlugin;
 
@@ -51,6 +76,10 @@ public class FeatureMovieView extends ViewPart {
     private Text timerText;
     private double timer;
 
+    private boolean isRunning = false;
+    private Button playButton;
+    private Label currentFidValue;
+
     public FeatureMovieView() {
         ImageDescriptor playImageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(JGrassToolsPlugin.PLUGIN_ID,
                 "icons/play.gif");
@@ -64,7 +93,7 @@ public class FeatureMovieView extends ViewPart {
     public void createPartControl( Composite theparent ) {
 
         Composite parent = new Composite(theparent, SWT.NONE);
-        parent.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+        parent.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         parent.setLayout(new GridLayout(1, true));
 
         Group playGroup = new Group(parent, SWT.NONE);
@@ -72,28 +101,92 @@ public class FeatureMovieView extends ViewPart {
         playGroup.setLayout(new GridLayout(1, false));
         playGroup.setText("Commands");
 
-        final Button playButton = new Button(playGroup, SWT.PUSH);
+        playButton = new Button(playGroup, SWT.PUSH);
         playButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
         playButton.setText("Play");
         playButton.setImage(playImage);
         playButton.addSelectionListener(new SelectionAdapter(){
             public void widgetSelected( SelectionEvent e ) {
+                if (isRunning) {
+                    // stop it
+                    stop();
 
-                playButton.setImage(stopImage);
+                } else {
+                    // run it
+                    start();
+
+                    final IMap activeMap = ApplicationGIS.getActiveMap();
+                    ILayer selectedLayer = activeMap.getEditManager().getSelectedLayer();
+                    if (selectedLayer != null) {
+                        SimpleFeatureSource featureSource;
+                        try {
+                            featureSource = (SimpleFeatureSource) selectedLayer.getResource(FeatureSource.class,
+                                    new SubProgressMonitor(new NullProgressMonitor(), 1));
+                            SimpleFeatureCollection featureCollection = featureSource.getFeatures();
+                            final CoordinateReferenceSystem crs = featureCollection.getSchema().getCoordinateReferenceSystem();
+                            final SimpleFeatureIterator featureIterator = featureCollection.features();
+
+                            new Thread(new Runnable(){
+                                public void run() {
+
+                                    while( featureIterator.hasNext() && isRunning ) {
+                                        SimpleFeature currentFeature = featureIterator.next();
+
+                                        SimpleFeatureType featureType = currentFeature.getFeatureType();
+                                        List<AttributeDescriptor> attributeDescriptors = featureType.getAttributeDescriptors();
+                                        List<String> attributeNames = new ArrayList<String>();
+                                        for( AttributeDescriptor attributeDescriptor : attributeDescriptors ) {
+                                            String name = attributeDescriptor.getLocalName();
+                                            attributeNames.add(name);
+                                        }
+                                        final StringBuilder sb = new StringBuilder();
+                                        for( String name : attributeNames ) {
+                                            Object attribute = currentFeature.getAttribute(name);
+                                            if (attribute != null) {
+                                                sb.append(name).append(" = ").append(attribute.toString()).append("\n");
+                                            }
+                                        }
+                                        Display.getDefault().asyncExec(new Runnable(){
+                                            public void run() {
+                                                currentFidValue.setText(sb.toString());
+                                            }
+                                        });
+
+                                        Geometry geometry = (Geometry) currentFeature.getDefaultGeometry();
+                                        Envelope envelope = geometry.getEnvelopeInternal();
+                                        envelope.expandBy(zoomBuffer);
+                                        ReferencedEnvelope ref = new ReferencedEnvelope(envelope, crs);
+
+                                        ZoomCommand zoomCommand = new ZoomCommand(ref);
+                                        activeMap.sendCommandASync(zoomCommand);
+                                        activeMap.getRenderManager().refresh(null);
+                                        try {
+                                            Thread.sleep((long) (timer * 1000));
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                }
+                            }).start();
+
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    } else {
+                        MessageDialog.openWarning(getSite().getShell(), "NO LAYER SELECTED",
+                                "A feature layer needs to be selected to use the tool.");
+                        stop();
+                    }
+                }
             }
+
         });
 
         Group paramsGroup = new Group(parent, SWT.NONE);
         paramsGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
         paramsGroup.setLayout(new GridLayout(2, true));
         paramsGroup.setText("Parameters");
-
-        Label currentFidLabel = new Label(paramsGroup, SWT.NONE);
-        currentFidLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        currentFidLabel.setText("Current feature fid: ");
-        Label currentFidValue = new Label(paramsGroup, SWT.NONE);
-        currentFidValue.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        currentFidValue.setText(" - ");
 
         Label zoomBufferLabel = new Label(paramsGroup, SWT.NONE);
         zoomBufferLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
@@ -123,6 +216,23 @@ public class FeatureMovieView extends ViewPart {
         });
         updateTimer();
 
+        Group infoGroup = new Group(parent, SWT.NONE);
+        infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        infoGroup.setLayout(new GridLayout(1, true));
+        infoGroup.setText("current Feature Info");
+
+        currentFidValue = new Label(infoGroup, SWT.NONE);
+        currentFidValue.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        currentFidValue.setText(" - ");
+    }
+
+    private synchronized void start() {
+        playButton.setImage(stopImage);
+        isRunning = true;
+    }
+    private synchronized void stop() {
+        playButton.setImage(playImage);
+        isRunning = false;
     }
 
     private void updateTimer() {
