64d7c6ab0223df853ecc9ec79660d75ee7142e6a
Levi Putna <levi.putna@gmail.com>
2012-07-13 16:59:19 +1000

    add EditFeatureListenet Interface

---
+++ plugins/net.refractions.udig.project/src/net/refractions/udig/project/listener/EditFeatureListenerImpl.java
@@ -0,0 +1,55 @@
+/*
+ *    Parkinfo
+ *    http://qpws/parkinfo
+ *
+ *    (C) 2011, Department of Environment Resource Management
+ *
+ *    This code is provided for department use.
+ */
+package net.refractions.udig.project.listener;
+
+import java.util.Iterator;
+import java.util.List;
+
+import net.refractions.udig.project.interceptor.FeatureInterceptor;
+
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.type.AttributeDescriptor;
+
+
+public class EditFeatureListenerImpl implements EditFeatureListener {
+
+    @Override
+    public Boolean beforeEdit(SimpleFeature feature, AttributeDescriptor attributeDescriptor) {
+        return true;
+    }
+
+    @Override
+    public void afterEdit(SimpleFeature feature, AttributeDescriptor attributeDescriptor) {
+    }
+
+    @Override
+    public Boolean beforeCreate(SimpleFeature feature) {
+        return true;
+    }
+
+    @Override
+    public void afterCreate(SimpleFeature feature) {
+    }
+
+    @Override
+    public Boolean beforeDelete(SimpleFeature feature) {
+        return null;
+    }
+
+    @Override
+    public void afterDelete(SimpleFeature feature) {
+    }
+
+    @Override
+    public Boolean beforeDesplay(SimpleFeature feature) {
+        return true;
+    }
+
+}
