394912e2e84c1eec92e38747118ccec4febddd44
Marco Foi <foimarco@gmail.com>
2012-08-21 21:19:35 +0200

    New uDig Spatial Editing tools - FIX02

---
+++ plugins/eu.udig.tools/src/eu/udig/tools/merge/MergeOperation.java
@@ -0,0 +1,67 @@
+package eu.udig.tools.merge;
+
+import java.util.List;
+
+import net.refractions.udig.project.ILayer;
+import net.refractions.udig.project.ui.ApplicationGIS;
+import net.refractions.udig.ui.operations.IOp;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.geotools.data.FeatureSource;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.Filter;
+
+import eu.udig.tools.merge.internal.view.MergeView;
+
+/**
+ * Supports an Operation-started Workflow for MergeTool usage.
+ * 
+ * User selects some feature and, through right-click, open the "Operation -> Merge selected" menu:
+ * this calls the present class.
+ * <p>
+ * This class provides a starting point for opening the MergeView in the MERGEMODE_OPERATION
+ * status. This field is stored in MergeContext that acts as a blackboard, and is retrieved
+ * throughout the whole plug-in whenever a difference in tool behaviour has been introduced
+ * to support the operation-mode workflow (against the 'classic' MergeTool one)
+ * </p>
+ * 
+ * @author Marco Foi (www.mcfoi.it)
+ */
+public class MergeOperation implements IOp {
+    
+    private MergeView mergeView;
+
+    @Override
+    public void op(final Display display, Object target, IProgressMonitor monitor) throws Exception {
+        
+        //final FeatureSource  preSelectedLayer = (FeatureSource) target;
+
+        Thread t = new Thread() {
+
+            public void run() {
+                try {
+                    // Set tool mode (also set in MergeTool.setContext to MERGEMODE_TOOL) 
+                    MergeContext mergeContextSingleton = MergeContext.getInstance();
+                    mergeContextSingleton.setMergeMode(MergeContext.MERGEMODE_OPERATION);
+                    // Store eventual pre-selected features for later display in MergeView
+                    ILayer preSelectedLayer = ApplicationGIS.getActiveMap().getEditManager().getSelectedLayer();
+                    Filter preSelectedFilter = preSelectedLayer.getFilter();
+                    if ( preSelectedFilter != Filter.EXCLUDE){
+                        List<SimpleFeature> preSelectedFeatures = Util.retrieveFeatures(preSelectedFilter, preSelectedLayer);
+                        mergeContextSingleton.addPreselectedFeatures(preSelectedFeatures, preSelectedLayer);
+                    }
+                    
+                    // Open view
+                    mergeView = (MergeView) ApplicationGIS.getView(true, MergeView.ID);
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+
+        display.asyncExec(t);
+
+    }
+}
