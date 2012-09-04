e3e0ee9d383e32356a60cafe53c851fee9fed653
Jody Garnett <jody.garnett@gmail.com>
2012-09-04 18:40:07 +1000

    Try out the new lin32 JRE

--- deploy/versions.sh
+++ deploy/versions.sh
@@ -8,7 +8,7 @@ export BASE=`dirname $0`
 export INSTALLER="${BASE}/installer"
 #export TARGET=${BASE}/target
 #export TARGET=${BASE}/../features/net.refractions.udig-product/target/products/
-export VERSION=1.3.2-SNAPSHOT
+export VERSION=1.3-SNAPSHOT
 export BUILD="${BASE}/build"
 
 # Tycho Build
@@ -17,16 +17,11 @@ export PRODUCT_TARGET="${BASE}/../features/net.refractions.udig-product/target/p
 # Tycho SDK Build
 export SDK_TARGET="${BASE}/../features/net.refractions.udig_sdk-feature/target"
 
-# net.refractions.udig.libs "qualifier" for SDK (used to update libs source reference
-# example: TAG=1.3.2
-# example: QUALIFIER=1.3.2.201201031509
-export TAG=1.3.1
-
 # Build Resources
 export JRE="${BASE}/jre"
 export JRE_WIN32=jre1.6.0_25.win32_gdal_ecw
 export JRE_WIN64=jre1.6.0.win64
-export JRE_LIN32=jre1.6.0_25.lin32_gdal_ecw
+export JRE_LIN32=jre1.6.0_25.lin32_jai_gdal_ecw
 export JRE_LIN64=jre1.6.0_25.lin64_gdal_ecw
 
 # echo "Release Version:  ${VERSION}"
