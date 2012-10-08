300f74e77b4fa53d01cc3a3905d013de409a0c33
Jody Garnett <jody.garnett@gmail.com>
2012-10-08 23:06:18 +1000

    review a few more missing headers in eu.udig.tools

--- plugins/eu.udig.tools/src/eu/udig/tools/merge/MergeToolException.java
+++ plugins/eu.udig.tools/src/eu/udig/tools/merge/MergeToolException.java
@@ -1,3 +1,23 @@
+/* Spatial Operations & Editing Tools for uDig
+ * 
+ * Axios Engineering under a funding contract with: 
+ *      Diputación Foral de Gipuzkoa, Ordenación Territorial 
+ *
+ *      http://b5m.gipuzkoa.net
+ *      http://www.axios.es 
+ *
+ * (C) 2006, Diputación Foral de Gipuzkoa, Ordenación Territorial (DFG-OT). 
+ * DFG-OT agrees to licence under Lesser General Public License (LGPL).
+ * 
+ * You can redistribute it and/or modify it under the terms of the 
+ * GNU Lesser General Public License as published by the Free Software 
+ * Foundation; version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 package eu.udig.tools.merge;
 
 public class MergeToolException extends Exception {
