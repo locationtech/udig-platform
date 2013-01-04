06576c9dbb3967d512448e7774748ac27788cfcd
Andrea Antonello O <andrea.antonello@gmail.com>
2013-01-04 09:32:49 +0100

    license update

--- plugins/eu.udig.jconsole/src/eu/udig/jconsole/JavaFileEditorInput.java
+++ plugins/eu.udig.jconsole/src/eu/udig/jconsole/JavaFileEditorInput.java
@@ -1,21 +1,3 @@
-/*
- * JGrass - Free Open Source Java GIS http://www.jgrass.org 
- * (C) HydroloGIS - www.hydrologis.com 
- * (C) C.U.D.A.M. Universita' di Trento
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package eu.udig.jconsole;
 
 /**
@@ -49,7 +31,7 @@ import org.eclipse.ui.model.IWorkbenchAdapter;
 public class JavaFileEditorInput implements IPathEditorInput, ILocationProvider {
 
     public final static String UNTITLEDFILE = System.getProperty("user.home") + File.separator
-            + "default.jgrass";
+            + "default.groovy";
 
     /**
      * The workbench adapter which simply provides the label.
