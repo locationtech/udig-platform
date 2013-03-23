775c35481a49ff3e944403bb023f6afbc3f84e8f
Jody Garnett <jody.garnett@gmail.com>
2013-03-23 14:22:14 +1100

    update mac DMG branding
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- deploy/support_functions.sh
+++ deploy/support_functions.sh
@@ -75,7 +75,7 @@ function prepare_resources () {
     if [[ $PLATFORM == win* ]] ; then
         cp *.bat ${BUILD}/${PLATFORM}/udig
         mkdir -p ${BUILD}/${PLATFORM}/udig/icons
-        cp installer/*.ico ${BUILD}/${PLATFORM}/udig/icons/
+        cp ${INSTALLER}/*.ico ${BUILD}/${PLATFORM}/udig/icons/
         rm ${BUILD}/${PLATFORM}/udig/eclipsec.exe
         windows_installer
     fi
@@ -97,7 +97,7 @@ function prepare_resources () {
         cp "${HERE}/mac-udig-debug.sh" "${PLATFORMCONTENT}/udig/udig-debug.sh"
         mv "${PLATFORMCONTENT}/udig/.options" "${PLATFORMCONTENT}/udig/udig.app/Contents/MacOS/"
         
-        make_dmg
+        make_dmg_pretty
     fi
 }
 
@@ -148,12 +148,119 @@ function make_dmg () {
     if [ $? == 0 ] ; then
 		echo "YES!"
 		echo -n "Building MacOS DMG for product"
-		hdiutil create -fs HFS+ -volname "udig-${VERSION}" -srcfolder "${PLATFORMCONTENT}" "${PLATFORMCONTENT}/../udig-${VERSION}.${EXT}.dmg"
+		#hdiutil create -fs HFS+ -volname "udig-${VERSION}" -srcfolder "${PLATFORMCONTENT}" "${PLATFORMCONTENT}/../udig-${VERSION}.${EXT}.dmg"
+		hdiutil create -fs HFS+ \
+		    -volname "udig-${VERSION}" \
+		    -srcfolder "${PLATFORMCONTENT}"\
+		    "${PLATFORMCONTENT}/../udig-${VERSION}.${EXT}.dmg"
+	else
+		echo "NO"
+	fi
+}
+# credit to stackexchange and geoserver for dmg example
+function make_dmg_pretty () {
+    echo -n "Mac packaging utilities available... "
+	DMGTOOLS=`which hdiutil`
+    if [ $? == 0 ] ; then
+		echo "YES!"
+		
+		VOL=udig-${VERSION}.${EXT}
+        if [ -d "/Volumes/${VOL}" ]; then
+		  # unmount VOL from a previous run        
+          umount "/Volumes/${VOL}"
+          check $? "unmount ${VOL}"
+        fi
+        DMG_TMP="${BUILD}/tmp-${VOL}.dmg"
+        DMG_FINAL="${BUILD}/${VOL}.dmg"
+        DMG_BACK="background.png"
+        
+        # remove previous run
+        if [ -f "${DMG_TMP}" ]; then
+          rm -f "${DMG_TMP}"
+          check $? "rm temp dmg file"
+        fi
+        if [ -f "${DMG_FINAL}" ]; then
+          rm -f "${DMG_FINAL}"
+          check $? "rm dmg dmg file"
+        fi
+        
+        echo -n "Building Temp MacOS DMG..."
+        hdiutil create \
+            -srcfolder "${PLATFORMCONTENT}"  \
+            -volname "${VOL}" \
+            -fs HFS+ \
+            -fsargs "-c c=64,a=16,e=16" \
+            -format UDRW \
+            "${DMG_TMP}"
+        check $? "dmg build"
+
+        # mount volume so we can copy in background to make it pretty
+        sleep 5
+        device=$(hdiutil attach -readwrite -noverify -noautoopen "${DMG_TMP}" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+        sleep 5
+        echo "DEVICE: ${device}"
+        
+        mkdir "/Volumes/${VOL}/.background"
+        cp "${INSTALLER}/${DMG_BACK}" "/Volumes/${VOL}/.background/"        
+        check $? "copy background img"
+        
+        # layout the icons
+        echo "Using apple script to position icons and register background image..."
+        ln -sf /Applications /Volumes/${VOL}/Applications
+        
+        dmg_width=500
+        dmg_height=320
+        dmg_topleft_x=200
+        dmg_topleft_y=200
+        dmg_bottomright_x=`expr $dmg_topleft_x + $dmg_width`
+        dmg_bottomright_y=`expr $dmg_topleft_y + $dmg_height`
+        
+        echo '
+           tell application "Finder"
+             tell disk "'${VOL}'"
+                   open
+                   set current view of container window to icon view
+                   set toolbar visible of container window to false
+                   set statusbar visible of container window to false
+                   set the bounds of container window to {'${dmg_topleft_x}', '${dmg_topleft_y}', '${dmg_bottomright_x}', '${dmg_bottomright_y}'}
+                   set theViewOptions to the icon view options of container window
+                   set arrangement of theViewOptions to not arranged
+                   set icon size of theViewOptions to 100
+                   set background picture of theViewOptions to file ".background:'${DMG_BACK}'"
+                   set position of item "'udig'" of container window to {150, 100}
+                   set position of item "'Applications'" of container window to {350, 100}
+                   close
+                   open
+                   update without registering applications
+                   delay 5
+                   eject
+                   delay 5
+             end tell
+           end tell
+        ' | osascript
+
+        # compress tmp
+        echo -n "Compressing Final MacOS DMG..."
+        hdiutil convert "${DMG_TMP}" -format UDZO -imagekey zlib-level=9 -o "${DMG_FINAL}"
+        check $? "dmg compressing"
+        
+        if [ -f "${DMG_TMP}" ]; then
+          rm -f "${DMG_TMP}"
+        fi
 	else
 		echo "NO"
 	fi
 }
 
+# Checks return value, allowing us to supply a better error message
+function check() {
+  if [ $1 -gt 0 ]; then
+    echo "$2 failed with return value $1"
+    exit 1
+  else
+    echo "$2 succeeded return value $1"
+  fi
+}
 
 function extract_jre () {
 
