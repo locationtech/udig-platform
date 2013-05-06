b6e8ae1e7a719ad781a104bf58442d7d121d09de
fgdrf <fdgrf@users.sourceforge.net>
2013-05-06 23:12:55 +0200

    change simple LGPL header to EPL+BSD

--- plugins/net.refractions.udig.project.ui/src/net/refractions/udig/project/ui/wizard/export/MapSelectorPage.java
+++ plugins/net.refractions.udig.project.ui/src/net/refractions/udig/project/ui/wizard/export/MapSelectorPage.java
@@ -1,5 +1,11 @@
-/*
- * LGPL 
+/* uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2004-2013, Refractions Research Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
+ * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
  */
 package net.refractions.udig.project.ui.wizard.export;
 
