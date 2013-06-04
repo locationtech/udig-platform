a5d1d631e40e6456c0bd928f2a848fd624561525
Andrea Antonello O <andrea.antonello@gmail.com>
2013-06-04 15:21:48 +0200

    fixing copyright for tools.jgrass plugin

--- plugins/eu.udig.omsbox/src/eu/udig/omsbox/processingregion/ProcessingRegion.java
+++ plugins/eu.udig.omsbox/src/eu/udig/omsbox/processingregion/ProcessingRegion.java
@@ -1,13 +1,11 @@
 /*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
- *
- *    (C) 2006-2010, Open Source Geospatial Foundation (OSGeo)
+ * uDig - User Friendly Desktop Internet GIS client
+ * (C) HydroloGIS - www.hydrologis.com 
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
- * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
+ * (http://www.eclipse.org/legal/epl-v10.html), and the HydroloGIS BSD
+ * License v1.0 (http://udig.refractions.net/files/hsd3-v10.html).
  */
 package eu.udig.omsbox.processingregion;
 
