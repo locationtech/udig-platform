fda2c09b18b2b0f7910312bcbdb4cebee12226eb
Andrea Antonello O <andrea.antonello@gmail.com>
2013-06-04 15:08:57 +0200

    fixing copyright for omsbox plugin

--- plugins/eu.udig.omsbox/src/eu/udig/omsbox/utils/AnnotationUtilities.java
+++ plugins/eu.udig.omsbox/src/eu/udig/omsbox/utils/AnnotationUtilities.java
@@ -1,11 +1,11 @@
 /*
- * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * uDig - User Friendly Desktop Internet GIS client
  * (C) HydroloGIS - www.hydrologis.com 
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
- * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
+ * (http://www.eclipse.org/legal/epl-v10.html), and the HydroloGIS BSD
+ * License v1.0 (http://udig.refractions.net/files/hsd3-v10.html).
  */
 package eu.udig.omsbox.utils;
 
