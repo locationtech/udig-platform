9c3544a27d874a646a7fcdd7c5b3a83b61a8e1ed
Jody Garnett <jody.garnett@gmail.com>
2013-06-15 23:30:53 +1000

    Fix up headers mentioning County Council of Gipuzkoa, Department of Environment and Planning
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- plugins/eu.udig.image.georeferencing/src/eu/udig/image/georeferencing/internal/ui/message/InfoMessage.java
+++ plugins/eu.udig.image.georeferencing/src/eu/udig/image/georeferencing/internal/ui/message/InfoMessage.java
@@ -1,13 +1,8 @@
-/* Spatial Operations & Editing Tools for uDig
- * 
- * Axios Engineering under a funding contract with: 
- *      Diputaci—n Foral de Gipuzkoa, Ordenaci—n Territorial 
- *
- *      http://b5m.gipuzkoa.net
- *      http://www.axios.es 
- *
- * (C) 2006, Diputaci—n Foral de Gipuzkoa, Ordenaci—n Territorial (DFG-OT). 
- * DFG-OT agrees to licence under Lesser General Public License (LGPL).
+/* uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2012, Refractions Research Inc.
+ * (C) 2006, Axios Engineering S.L. (Axios)
+ * (C) 2006, County Council of Gipuzkoa, Department of Environment and Planning
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
