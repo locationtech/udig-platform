c8c2e3610d11842331bd730f12a1e48ba76db925
Jody Garnett <jody.garnett@gmail.com>
2013-06-16 21:43:47 +1000

    Removed SLDValidator from GeoServer as it was GPL code
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/sld/editor/StyleXMLPage.java
+++ plugins/net.refractions.udig.style.sld/src/net/refractions/udig/style/sld/editor/StyleXMLPage.java
@@ -57,11 +57,7 @@ public class StyleXMLPage extends StyleEditorPage {
     SashForm sash;
     Text sldTextBox;
     Text errTextBox;
-    SLDValidator validator;
-    Label validateStatus;
-    Button validateButton;
     
-    boolean validSLD = true;
     boolean dirty = false;
         
     @Override
@@ -84,8 +80,6 @@ public class StyleXMLPage extends StyleEditorPage {
             
             public void modifyText( ModifyEvent e ) {
                 dirty = true;
-                validateStatus.setText(Messages.StyleEditor_xml_validation_needed); 
-                validateButton.setEnabled(true);
             }
             
         });
@@ -104,11 +98,6 @@ public class StyleXMLPage extends StyleEditorPage {
         errTextBox.setVisible(false);
         errTextBox.setEditable(false);
 
-        validateStatus = new Label(comp, SWT.LEFT);
-        validateStatus.setText(Messages.StyleEditor_xml_validation_needed); 
-        gd = new GridData(SWT.LEFT, SWT.BOTTOM, true, false);
-        validateStatus.setLayoutData(gd);
-
 //TODO: add code for style reset        
 //        Button resetButton = new Button(comp, SWT.RIGHT);
 //        resetButton.setText("Reset");
@@ -125,25 +114,6 @@ public class StyleXMLPage extends StyleEditorPage {
 //            }
 //            
 //        });
-        
-        validateButton = new Button(comp, SWT.RIGHT);
-        validateButton.setText(Messages.StyleEditor_xml_validate); 
-        gd = new GridData(SWT.RIGHT, SWT.NONE, false, false);
-        validateButton.setLayoutData(gd);
-        validateButton.addSelectionListener(new SelectionListener() {
-
-            public void widgetSelected( SelectionEvent e ) {
-                validateSLD();
-                if (validSLD) {
-                    updateSLD();
-                }
-            }
-
-            public void widgetDefaultSelected( SelectionEvent e ) {
-                widgetSelected(e);
-            }
-            
-        });
 
         styleChanged(null);
     }
@@ -155,62 +125,9 @@ public class StyleXMLPage extends StyleEditorPage {
 
     @Override
     public String getErrorMessage() {
-        if (!validSLD) {
-            return Messages.StyleEditor_xml_invalid; 
-        }
         return null;
     }
 
-    @SuppressWarnings("unchecked")
-    public void validateSLD() {
-        Cursor waitCursor = new Cursor(Display.getCurrent(), SWT.CURSOR_WAIT);
-        setDisplayCursor(waitCursor);
-
-        validateButton.setEnabled(false);
-        validateStatus.setText(Messages.StyleEditor_xml_validation_progress); 
-        String xml = sldTextBox.getText();
-        dirty = false;
-        if (xml.length() == 0) { 
-            validSLD = false;
-            sldTextBox.setText(styleToXML()); //revert to default
-            return; 
-        }
-        InputStream is = getXMLasInputStream(xml, "UTF-8"); //$NON-NLS-1$
-        if (validator == null) {
-            validator = new SLDValidator();
-        }
-        Object result = validator.validateSLD(is, "http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"); //$NON-NLS-1$
-        
-        if (result instanceof List) {
-            List errorList = (List) result; 
-            if (errorList.size() == 0) {
-            	validSLD = true;
-                this.errTextBox.setText("");  //$NON-NLS-1$
-                this.validateStatus.setText(Messages.StyleEditor_xml_validation_success); 
-                this.errTextBox.setVisible(false);
-            } else {
-            	validSLD = false;
-                this.errTextBox.setText(SLDValidator.getErrorMessage(is, errorList));
-                this.validateStatus.setText(Messages.StyleEditor_xml_validation_failure); 
-                this.errTextBox.setVisible(true);
-            }
-            this.errTextBox.getParent().layout();
-            getContainer().updateMessage();
-        }
-        try {
-            is.close();
-        } catch (IOException e) {
-            // TODO Handle IOException
-            throw (RuntimeException) new RuntimeException( ).initCause( e );
-        } finally {
-            resetCursor(waitCursor);
-        }
-        if (dirty) {
-            validateButton.setEnabled(true);
-            validateStatus.setText(Messages.StyleEditor_xml_validation_needed); 
-        }
-    }
-    
     private void resetCursor(Cursor c) {
         setDisplayCursor(null);
         c.dispose();
@@ -236,7 +153,7 @@ public class StyleXMLPage extends StyleEditorPage {
 
     public boolean okToLeave() {
         boolean readyToLeave = false;
-        if (validSLD || (!validSLD && dirty)) { //!dirty was a condition -- not required for the moment  
+        if (dirty) { //!dirty was a condition -- not required for the moment  
             readyToLeave = updateSLD();
         }
         if (readyToLeave) {
@@ -290,7 +207,6 @@ public class StyleXMLPage extends StyleEditorPage {
     @Override
     public void styleChanged( Object source ) {
         dirty = true;
-        validateButton.setEnabled(true);
     }
  
     public void refresh() {
@@ -298,7 +214,6 @@ public class StyleXMLPage extends StyleEditorPage {
         String xml = styleToXML();
         if (xml != null && !xml.equals(xmlOrig)) {
             sldTextBox.setText(xml);
-            validSLD = true;
             dirty = true;
         }
     }
@@ -321,8 +236,6 @@ public class StyleXMLPage extends StyleEditorPage {
             style = SLDs.getDefaultStyle(sld);
         } catch (Exception e) {
             
-            validSLD = false;
-
             boolean result = MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
                     Messages.StyleEditor_xml_failure_1, 
                     Messages.StyleEditor_xml_failure_2);
@@ -354,7 +267,6 @@ public class StyleXMLPage extends StyleEditorPage {
         }
         //update the style / SLD
         if (sld != null && style != null) {
-            validSLD = true;
             setStyle(style);
             resetCursor(waitCursor);
             return true;
@@ -365,7 +277,6 @@ public class StyleXMLPage extends StyleEditorPage {
 
     @Override
     public void dispose() {
-        validator = null;
         super.dispose();
     }
 
