334c330a7019aea64625975fc64e9189b602f4d1
Emily <egouge@refractions.net>
2013-08-09 15:56:29 -0700

    ui fixes to get export dialog to work

--- plugins/eu.udig.tools.jgrass/src/eu/udig/tools/jgrass/coverageexport/TiffAsciiExportWizardPage.java
+++ plugins/eu.udig.tools.jgrass/src/eu/udig/tools/jgrass/coverageexport/TiffAsciiExportWizardPage.java
@@ -17,6 +17,7 @@ import net.refractions.udig.project.ui.ApplicationGIS;
 import net.refractions.udig.ui.CRSChooserDialog;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -28,10 +29,12 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
@@ -46,15 +49,16 @@ public class TiffAsciiExportWizardPage extends WizardPage {
     private CoordinateReferenceSystem fileCrs;
     private IGeoResource geoResource;
 
-    private boolean crsIsOk = true;
-    private boolean nameIsOk = true;
-    private boolean folderIsOk = true;
-
     private CoordinateReferenceSystem newCrs;
     private String folderPath;
     private String fileName;
     private boolean isTiff = true;
     private boolean isAscii = false;
+    
+    private ControlDecoration cdFolder;
+    private ControlDecoration cdFile;
+    private ControlDecoration cdCrs;
+    
 
     public TiffAsciiExportWizardPage() {
         super(ID);
@@ -81,21 +85,29 @@ public class TiffAsciiExportWizardPage extends WizardPage {
          */
         Label selectedLayerLabel = new Label(mainComposite, SWT.NONE);
         GridData selectedLayerLabelGd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
-        selectedLayerLabelGd.horizontalSpan = 3;
+        selectedLayerLabelGd.horizontalSpan = 1;
         selectedLayerLabel.setLayoutData(selectedLayerLabelGd);
-        selectedLayerLabel.setText("Selected layer to export: " + geoResource.getTitle());
+        selectedLayerLabel.setText("Selected Layer to Export: ");
+        
+        Label lbl = new Label(mainComposite, SWT.NONE);
+        GridData gd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+        gd.horizontalSpan = 2;
+        lbl.setLayoutData(gd);
+        lbl.setText(geoResource.getTitle());
 
         /*
          * output folder
          */
         Label outFolderLabel = new Label(mainComposite, SWT.NONE);
         outFolderLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        outFolderLabel.setText("Folder to which to save the file to");
+        outFolderLabel.setText("Output Folder:");
+        outFolderLabel.setToolTipText("Folder to which to save the file to");
 
         outFolderText = new Text(mainComposite, SWT.BORDER);
-        outFolderText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        outFolderText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
         outFolderText.setEditable(false);
-
+        cdFolder = createControlDecoration(outFolderText);
+        
         final Button outFolderButton = new Button(mainComposite, SWT.PUSH);
         outFolderButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
         outFolderButton.setText("..."); //$NON-NLS-1$
@@ -105,38 +117,32 @@ public class TiffAsciiExportWizardPage extends WizardPage {
                 String path = folderDialog.open();
                 if (path == null || path.length() < 1) {
                     outFolderText.setText(""); //$NON-NLS-1$
-                    folderIsOk = false;
+                    folderPath = null;
                 } else {
                     outFolderText.setText(path);
-                    folderIsOk = true;
                     folderPath = path;
                 }
-                checkFinish();
+                validate();
             }
         });
-
+        
         /*
          * output file name
          */
         Label outputNameLabel = new Label(mainComposite, SWT.NONE);
         outputNameLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        outputNameLabel.setText("Output file name (without extension)");
+        outputNameLabel.setText("Output File Name (without extension):");
 
         final Text outputNameText = new Text(mainComposite, SWT.BORDER);
         outputNameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
         outputNameText.addKeyListener(new KeyAdapter(){
             public void keyReleased( KeyEvent e ) {
-                String text = outputNameText.getText();
-                if (text.length() > 0) {
-                    nameIsOk = true;
-                    fileName = text;
-                } else {
-                    nameIsOk = false;
-                }
-                checkFinish();
+                fileName = outputNameText.getText();
+                validate();
             }
         });
-
+        cdFile = createControlDecoration(outputNameText);
+        
         Label dummyLabel = new Label(mainComposite, SWT.NONE);
         dummyLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
         dummyLabel.setText("");
@@ -146,13 +152,15 @@ public class TiffAsciiExportWizardPage extends WizardPage {
          */
         Label crsLabel = new Label(mainComposite, SWT.NONE);
         crsLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        crsLabel.setText("Crs to which to reproject (optional)");
+        crsLabel.setText("Crs (optional):");
+        crsLabel.setToolTipText("Crs to which to reproject (optional):");
 
         crsText = new Text(mainComposite, SWT.BORDER);
         crsText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
         crsText.setEditable(false);
-
-        final Button crsButton = new Button(mainComposite, SWT.BORDER);
+        cdCrs = createControlDecoration(crsText);
+        
+        final Button crsButton = new Button(mainComposite, SWT.PUSH);
         crsButton.setText("...");
         crsButton.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter(){
             public void widgetSelected( org.eclipse.swt.events.SelectionEvent e ) {
@@ -165,54 +173,98 @@ public class TiffAsciiExportWizardPage extends WizardPage {
                     crsText.setData(result);
                     newCrs = result;
                 }
+                validate();
             }
         });
 
+        lbl = new Label(mainComposite, SWT.NONE);
+        lbl.setLayoutData(new GridData(SWT.BEGINNING, SWT.TOP, false, false));
+        lbl.setText("Output Format:");
+
+        
         /*
          * output type
          */
-        final Button tiffRadioButton = new Button(mainComposite, SWT.RADIO);
+        Composite opFormatComp = new Composite(mainComposite, SWT.NONE);
+        opFormatComp.setLayout(new GridLayout());
+        opFormatComp.setLayoutData(new GridData(SWT.FILL,SWT.FILL, true, false));
+        ((GridData)opFormatComp.getLayoutData()).horizontalSpan = 2;
+        
+        final Button tiffRadioButton = new Button(opFormatComp, SWT.RADIO);
         GridData tiffRadioGd = new GridData(SWT.BEGINNING, SWT.CENTER, true, false);
-        tiffRadioGd.horizontalSpan = 3;
         tiffRadioButton.setLayoutData(tiffRadioGd);
         tiffRadioButton.setText("export as geotiff");
         tiffRadioButton.setSelection(true);
         tiffRadioButton.addSelectionListener(new SelectionAdapter(){
             public void widgetSelected( SelectionEvent e ) {
                 isTiff = tiffRadioButton.getSelection();
+                validate();
             }
         });
 
-        final Button asciiRadioButton = new Button(mainComposite, SWT.RADIO);
+        final Button asciiRadioButton = new Button(opFormatComp, SWT.RADIO);
         asciiRadioButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, true, false));
         asciiRadioButton.setText("export as esri ascii");
         asciiRadioButton.addSelectionListener(new SelectionAdapter(){
             public void widgetSelected( SelectionEvent e ) {
                 isAscii = asciiRadioButton.getSelection();
-                if (isAscii) {
-                    // check that it is not lat long
-                    if (fileCrs instanceof DefaultGeographicCRS) {
-                        Object crsData = crsText.getData();
-                        if (crsData == null || crsData instanceof DefaultGeographicCRS) {
-                            MessageBox msgBox = new MessageBox(asciiRadioButton.getShell(), SWT.ICON_ERROR);
-                            msgBox.setMessage("In the case of export to ascii grids, it is necessary to choose a non angular coordinate system.");
-                            msgBox.setText("Export error");
-                            msgBox.open();
-
-                            tiffRadioButton.setSelection(true);
-                            asciiRadioButton.setSelection(false);
-                            crsIsOk = false;
-                        }
-                    }
-                    checkFinish();
-                }
+                validate();
             }
         });
 
         setControl(mainComposite);
-
+        validate();
     }
 
+    private ControlDecoration createControlDecoration(Control parent){
+    	ControlDecoration dec = new ControlDecoration(parent, SWT.TOP | SWT.LEFT);
+    	dec.setImage(PlatformUI.getWorkbench().
+    	getSharedImages().getImage(ISharedImages.IMG_DEC_FIELD_ERROR));
+    	dec.hide();
+    	return dec;
+    }
+    
+	private void validate() {
+		String errorMessage = null;
+		
+		boolean show = false;
+		if (isAscii) {
+			// check that it is not lat long
+			if (fileCrs instanceof DefaultGeographicCRS) {
+				Object crsData = crsText.getData();
+				if (crsData == null || crsData instanceof DefaultGeographicCRS) {
+					errorMessage = "In the case of export to ascii grids, it is necessary to choose a non angular coordinate system.";
+					cdCrs.setDescriptionText(errorMessage);
+					show = true;
+				}
+			}
+		}
+		if (show){
+			cdCrs.show();
+		}else{
+			cdCrs.hide();
+		}
+
+		if (fileName == null || fileName.length() <= 0) {
+			errorMessage = "Invalid file name.";
+			cdFile.setDescriptionText(errorMessage);
+			cdFile.show();
+		}else{
+			cdFile.hide();
+		}
+
+		if (folderPath == null) {
+			errorMessage = "Invalid output folder.";
+			cdFolder.setDescriptionText(errorMessage);
+			cdFolder.show();
+		}else{
+			cdFolder.hide();
+		}
+
+		TiffAsciiExportWizard.canFinish = (errorMessage == null);
+		getWizard().getContainer().updateButtons();
+	}
+    
     public CoordinateReferenceSystem getNewCrs() {
         return newCrs;
     }
@@ -244,13 +296,5 @@ public class TiffAsciiExportWizardPage extends WizardPage {
         super.dispose();
     }
 
-    private void checkFinish() {
-        if (crsIsOk && nameIsOk && folderIsOk) {
-            TiffAsciiExportWizard.canFinish = true;
-        } else {
-            TiffAsciiExportWizard.canFinish = false;
-        }
-        getWizard().getContainer().updateButtons();
-    }
 
 }
