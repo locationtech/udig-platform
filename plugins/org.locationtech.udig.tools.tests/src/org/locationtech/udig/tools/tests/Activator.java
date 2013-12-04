2ff65db74f5df5e49e6c0ae337b0d6aaddee29dd
fgdrf <fgdrf@users.sourceforge.net>
2013-12-04 20:48:06 +0100

    changed missing header

--- plugins/org.locationtech.udig.tools.tests/src/org/locationtech/udig/tools/tests/Activator.java
+++ plugins/org.locationtech.udig.tools.tests/src/org/locationtech/udig/tools/tests/Activator.java
@@ -1,12 +1,17 @@
-/* uDig - User Friendly Desktop Internet GIS client
- * http://udig.refractions.net
- * (C) 2012, Refractions Research Inc.
+/*******************************************************************************
+ * Copyright (c) 2010,2012 City of Vienna.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
- * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
- */
+ * This program and the accompanying materials are made available under the            
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0    
+ * which accompanies this distribution.                                                
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at                                
+ * http://www.eclipse.org/org/documents/edl-v10.php.                                   
+ *                                                                                     
+ * Contributors:                                                                       
+ *    Aritz Davila (Axios) - initial implementation and documentation                  
+ *    Mauricio Pazos (Axios) - initial implementation and documentation
+ *******************************************************************************/
 package org.locationtech.udig.tools.tests;
 
 import org.eclipse.jface.resource.ImageDescriptor;
