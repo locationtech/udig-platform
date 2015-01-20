dccd3f908091814ac693213c0de9d5875ab8a435
fgdrf <fgdrf@users.sourceforge.net>
2015-01-20 22:51:44 +0100

    code formater

--- plugins/org.locationtech.udig.catalog.tests/src/org/locationtech/udig/catalog/tests/ShapeCatalogTest.java
+++ plugins/org.locationtech.udig.catalog.tests/src/org/locationtech/udig/catalog/tests/ShapeCatalogTest.java
@@ -40,56 +40,60 @@ import org.locationtech.udig.catalog.internal.shp.ShpServiceImpl;
  */
 public class ShapeCatalogTest {
     /**
-     * We expect the default to change during GeoTools 9.x development 
+     * We expect the default to change during GeoTools 9.x development
+     * 
      * @throws IOException
      */
-    public void testDefaultShapefileImplemenation()  throws IOException {
+    public void testDefaultShapefileImplemenation() throws IOException {
         File file = new File("data/point.shp");
-        
-        FileDataStore raw = FileDataStoreFinder.getDataStore( file );
-        assertNotNull( raw );
-        assertTrue( "shapefile implementation", raw instanceof org.geotools.data.shapefile.ShapefileDataStore );
+
+        FileDataStore raw = FileDataStoreFinder.getDataStore(file);
+        assertNotNull(raw);
+        assertTrue("shapefile implementation",
+                raw instanceof org.geotools.data.shapefile.ShapefileDataStore);
     }
-   
+
     @Test
     public void testCreateShapeNG() throws IOException {
         File file = new File("data/point.shp");
-        
+
         Map<String, Serializable> map = new HashMap<String, Serializable>();
-        map.put( "url", file.toURI().toURL() );
-        map.put( "fstype", "shape-ng" );
+        map.put("url", file.toURI().toURL());
+        map.put("fstype", "shape-ng");
         DataStore dataStore = DataStoreFinder.getDataStore(map);
-        
+
         boolean test = false;
-        
-        if(dataStore instanceof org.geotools.data.shapefile.ShapefileDataStore){
-            test  = true;
+
+        if (dataStore instanceof org.geotools.data.shapefile.ShapefileDataStore) {
+            test = true;
         }
-        assertTrue( "Check Next Generation ShapefileDataStore", test );
-        assertEquals("package",dataStore.getClass().getPackage(), org.geotools.data.shapefile.ShapefileDataStore.class.getPackage());
+        assertTrue("Check Next Generation ShapefileDataStore", test);
+        assertEquals("package", dataStore.getClass().getPackage(),
+                org.geotools.data.shapefile.ShapefileDataStore.class.getPackage());
     }
-    
+
     @Test
     public void testCreateShape() throws IOException {
-        
+
         File file = new File("data/point.shp");
         Map<String, Serializable> map = new HashMap<String, Serializable>();
-        map.put( "url", file.toURI().toURL() );
-        map.put( "fstype", "shape" );
+        map.put("url", file.toURI().toURL());
+        map.put("fstype", "shape");
         DataStore dataStore = DataStoreFinder.getDataStore(map);
-        
+
         String packageName = dataStore.getClass().getPackage().getName();
         System.out.println(packageName);
-        
-//        boolean test = false;
-//        if(dataStore instanceof IndexedShapefileDataStore){
-//            test  = true;
-//        }
-//        
-//        assertTrue( "Check is IndexedShapefileDataStore", test );
-//        assertEquals(dataStore.getClass().getPackage(), IndexedShapefileDataStore.class.getPackage());
+
+        // boolean test = false;
+        // if(dataStore instanceof IndexedShapefileDataStore){
+        // test = true;
+        // }
+        //
+        // assertTrue( "Check is IndexedShapefileDataStore", test );
+        // assertEquals(dataStore.getClass().getPackage(),
+        // IndexedShapefileDataStore.class.getPackage());
     }
-    
+
     @Test
     public void testShapeCatalogPluginFSTYPEshape() throws IOException {
         assertShapefileServiceImpl("shape");
@@ -103,21 +107,21 @@ public class ShapeCatalogTest {
     private void assertShapefileServiceImpl(final String fstype) throws MalformedURLException {
         File file = new File("data/point.shp");
         Map<String, Serializable> map = new HashMap<String, Serializable>();
-        map.put( "url", file.toURI().toURL() );
-        map.put( "fstype", fstype);
-               
+        map.put("url", file.toURI().toURL());
+        map.put("fstype", fstype);
+
         IServiceFactory serviceFactory = CatalogPlugin.getDefault().getServiceFactory();
         List<IService> test = serviceFactory.createService(map);
-        assertFalse( "Able to connect to shapefile", test.isEmpty() );
-        
-        boolean found = checkForInstanceOf( test, ShpServiceImpl.class);
-        assertTrue( "ServiceFactory was unable to connect", found );
+        assertFalse("Able to connect to shapefile", test.isEmpty());
+
+        boolean found = checkForInstanceOf(test, ShpServiceImpl.class);
+        assertTrue("ServiceFactory was unable to connect", found);
     }
-    
-    private <T> boolean checkForInstanceOf( List<T> list, Class<? extends T> type ){
-        if( list != null ){
-            for( T item : list){
-                if( type.isInstance( item )){
+
+    private <T> boolean checkForInstanceOf(List<T> list, Class<? extends T> type) {
+        if (list != null) {
+            for (T item : list) {
+                if (type.isInstance(item)) {
                     return true;
                 }
             }
--- plugins/org.locationtech.udig.issues/src/org/locationtech/udig/issues/IssuesList.java
+++ plugins/org.locationtech.udig.issues/src/org/locationtech/udig/issues/IssuesList.java
@@ -28,13 +28,11 @@ import org.locationtech.udig.issues.listeners.IssuesListEventType;
 
 import org.eclipse.ui.WorkbenchException;
 
-
-
 /**
- * An in-memory issues list. All issues will be lost at shut down unless {@link #save()} is
- * called. This is a useful utility class for implementing other issues list
- * implementations. The Datastore issues list (internal implementation) for
- * example wraps this list and uses it to cache all of its issues.
+ * An in-memory issues list. All issues will be lost at shut down unless {@link #save()} is called.
+ * This is a useful utility class for implementing other issues list implementations. The Datastore
+ * issues list (internal implementation) for example wraps this list and uses it to cache all of its
+ * issues.
  * <p>
  * Notifies listeners when issues are added or removed from list.
  * </p>
@@ -42,278 +40,263 @@ import org.eclipse.ui.WorkbenchException;
  * @author jones
  * @since 1.0.0
  */
-public class IssuesList extends AbstractSequentialList<IIssue> implements IIssuesList{
+public class IssuesList extends AbstractSequentialList<IIssue> implements IIssuesList {
 
-    LinkedList<IIssue> list=new LinkedList<IIssue>();
-    
-	/** long serialVersionUID field */
-	private static final long serialVersionUID = -7157333022429945548L;
+    LinkedList<IIssue> list = new LinkedList<IIssue>();
 
     private static final String ID = "org.locationtech.udig.issues.memory"; //$NON-NLS-1$
 
-	private boolean notify = true;
+    private boolean notify = true;
 
-    Set<String>ids=new CopyOnWriteArraySet<String>();
-    volatile int nextID=0;
-	private void checkID(IIssue i, int index){
-        if( i.getId()==null || ids.contains(i.getId()) ){
-            String newID=findNextID(index);
+    Set<String> ids = new CopyOnWriteArraySet<String>();
+
+    volatile int nextID = 0;
+
+    private void checkID(IIssue i, int index) {
+        if (i.getId() == null || ids.contains(i.getId())) {
+            String newID = findNextID(index);
             i.setId(newID);
         }
         ids.add(i.getId());
-	}
-	private String findNextID(int index) {
-        return "Issue."+nextID++; //$NON-NLS-1$
     }
+
+    private String findNextID(int index) {
+        return "Issue." + nextID++; //$NON-NLS-1$
+    }
+
     public boolean add(IIssue o) {
         checkID(o, list.size());
-		list.add(o);
-		notify(o, IssuesListEventType.ADD);
-		return true;
-	}
+        list.add(o);
+        notify(o, IssuesListEventType.ADD);
+        return true;
+    }
 
-	 
-	public void add(int index, IIssue element) {
+    public void add(int index, IIssue element) {
         checkID(element, index);
-		list.add(index, element);
-		notify(element, IssuesListEventType.ADD);
-	}
+        list.add(index, element);
+        notify(element, IssuesListEventType.ADD);
+    }
 
-	 
-	public boolean addAll(Collection<? extends IIssue> c) {
-		try {
-			notify = false;
-            for( IIssue issue : c ) {
+    public boolean addAll(Collection<? extends IIssue> c) {
+        try {
+            notify = false;
+            for (IIssue issue : c) {
                 add(issue);
             }
-		} finally {
-			notify = true;
-		}
+        } finally {
+            notify = true;
+        }
 
-		notify(c, IssuesListEventType.ADD);
+        notify(c, IssuesListEventType.ADD);
         return true;
-	}
+    }
 
-	 
-	public boolean addAll(int index, Collection<? extends IIssue> c) {
-        boolean doNotify=notify;
-		try {
-			notify = false;
-            int i=index;
-            for( IIssue issue : c ) {
-                add(i++,issue);
+    public boolean addAll(int index, Collection<? extends IIssue> c) {
+        boolean doNotify = notify;
+        try {
+            notify = false;
+            int i = index;
+            for (IIssue issue : c) {
+                add(i++, issue);
             }
-		} finally {
-			notify = doNotify;
-		}
-		notify(c, IssuesListEventType.ADD);
-		return true;
-	}
+        } finally {
+            notify = doNotify;
+        }
+        notify(c, IssuesListEventType.ADD);
+        return true;
+    }
 
-	 
-	public void addFirst(IIssue o) {
-        add(0,o);
-	}
+    public void addFirst(IIssue o) {
+        add(0, o);
+    }
 
-	 
-	public void addLast(IIssue o) {
+    public void addLast(IIssue o) {
         add(o);
-	}
+    }
 
-	 
-	public IIssue remove() {
-		IIssue issue = remove(0);
-		return issue;
-	}
+    public IIssue remove() {
+        IIssue issue = remove(0);
+        return issue;
+    }
 
-	 
-	public IIssue remove(int index) {
-		IIssue issue = list.remove(index);
+    public IIssue remove(int index) {
+        IIssue issue = list.remove(index);
         ids.remove(issue);
-		if (issue != null)
-			notify(issue, IssuesListEventType.REMOVE);
-		return issue;
-	}
+        if (issue != null)
+            notify(issue, IssuesListEventType.REMOVE);
+        return issue;
+    }
 
-	 
-	public boolean remove(Object o) {
-		if (o instanceof IIssue) {
-			IIssue issue = (IIssue) o;
-			boolean v = list.remove(issue);
-			if (v){
+    public boolean remove(Object o) {
+        if (o instanceof IIssue) {
+            IIssue issue = (IIssue) o;
+            boolean v = list.remove(issue);
+            if (v) {
                 ids.remove(issue.getId());
-				notify(issue, IssuesListEventType.REMOVE);
+                notify(issue, IssuesListEventType.REMOVE);
             }
-			return v;
-		}
-		return false;
-	}
+            return v;
+        }
+        return false;
+    }
 
-	 
-	public boolean removeAll(Collection<?> c) {
-		Collection<IIssue> i = new HashSet<IIssue>();
-		for (Object object : c) {
-			if (!(object instanceof IIssue))
-				return false;
-			i.add((IIssue) object);
-		}
-		boolean v;
-		try {
-			notify = false;
-			for( IIssue issue : i ) {
+    public boolean removeAll(Collection<?> c) {
+        Collection<IIssue> i = new HashSet<IIssue>();
+        for (Object object : c) {
+            if (!(object instanceof IIssue))
+                return false;
+            i.add((IIssue) object);
+        }
+        boolean v;
+        try {
+            notify = false;
+            for (IIssue issue : i) {
                 ids.remove(issue.getId());
             }
-            v=list.removeAll(i);
-		} finally {
-			notify = true;
-		}
-		if (v)
-			notify(i, IssuesListEventType.REMOVE);
-		return v;
-	}
+            v = list.removeAll(i);
+        } finally {
+            notify = true;
+        }
+        if (v)
+            notify(i, IssuesListEventType.REMOVE);
+        return v;
+    }
 
-	 
-	public IIssue removeFirst() {
-		return remove(0);
-	}
+    public IIssue removeFirst() {
+        return remove(0);
+    }
 
-	 
-	public IIssue removeLast() {
-        return remove(list.size()-1);
-	}
+    public IIssue removeLast() {
+        return remove(list.size() - 1);
+    }
 
-	 
-	public IIssue set(int index, IIssue element) {
-		IIssue old = list.set(index, element);
+    public IIssue set(int index, IIssue element) {
+        IIssue old = list.set(index, element);
         element.setId(old.getId());
-		notify(old, IssuesListEventType.REMOVE);
-		notify(old, IssuesListEventType.ADD);
-		return old;
-	}
+        notify(old, IssuesListEventType.REMOVE);
+        notify(old, IssuesListEventType.ADD);
+        return old;
+    }
 
-	 
-	public boolean retainAll(Collection<?> c) {
-		List<IIssue> changed = new LinkedList<IIssue>();
-		boolean modified = false;
-		Iterator<IIssue> e = list.iterator();
-		try {
-			notify = false;
-			while (e.hasNext()) {
-				IIssue current = e.next();
-				if (!c.contains(current)) {
-					e.remove();
+    public boolean retainAll(Collection<?> c) {
+        List<IIssue> changed = new LinkedList<IIssue>();
+        boolean modified = false;
+        Iterator<IIssue> e = list.iterator();
+        try {
+            notify = false;
+            while (e.hasNext()) {
+                IIssue current = e.next();
+                if (!c.contains(current)) {
+                    e.remove();
                     ids.remove(current.getId());
-					modified = true;
-					changed.add(current);
-				}
-			}
-		} finally {
-			notify = true;
-		}
-		notify(changed, IssuesListEventType.REMOVE);
-		return modified;
-	}
+                    modified = true;
+                    changed.add(current);
+                }
+            }
+        } finally {
+            notify = true;
+        }
+        notify(changed, IssuesListEventType.REMOVE);
+        return modified;
+    }
 
-	@SuppressWarnings("unchecked")
-	 
-	public void clear() {
-		List<IIssue> changed;
-		try {
-			notify = false;
-			changed = (List<IIssue>) list.clone();
+    @SuppressWarnings("unchecked")
+    public void clear() {
+        List<IIssue> changed;
+        try {
+            notify = false;
+            changed = (List<IIssue>) list.clone();
             ids.clear();
-            nextID=0;
-			list.clear();
-		} finally {
-			notify = true;
-		}
-		notify(changed, IssuesListEventType.REMOVE);
-	}
+            nextID = 0;
+            list.clear();
+        } finally {
+            notify = true;
+        }
+        notify(changed, IssuesListEventType.REMOVE);
+    }
 
-	/**
-	 * <b>This is public for tesing purposes only!!!!</b>
-	 */
-	public Collection<IIssuesListListener> listeners = new CopyOnWriteArraySet<IIssuesListListener>();
+    /**
+     * <b>This is public for tesing purposes only!!!!</b>
+     */
+    public Collection<IIssuesListListener> listeners = new CopyOnWriteArraySet<IIssuesListListener>();
 
-	private ReentrantLock issuesListLock = new ReentrantLock();
+    private ReentrantLock issuesListLock = new ReentrantLock();
 
-	public void addListener(IIssuesListListener listener) {
-		issuesListLock.lock();
-		try {
-			listeners.add(listener);
-		} finally {
-			issuesListLock.unlock();
-		}
-	}
+    public void addListener(IIssuesListListener listener) {
+        issuesListLock.lock();
+        try {
+            listeners.add(listener);
+        } finally {
+            issuesListLock.unlock();
+        }
+    }
 
-	public void removeListener(IIssuesListListener listener) {
-		issuesListLock.lock();
-		try {
-			listeners.remove(listener);
-		} finally {
-			issuesListLock.unlock();
-		}
-	}
+    public void removeListener(IIssuesListListener listener) {
+        issuesListLock.lock();
+        try {
+            listeners.remove(listener);
+        } finally {
+            issuesListLock.unlock();
+        }
+    }
 
-	protected void notify(Collection<? extends IIssue> changed,
-			IssuesListEventType type) {
-		if (!notify)
-			return;
-		
-		IssuesListEvent issuesListEvent = new IssuesListEvent(changed, type);
-		for (IIssuesListListener listener : listeners) {
+    protected void notify(Collection<? extends IIssue> changed, IssuesListEventType type) {
+        if (!notify)
+            return;
+
+        IssuesListEvent issuesListEvent = new IssuesListEvent(changed, type);
+        for (IIssuesListListener listener : listeners) {
             listener.notifyChange(issuesListEvent);
-		}
-	}
+        }
+    }
 
     /**
      * Notify listeners of a change to the list.
-     *
-     * @param changed issue that has changed.  
+     * 
+     * @param changed issue that has changed.
      * @param type Type of change.
      */
-	public void notify(IIssue changed, IssuesListEventType type) {
-		HashSet<IIssue> set = new HashSet<IIssue>();
-		set.add(changed);
-		notify(set, type);
-	}
+    public void notify(IIssue changed, IssuesListEventType type) {
+        HashSet<IIssue> set = new HashSet<IIssue>();
+        set.add(changed);
+        notify(set, type);
+    }
 
-	public Set<String> getGroups() {
-		Set<String> groups = new HashSet<String>();
-		for (IIssue issue : list) {
-			groups.add(issue.getGroupId());
-		}
-		return groups;
-	}
+    public Set<String> getGroups() {
+        Set<String> groups = new HashSet<String>();
+        for (IIssue issue : list) {
+            groups.add(issue.getGroupId());
+        }
+        return groups;
+    }
 
-	public List<IIssue> getIssues(String groupId) {
-		List<IIssue> group = new LinkedList<IIssue>();
-		for (IIssue issue : list) {
-			if( groupId==null ){
-				if( issue.getGroupId() == null ){
-					group.add(issue);
-				}
-			}else if (groupId.equals(issue.getGroupId())){
-				group.add(issue);
-			}
-		}
-		return group;
-	}
+    public List<IIssue> getIssues(String groupId) {
+        List<IIssue> group = new LinkedList<IIssue>();
+        for (IIssue issue : list) {
+            if (groupId == null) {
+                if (issue.getGroupId() == null) {
+                    group.add(issue);
+                }
+            } else if (groupId.equals(issue.getGroupId())) {
+                group.add(issue);
+            }
+        }
+        return group;
+    }
 
-	public void removeIssues(String groupId) {
-		if (groupId == null)
-			return;
-		LinkedList<IIssue> group = new LinkedList<IIssue>();
-		for (IIssue issue : list) {
-			if (groupId.equals(issue.getGroupId()))
-				group.add(issue);
-		}
-		this.removeAll(group);
-	}
-
+    public void removeIssues(String groupId) {
+        if (groupId == null)
+            return;
+        LinkedList<IIssue> group = new LinkedList<IIssue>();
+        for (IIssue issue : list) {
+            if (groupId.equals(issue.getGroupId()))
+                group.add(issue);
+        }
+        this.removeAll(group);
+    }
 
     @Override
-    public ListIterator<IIssue> listIterator( int index ) {
+    public ListIterator<IIssue> listIterator(int index) {
         return list.listIterator(index);
     }
 
@@ -321,40 +304,45 @@ public class IssuesList extends AbstractSequentialList<IIssue> implements IIssue
     public int size() {
         return list.size();
     }
+
     public String getExtensionID() {
         return ID;
     }
+
     /**
      * Sets it so that adds and removes will not raise notifications.
-     *
+     * 
      * @param notifyListeners true if notifications should be sent
      */
-    public void setNotify( boolean notifyListeners ) {
-        notify=notifyListeners;
+    public void setNotify(boolean notifyListeners) {
+        notify = notifyListeners;
     }
+
     /**
      * Loads the issues list from the .issues.xml file in the workspace
      */
-	public void load() {
-		IssuesListPersister persister = new IssuesListPersister(this, ".issues.xml"); //$NON-NLS-1$
-		try {
-			persister.load();
-		} catch (WorkbenchException e) {
-			IssuesActivator.log("Failed to load old issues because the memento could not be parsed", e); //$NON-NLS-1$
-		} catch (IOException e) {
-			IssuesActivator.log("Failed to load old issues because the file could not be read", e); //$NON-NLS-1$
-		}
-	}
+    public void load() {
+        IssuesListPersister persister = new IssuesListPersister(this, ".issues.xml"); //$NON-NLS-1$
+        try {
+            persister.load();
+        } catch (WorkbenchException e) {
+            IssuesActivator.log(
+                    "Failed to load old issues because the memento could not be parsed", e); //$NON-NLS-1$
+        } catch (IOException e) {
+            IssuesActivator.log("Failed to load old issues because the file could not be read", e); //$NON-NLS-1$
+        }
+    }
+
     /**
      * Saves the issues list to the .issues.xml file in the workspace
      */
-	public void save() {
-		IssuesListPersister persister = new IssuesListPersister(this, ".issues.xml"); //$NON-NLS-1$
-		try {
-			persister.save();
-		} catch (IOException e) {
-			IssuesActivator.log("Failed to save issues because the file could not be written", e); //$NON-NLS-1$
-		}
-	}
-    
+    public void save() {
+        IssuesListPersister persister = new IssuesListPersister(this, ".issues.xml"); //$NON-NLS-1$
+        try {
+            persister.save();
+        } catch (IOException e) {
+            IssuesActivator.log("Failed to save issues because the file could not be written", e); //$NON-NLS-1$
+        }
+    }
+
 }
