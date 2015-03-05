944695576827b36becb568821fe4b948d554208e
Frank Gasdorf <fgdrf@users.sourceforge.net>
2015-03-05 10:36:03 +0100

    fixed double listed styles after import & style remove by name
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.style.advanced/src/org/locationtech/udig/style/advanced/internal/ImportStyleSelectionAdapter.java
+++ plugins/org.locationtech.udig.style.advanced/src/org/locationtech/udig/style/advanced/internal/ImportStyleSelectionAdapter.java
@@ -1,21 +1,19 @@
 package org.locationtech.udig.style.advanced.internal;
 
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.locationtech.udig.style.advanced.common.StyleManager;
-import org.locationtech.udig.style.advanced.common.styleattributeclasses.StyleWrapper;
-import org.locationtech.udig.style.advanced.utils.Utilities;
 
 /**
- * SelectionHandler for Style Editor Import Buttons 
+ * SelectionHandler for Style Editor Import Buttons
  * 
  * @author Frank Gasdorf
  *
@@ -23,6 +21,7 @@ import org.locationtech.udig.style.advanced.utils.Utilities;
 public final class ImportStyleSelectionAdapter extends SelectionAdapter {
 
     private StyleManager styleManager;
+
     private Shell shell;
 
     /**
@@ -36,40 +35,36 @@ public final class ImportStyleSelectionAdapter extends SelectionAdapter {
 
     @Override
     public void widgetSelected(SelectionEvent e) {
-      FileDialog fileDialog = new FileDialog(shell, SWT.OPEN | SWT.MULTI);
+        FileDialog fileDialog = new FileDialog(shell, SWT.OPEN | SWT.MULTI);
 
-      // TODO add supported extension for FileDialog
-      String path = fileDialog.open();
-      if (path == null || path.length() < 1) {
-          return;
-      }
-      File firstFile = new File(path);
-      if (!firstFile.exists()) {
-          throw new IllegalArgumentException();
-      }
-      File folder = firstFile.getParentFile();
-      String[] fileNames = fileDialog.getFileNames();
-      File[] files = new File[fileNames.length];
-      for( int i = 0; i < files.length; i++ ) {
-          files[i] = new File(folder, fileNames[i]);
-      }
-      try {
-          List<StyleWrapper> styles = styleManager.getStyles();
+        // TODO add supported extension for FileDialog
+        String path = fileDialog.open();
+        if (path == null || path.length() < 1) {
+            return;
+        }
+        File firstFile = new File(path);
+        if (!firstFile.exists()) {
+            throw new IllegalArgumentException();
+        }
+        File folder = firstFile.getParentFile();
+        String[] fileNames = fileDialog.getFileNames();
+        File[] files = new File[fileNames.length];
+        for (int i = 0; i < files.length; i++) {
+            files[i] = new File(folder, fileNames[i]);
+        }
 
-          for( File file : files ) {
-              String name = file.getName();
-              File newFile = new File(styleManager.getStyleFolderFile(), name);
-              FileUtils.copyFile(file, newFile);
-              StyleWrapper style = Utilities.createStyleFromGraphic(newFile);
-              styles.add(style);
-              styleManager.addStyle(style);
-          }
-          TableViewer tableViewer = styleManager.getTableViewer();
-          if (tableViewer != null) {
-              tableViewer.refresh(false, true);
-          }
-      } catch (Exception e1) {
-          e1.printStackTrace();
-      }
+        for (File file : files) {
+            String name = file.getName();
+            File newFile = new File(styleManager.getStyleFolderFile(), name);
+            try {
+                FileUtils.copyFile(file, newFile);
+            } catch (IOException e1) {
+                MessageDialog.openWarning(shell, Messages.PointPropertiesEditor_25,
+                        "Failed to copy style file into folder " + folder + "\n" + e1.getMessage());
+            }
+        }
+
+        styleManager.reloadStyleFolder();
     }
+
 }
