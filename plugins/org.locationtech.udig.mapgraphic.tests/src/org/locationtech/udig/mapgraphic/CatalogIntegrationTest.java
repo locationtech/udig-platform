9e7679a438e14416b98997df4e308654fdfad910
fgdrf <fgdrf@users.sourceforge.net>
2015-03-17 14:06:14 +0100

    fixed mapgraphic test (getInfo called in display thread)
    Signed-off-by: fgdrf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.mapgraphic.tests/src/org/locationtech/udig/mapgraphic/CatalogIntegrationTest.java
+++ plugins/org.locationtech.udig.mapgraphic.tests/src/org/locationtech/udig/mapgraphic/CatalogIntegrationTest.java
@@ -1,31 +1,31 @@
 package org.locationtech.udig.mapgraphic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Test;
 import org.locationtech.udig.catalog.CatalogPlugin;
 import org.locationtech.udig.catalog.ID;
 import org.locationtech.udig.catalog.IGeoResource;
 import org.locationtech.udig.catalog.IRepository;
 import org.locationtech.udig.mapgraphic.internal.MapGraphicService;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.Test;
-
 public class CatalogIntegrationTest {
 
     @Test
     public void testAquire() throws Exception {
+        
         IRepository local = CatalogPlugin.getDefault().getLocal();
         local.acquire(MapGraphicService.SERVICE_URL, null);
         
         // Test ScaleBar
-        final ID SCALE_BAR = new ID("mapgraphic:/localhost/mapgraphic#scalebar", null); //$NON-NLS-1$
+        final ID SCALE_BAR = new ID(MapGraphicService.SERVICE_URL+"#scalebar", null); //$NON-NLS-1$
         IGeoResource scalebarResource = local.getById(IGeoResource.class, SCALE_BAR,
                 new NullProgressMonitor());
         assertNotNull(scalebarResource);
         
         // Test Graticule
-        final ID GRATICULE = new ID("mapgraphic:/localhost/mapgraphic#graticule", null); //$NON-NLS-1$
+        final ID GRATICULE = new ID(MapGraphicService.SERVICE_URL+"#graticule", null); //$NON-NLS-1$
         IGeoResource graticuleResource = local.getById(IGeoResource.class, GRATICULE,
                 new NullProgressMonitor());
         assertNotNull(graticuleResource);
