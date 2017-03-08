6483fe1d123a2fca36521b5e30b6cce71b61d779
Nikolaos Pringouris <nprigour@gmail.com>
2017-03-08 12:11:21 +0200

    Enable move of features between layers when reordered attribute fields exist. Consider existence of adapters during feature copy between layers
    Signed-off-by: Nikolaos Pringouris <nprigour@gmail.com>

--- plugins/org.locationtech.udig.core/src/org/locationtech/udig/core/internal/FeatureUtils.java
+++ plugins/org.locationtech.udig.core/src/org/locationtech/udig/core/internal/FeatureUtils.java
@@ -44,6 +44,11 @@ import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.CodeList;
 
+import org.locationtech.udig.core.AdapterUtil;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
@@ -234,7 +239,7 @@ public class FeatureUtils {
 
                 // don't worry about case of attribute name
                 if (target.getName().getLocalPart().equalsIgnoreCase(source.getName().getLocalPart())
-                        && target.getType().getBinding().isAssignableFrom(source.getType().getBinding())) {
+                       /* && target.getType().getBinding().isAssignableFrom(source.getType().getBinding())*/) {
                     queryAttributes.put(target.getName().getLocalPart(), source.getName().getLocalPart());
                 }
             }
@@ -336,12 +341,36 @@ public class FeatureUtils {
     private static Object[] copyAttributes( SimpleFeatureType destSchema, SimpleFeature source,
             Map<String, Iterator< ? extends Geometry>> geometries, Map<String, String> attributeMap, MathTransform mt  ) {
         Object[] attributes = new Object[destSchema.getAttributeCount()];
+        
+        //dummy progress monitor
+        IProgressMonitor mon = new NullProgressMonitor();
+        
         for( int i = 0; i < attributes.length; i++ ) {
             String sourceAttributeName = destSchema.getDescriptor(i).getName().getLocalPart();
             String name = attributeMap.get(sourceAttributeName);
-            if (name != null)
-                attributes[i] = source.getAttribute(name);
-            else {
+            //if an attribute match exists set its value
+            if (name != null) {                 
+                if (destSchema.getDescriptor(sourceAttributeName).getType().getBinding().isAssignableFrom(
+                        source.getFeatureType().getDescriptor(sourceAttributeName).getType().getBinding())) {
+                    attributes[i] = source.getAttribute(name);
+                } else {
+                    //if not directly assignable then check for an adapter
+                    if (AdapterUtil.instance.canAdaptTo(
+                            source.getAttribute(sourceAttributeName), 
+                            destSchema.getDescriptor(i).getType().getBinding())) {
+                        try {
+                            attributes[i] = AdapterUtil.instance.adaptTo(
+                                    destSchema.getDescriptor(i).getType().getBinding(), 
+                                    source.getAttribute(sourceAttributeName), mon);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        attributes[i] = destSchema.getDescriptor(i).getDefaultValue();
+                    }
+                }
+            } else {
+                //if we reach here use the default value
                 attributes[i] = destSchema.getDescriptor(i).getDefaultValue();
             }
             if (attributes[i] instanceof Geometry) {
