d24911bc783569780c32832f338eaf252c202bcf
Frank Gasdorf <fgdrf@users.sourceforge.net>
2017-03-20 15:46:02 +0100

    [pdf] remember export settings for pdf wizard page
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.project.ui/src/org/locationtech/udig/project/ui/wizard/export/image/PDFImageExportFormat.java
+++ plugins/org.locationtech.udig.project.ui/src/org/locationtech/udig/project/ui/wizard/export/image/PDFImageExportFormat.java
@@ -13,6 +13,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -24,6 +25,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 import org.locationtech.udig.project.IMap;
 import org.locationtech.udig.project.ui.internal.Messages;
+import org.locationtech.udig.project.ui.internal.ProjectUIPlugin;
 
 /**
  * A strategy for exporting to PDF
@@ -34,13 +36,21 @@ import org.locationtech.udig.project.ui.internal.Messages;
  */
 public class PDFImageExportFormat extends ImageExportFormat {
 
+
     private static final int PDF_DEFAULT_USER_UNIT = 72;
-    
+    private static final String PREFSTORE_PARAM_MARGIN_TOP = "MarginTop";
+    private static final String PREFSTORE_PARAM_MARGIN_BOTTOM = "MarginBottom";
+    private static final String PREFSTORE_PARAM_MARGIN_RIGHT = "MarginRight";
+    private static final String PREFSTORE_PARAM_MARGIN_LEFT = "MarginLeft";
+    private static final String PREFSTORE_PARAM_DPI = "DPI";
+    private static final String PREFSTORE_PARAM_PAGEFORMAT = "Pageformat";
+    private static final String PREFSTORE_PARAM_LANDSCAPE = "Landscape";
+
     private Combo dpiCombo;
-    private Spinner topMarginSpinner;
-    private Spinner lowerMarginSpinner;
-    private Spinner leftMarginSpinner;
-    private Spinner rightMarginSpinner;
+    private Spinner marginTopSpinner;
+    private Spinner marginBottomSpinner;
+    private Spinner marginLeftSpinner;
+    private Spinner marginRightSpinner;
     private Combo paperCombo;
     private Button landscape;
 
@@ -59,12 +69,13 @@ public class PDFImageExportFormat extends ImageExportFormat {
 
     @Override
     public void write( IMap map, BufferedImage image, File destination ) {
+        savePreferences();
         Image2Pdf.write(image, destination.getAbsolutePath(), paper(),
                new Insets(
-                       Paper.toPixels(this.topMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
-                       Paper.toPixels(this.leftMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
-                       Paper.toPixels(this.lowerMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
-                       Paper.toPixels(this.rightMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT)
+                       Paper.toPixels(this.marginTopSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
+                       Paper.toPixels(this.marginLeftSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
+                       Paper.toPixels(this.marginBottomSpinner.getSelection(), PDF_DEFAULT_USER_UNIT),
+                       Paper.toPixels(this.marginRightSpinner.getSelection(), PDF_DEFAULT_USER_UNIT)
                        ), landscape());
     }
 
@@ -81,6 +92,7 @@ public class PDFImageExportFormat extends ImageExportFormat {
         createDpiCombo(group);
         createMarginsGroup(group);
 
+        loadAndApplyPreferences();
         setControl(group);
     }
 
@@ -114,20 +126,20 @@ public class PDFImageExportFormat extends ImageExportFormat {
         marginsGroup.setText(Messages.ImageExportPage_marginsGroup);
         Label topLabel = new Label(marginsGroup, SWT.NONE);
         topLabel.setText(Messages.ImageExportPage_topMargin);
-        topMarginSpinner = new Spinner(marginsGroup, SWT.NONE);
-        topMarginSpinner.setSelection(10);
-        Label lowerLabel = new Label(marginsGroup, SWT.NONE);
-        lowerLabel.setText(Messages.ImageExportPage_lowerMargin);
-        lowerMarginSpinner = new Spinner(marginsGroup, SWT.NONE);
-        lowerMarginSpinner.setSelection(10);
+        marginTopSpinner = new Spinner(marginsGroup, SWT.NONE);
+        marginTopSpinner.setSelection(10);
+        Label bottomLabel = new Label(marginsGroup, SWT.NONE);
+        bottomLabel.setText(Messages.ImageExportPage_bottomMargin);
+        marginBottomSpinner = new Spinner(marginsGroup, SWT.NONE);
+        marginBottomSpinner.setSelection(10);
         Label leftLabel = new Label(marginsGroup, SWT.NONE);
         leftLabel.setText(Messages.ImageExportPage_leftMargin);
-        leftMarginSpinner = new Spinner(marginsGroup, SWT.NONE);
-        leftMarginSpinner.setSelection(10);
+        marginLeftSpinner = new Spinner(marginsGroup, SWT.NONE);
+        marginLeftSpinner.setSelection(10);
         Label rightLabel = new Label(marginsGroup, SWT.NONE);
         rightLabel.setText(Messages.ImageExportPage_rightMargin);
-        rightMarginSpinner = new Spinner(marginsGroup, SWT.NONE);
-        rightMarginSpinner.setSelection(10);
+        marginRightSpinner = new Spinner(marginsGroup, SWT.NONE);
+        marginRightSpinner.setSelection(10);
 
     }
 
@@ -180,20 +192,63 @@ public class PDFImageExportFormat extends ImageExportFormat {
     public int getHeight( double mapwidth, double mapheight ) {
         // ignore viewport size of the current map and use paper format instead
         int paperHeight = paper().getPixelHeight(landscape(), getDPI());
-        int topMargin = Paper.toPixels(topMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
-        int lowerMargin = Paper.toPixels(lowerMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
+        int topMargin = Paper.toPixels(marginTopSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
+        int bottomMargin = Paper.toPixels(marginBottomSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
 
-        return paperHeight - topMargin - lowerMargin;
+        return paperHeight - topMargin - bottomMargin;
     }
 
     @Override
     public int getWidth( double mapwidth, double mapheight ) {
         // ignore viewport size of the current map and use paper format instead
         int paperWidth = paper().getPixelWidth(landscape(), getDPI());
-        int rightMargin = Paper.toPixels(rightMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
-        int leftMargin = Paper.toPixels(leftMarginSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
+        int rightMargin = Paper.toPixels(marginRightSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
+        int leftMargin = Paper.toPixels(marginLeftSpinner.getSelection(), PDF_DEFAULT_USER_UNIT);
 
         return paperWidth - rightMargin - leftMargin;
     }
 
+    private void savePreferences() {
+    	final IPreferenceStore prefStore = ProjectUIPlugin.getDefault().getPreferenceStore();
+        prefStore.setValue(PREFSTORE_PARAM_MARGIN_TOP, marginTopSpinner.getSelection());
+        prefStore.setValue(PREFSTORE_PARAM_MARGIN_BOTTOM, marginBottomSpinner.getSelection());
+        prefStore.setValue(PREFSTORE_PARAM_MARGIN_LEFT, marginLeftSpinner.getSelection());
+        prefStore.setValue(PREFSTORE_PARAM_MARGIN_RIGHT, marginRightSpinner.getSelection());
+        prefStore.setValue(PREFSTORE_PARAM_DPI, dpiCombo.getSelectionIndex());
+        prefStore.setValue(PREFSTORE_PARAM_LANDSCAPE, landscape());
+        prefStore.setValue(PREFSTORE_PARAM_PAGEFORMAT, paperCombo.getSelectionIndex());
+    }
+
+    private void loadAndApplyPreferences() {
+    	final IPreferenceStore prefStore = ProjectUIPlugin.getDefault().getPreferenceStore();
+        final int dpiFromStore = prefStore.getInt(PREFSTORE_PARAM_DPI);
+        final int marginTop = prefStore.getInt(PREFSTORE_PARAM_MARGIN_TOP);
+        final int marginBottom = prefStore.getInt(PREFSTORE_PARAM_MARGIN_BOTTOM);
+        final int marginLeft = prefStore.getInt(PREFSTORE_PARAM_MARGIN_LEFT);
+        final int marginRight = prefStore.getInt(PREFSTORE_PARAM_MARGIN_RIGHT);
+        final boolean landscapeFromStore = prefStore.getBoolean(PREFSTORE_PARAM_LANDSCAPE);
+        final int pageFormatFromStore = prefStore.getInt(PREFSTORE_PARAM_PAGEFORMAT);
+
+        if (dpiFromStore != 0) {
+            dpiCombo.select(dpiFromStore);
+        }
+        if (marginTop != 0) {
+            marginTopSpinner.setSelection(marginTop);
+        }
+        if (marginBottom != 0) {
+            marginBottomSpinner.setSelection(marginBottom);
+        }
+        if (marginLeft != 0) {
+            marginLeftSpinner.setSelection(marginLeft);
+        }
+        if (marginRight != 0) {
+            marginRightSpinner.setSelection(marginRight);
+        }
+        if (landscapeFromStore) {
+            landscape.setSelection(true);
+        }
+        if (pageFormatFromStore != 0) {
+            paperCombo.select(pageFormatFromStore);
+        }
+    }
 }
