2f5ec3f17fe42a604627bee82f08997a159915cc
Frank Gasdorf <fgdrf@users.sourceforge.net>
2017-03-21 23:44:31 +0100

    cleanup non-functional & un-maintained csw modules
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- libs/scala-csw-client/src/test/scala/eu/udig/catalog/csw/Setup.scala
+++
@@ -1,8 +0,0 @@
-package eu.udig.catalog.csw
-
-object Setup {
-	val context = GeonetworkContext("www.ifremer.fr", lang = "fr")
-//  val context = GeonetworkContext("www.geocat.ch", lang = "eng")
-//  val context = GeonetworkContext("c2cpc61.camptocamp.com", lang = "en")
-//	val context = GeonetworkContext("geonetwork.grid.unep.ch", lang = "en")
-}
--- plugins/org.locationtech.udig.catalog.csw/readme.txt
+++
@@ -1,9 +0,0 @@
-This plugin requires:
-
-* libs/csw-client_2.9.1-1.0-SNAPSHOT.min.jar
-
-As created by the scala project: libs/scala-csw-client
-
-For now the code is commented out until we can find a smooth
-way to integrate this into our build system as
-a maven dependency.
