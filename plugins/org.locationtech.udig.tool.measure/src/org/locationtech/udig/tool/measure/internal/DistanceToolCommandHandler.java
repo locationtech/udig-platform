f2a8db9e5c381aee82f20c72f3c18f4821bfdadc
Frank Gasdorf <fgdrf@users.sourceforge.net>
2018-02-14 15:18:03 +0100

    #266 - moves measurement tool into seperate bundle
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.tool.measure/src/org/locationtech/udig/tool/measure/internal/DistanceToolCommandHandler.java
+++ plugins/org.locationtech.udig.tool.measure/src/org/locationtech/udig/tool/measure/internal/DistanceToolCommandHandler.java
@@ -1,18 +1,17 @@
 /* uDig - User Friendly Desktop Internet GIS client
  * http://udig.refractions.net
- * (C) 2004, Refractions Research Inc.
+ * (C) 2018, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
  * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
  */
-package org.locationtech.udig.tool.info.internal;
+package org.locationtech.udig.tool.measure.internal;
 
 import org.locationtech.udig.project.ui.tool.IToolHandler;
 import org.locationtech.udig.project.ui.tool.Tool;
-import org.locationtech.udig.tool.info.DistanceTool;
-
+import org.locationtech.udig.tool.measure.DistanceTool;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
