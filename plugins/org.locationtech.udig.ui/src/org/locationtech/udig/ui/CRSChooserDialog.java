905a3909f0ced45e88830cf1b21376ecca6e61b8
Frank Gasdorf <fgdrf@users.sourceforge.net>
2020-09-30 07:40:48 +0200

    [HK] refactoring Statusbar ContributionItems
    * single sourcing and
    * cleanup MapEditors
    
    Change-Id: Id29b2d2f9c2fc98bcab94d83cca884fda492d58a
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.ui/src/org/locationtech/udig/ui/CRSChooserDialog.java
+++ plugins/org.locationtech.udig.ui/src/org/locationtech/udig/ui/CRSChooserDialog.java
@@ -3,51 +3,58 @@
  */
 package org.locationtech.udig.ui;
 
-import org.locationtech.udig.ui.internal.Messages;
-
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.locationtech.udig.ui.internal.Messages;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+/**
+ * Dialog to choose a {@link CoordinateReferenceSystem} with CRSChooser.
+ *
+ */
 public class CRSChooserDialog extends Dialog {
-	private final CRSChooser chooser=new CRSChooser();
-	private final CoordinateReferenceSystem initialValue;
-	private CoordinateReferenceSystem result;
+    private final CRSChooser chooser = new CRSChooser();
 
-	public CRSChooserDialog(Shell parentShell, CoordinateReferenceSystem initialValue) {
-		super(parentShell);
-		this.initialValue=initialValue;
-	}
+    private final CoordinateReferenceSystem initialValue;
 
-	@Override
-	protected Control createDialogArea( Composite parent ) {
-		getShell().setText(Messages.CRSChooserDialog_title);
-	    chooser.setController(new Controller(){
+    private CoordinateReferenceSystem result;
 
-	        public void handleClose() {
-	            close();
-	        }
+    public CRSChooserDialog(Shell parentShell, CoordinateReferenceSystem initialValue) {
+        super(parentShell);
+        this.initialValue = initialValue;
+    }
 
-	        public void handleOk() {
-	            result=chooser.getCRS();
-	        }
-	        
-	    });
-	    
-	    Control control = chooser.createControl(parent, initialValue);
-	    chooser.setFocus();
-		return control;
-	}
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        getShell().setText(Messages.CRSChooserDialog_title);
+        chooser.setController(new Controller() {
 
-	@Override
-	public boolean close() {
-	    result=chooser.getCRS();
-	    return super.close();
-	}
-	
-	public CoordinateReferenceSystem getResult() {
-		return result;
-	}
+            @Override
+            public void handleClose() {
+                close();
+            }
+
+            @Override
+            public void handleOk() {
+                result = chooser.getCRS();
+            }
+
+        });
+
+        Control control = chooser.createControl(parent, initialValue);
+        chooser.setFocus();
+        return control;
+    }
+
+    @Override
+    public boolean close() {
+        result = chooser.getCRS();
+        return super.close();
+    }
+
+    public CoordinateReferenceSystem getResult() {
+        return result;
+    }
 }
