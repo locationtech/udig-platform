df84159cd713117055c753010a1d115d32e9d345
Emily <egouge@refractions.net>
2021-05-24 10:55:10 -0700

    fix for metric rating computation (#453)
    * fix for metric rating computation
    
    Signed-off-by: Emily Gouge <egouge@refractions.net>
    
    * test case for service getInfo().getMetric() for rasters
    
    Signed-off-by: Emily Gouge <egouge@refractions.net>
    
    * first aproach using Mockito
    
    Change-Id: I66b1ef308b07581696610079a90908077071bb3f
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>
    
    * moves test into rasterings.tests-bundle
    
    Change-Id: Ibba1dc64c9aa68f478b2b1baf4292f791313a88e
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>
    
    Co-authored-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.catalog.rasterings/src/org/locationtech/udig/catalog/rasterings/AbstractRasterService.java
+++ plugins/org.locationtech.udig.catalog.rasterings/src/org/locationtech/udig/catalog/rasterings/AbstractRasterService.java
@@ -24,6 +24,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
+import org.geotools.coverage.grid.io.GridCoverage2DReader;
 import org.geotools.coverage.grid.io.GridFormatFactorySpi;
 
 /**
@@ -31,76 +32,83 @@ import org.geotools.coverage.grid.io.GridFormatFactorySpi;
  * <p>
  * This class provides functionality common to services attached to GridCoverage based resources.
  * </p>
- * 
+ *
  * @author mleslie
  * @since 0.6.0
  */
 public abstract class AbstractRasterService extends IService {
     private ID id;
-        
+
     /** <code>message</code> field reports any errors encountered. May be null. */
     protected Exception message = null;
-    
+
     /** <code>reader</code> field */
     protected AbstractGridCoverage2DReader reader;
-    
+
     private AbstractGridFormat format;
-    
+
     private GridFormatFactorySpi factory;
 
     /**
      * Construct <code>AbstractRasterService</code>.
-     * 
+     *
      * @param id
      * @param factory
      */
-    public AbstractRasterService( URL url, String typeQualifier, GridFormatFactorySpi factory ) {
-        this.id = new ID( url, typeQualifier );
+    public AbstractRasterService(URL url, String typeQualifier, GridFormatFactorySpi factory) {
+        this.id = new ID(url, typeQualifier);
         this.factory = factory;
     }
 
     /**
      * Adapts to to provide AbstractGridCoverage2DReader if needed.
      */
+    @Override
     public <T> boolean canResolve(Class<T> adaptee) {
-        return adaptee != null && (
-            adaptee.isAssignableFrom( AbstractGridCoverage2DReader.class ) ||
-            super.canResolve(adaptee) );
+        return adaptee != null && (adaptee.isAssignableFrom(AbstractGridCoverage2DReader.class)
+                || super.canResolve(adaptee));
     }
+
+    @Override
     public Status getStatus() {
-        if( reader == null ){
+        if (reader == null) {
             return super.getStatus();
         }
         return Status.CONNECTED;
     }
-    public void dispose( IProgressMonitor monitor ) {
-        if( reader != null ){
+
+    @Override
+    public void dispose(IProgressMonitor monitor) {
+        if (reader != null) {
             reader.dispose();
             reader = null;
         }
         super.dispose(monitor);
     }
-    
+
+    @Override
     public Throwable getMessage() {
         return this.message;
     }
 
+    @Override
     public URL getIdentifier() {
         return id.toURL();
     }
-    
+
+    @Override
     public ID getID() {
         return id;
     }
 
     /**
      * Finds or creates the GridFormat object describing the service.
-     * 
+     *
      * @return GridFormat describing this coverage.
      */
     public synchronized AbstractGridFormat getFormat() {
         if (this.format == null) {
-            this.format = (AbstractGridFormat) this.factory.createFormat();
+            this.format = this.factory.createFormat();
         }
         return this.format;
     }
@@ -108,28 +116,27 @@ public abstract class AbstractRasterService extends IService {
     /**
      * Finds or creates the Reader used to access this service. Apon any exception, the message
      * field is populated and null is returned.
-     * 
+     *
      * @return Reader linked to this service.
      */
-    public synchronized AbstractGridCoverage2DReader getReader(IProgressMonitor monitor) {
+    public synchronized GridCoverage2DReader getReader(IProgressMonitor monitor) {
         if (this.reader == null) {
             try {
-                AbstractGridFormat frmt = (AbstractGridFormat) getFormat();
+                AbstractGridFormat frmt = getFormat();
                 ID id = getID();
-                if( id.isFile() ){
+                if (id.isFile()) {
                     File file = id.toFile();
                     if (file != null) {
                         // to force crs
                         // Hints hints = new Hints();
                         // hints.put(Hints.DEFAULT_COORDINATE_REFERENCE_SYSTEM, )
-                        this.reader = (AbstractGridCoverage2DReader) frmt.getReader(file);
+                        this.reader = frmt.getReader(file);
                         return this.reader;
-	                }
-	                else {
-	                	throw new FileNotFoundException( id.toFile().toString() );
-	                }
+                    } else {
+                        throw new FileNotFoundException(id.toFile().toString());
+                    }
                 }
-                this.reader = (AbstractGridCoverage2DReader) frmt.getReader( id.toURL() );
+                this.reader = frmt.getReader(id.toURL());
             } catch (Exception ex) {
                 this.message = ex;
             }
@@ -140,23 +147,23 @@ public abstract class AbstractRasterService extends IService {
     /**
      * Retrieves the string identifying this service. This is the location of the resource to be
      * loaded.
-     * 
+     *
      * @return String describing this service.
      */
     public String getDescription() {
         return getIdentifier().toString();
     }
 
-    /** Retrieves a relatively human readable title for this service. */     
-    public String getHandle(){
-    	// we should check the Reader in order to reveal any internal content
-    	// or assume "raster" to agree with Symbology encoding specification
-    	// for now we will use the filename.    	
-    	return getID().toBaseFile();
+    /** Retrieves a relatively human readable title for this service. */
+    public String getHandle() {
+        // we should check the Reader in order to reveal any internal content
+        // or assume "raster" to agree with Symbology encoding specification
+        // for now we will use the filename.
+        return getID().toBaseFile();
     }
 
     @Override
-    public <T> T resolve( Class<T> adaptee, IProgressMonitor monitor ) throws IOException {
+    public <T> T resolve(Class<T> adaptee, IProgressMonitor monitor) throws IOException {
 
         if (monitor == null)
             monitor = new NullProgressMonitor();
@@ -171,12 +178,11 @@ public abstract class AbstractRasterService extends IService {
     }
 
     @Override
-    public abstract List<AbstractRasterGeoResource> resources( IProgressMonitor monitor )
+    public abstract List<AbstractRasterGeoResource> resources(IProgressMonitor monitor)
             throws IOException;
 
-
     @Override
-    public AbstractRasterServiceInfo getInfo( IProgressMonitor monitor ) throws IOException {
+    public AbstractRasterServiceInfo getInfo(IProgressMonitor monitor) throws IOException {
         return (AbstractRasterServiceInfo) super.getInfo(monitor);
     }
 }
--- plugins/org.locationtech.udig.catalog.rasterings/src/org/locationtech/udig/catalog/rasterings/AbstractRasterServiceInfo.java
+++ plugins/org.locationtech.udig.catalog.rasterings/src/org/locationtech/udig/catalog/rasterings/AbstractRasterServiceInfo.java
@@ -12,9 +12,11 @@
 
 package org.locationtech.udig.catalog.rasterings;
 
+import java.io.IOException;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
+import org.geotools.coverage.grid.io.GridCoverage2DReader;
 import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.referencing.crs.DefaultEngineeringCRS;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -25,24 +27,24 @@ import org.locationtech.udig.catalog.IServiceInfo;
 public class AbstractRasterServiceInfo extends IServiceInfo {
 
     private final AbstractRasterService service;
-    
+
     private static double UNCACHED = -1.0;
+
     private double completeness = UNCACHED;
 
-    public AbstractRasterServiceInfo( AbstractRasterService service, String... keywords ) {
+    public AbstractRasterServiceInfo(AbstractRasterService service, String... keywords) {
         this.service = service;
-        super.keywords = keywords; 
+        super.keywords = keywords;
     }
-    
+
     @Override
     public String getTitle() {
         ID id = service.getID();
-        
+
         String title;
-        if( id.isFile() ){
-            title = id.toFile().getAbsolutePath();            
-        }
-        else {
+        if (id.isFile()) {
+            title = id.toFile().getAbsolutePath();
+        } else {
             title = id.toString();
         }
         return title;
@@ -52,47 +54,53 @@ public class AbstractRasterServiceInfo extends IServiceInfo {
     public String getShortTitle() {
         return service.getID().toFile().getName();
     }
-    
+
     @Override
     public String getDescription() {
         return service.getIdentifier().toString();
     }
-    
+
     @Override
     public double getMetric() {
-        if (!(completeness == UNCACHED)){
+        if (!(completeness == UNCACHED)) {
             return completeness;
         }
-        
+
         final int numberOfexpectedInfoFields = 2;
         int numberOfAvailableInfoFields = 0;
 
-        AbstractGridCoverage2DReader reader = null;
+        GridCoverage2DReader reader = null;
 
         try {
             IProgressMonitor monitor = new NullProgressMonitor();
             monitor.setTaskName("Checking availability of metadata for " + service.getTitle());
-            
+
             reader = service.getReader(monitor);
 
             CoordinateReferenceSystem CRS = reader.getCoordinateReferenceSystem();
 
             if (!(CRS instanceof DefaultEngineeringCRS))
                 numberOfAvailableInfoFields++;
-            
+
             GeneralEnvelope env = reader.getOriginalEnvelope();
-            
+
             if (!(env.isEmpty() || env.isNull() || env.isInfinite()))
                 numberOfAvailableInfoFields++;
 
-            completeness = (double) numberOfexpectedInfoFields
-                    / (double) numberOfAvailableInfoFields;
+            completeness = (double) numberOfAvailableInfoFields
+                    / (double) numberOfexpectedInfoFields;
 
         } finally {
-            reader.dispose();//clean up render.
+            try {
+                if (reader != null) {
+                    reader.dispose(); // clean up render.
+                }
+            } catch (IOException e) {
+                RasteringsPlugin.log("error disposing GridCoverage2DReader", e);
+            }
         }
-        
+
         return completeness;
     }
-    
+
 }
