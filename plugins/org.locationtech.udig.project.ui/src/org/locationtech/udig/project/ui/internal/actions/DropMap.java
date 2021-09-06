78019492aac93a9df913e18a5c3da329d8ae02da
Frank Gasdorf <fgdrf@users.sourceforge.net>
2021-09-06 10:08:58 +0200

    [HK] DropAction checks MapPart instead of MapEditorPart
    to support ViewParts Implementations as well
    
    Change-Id: Ic01ff5aa08c903d543eb62cf7509b0690e1a8c83
    Signed-off-by: Frank Gasdorf <fgdrf@users.sourceforge.net>

--- plugins/org.locationtech.udig.project.ui/src/org/locationtech/udig/project/ui/internal/actions/DropMap.java
+++ plugins/org.locationtech.udig.project.ui/src/org/locationtech/udig/project/ui/internal/actions/DropMap.java
@@ -25,54 +25,56 @@ import org.locationtech.udig.project.IProjectElement;
 import org.locationtech.udig.project.internal.ProjectPlugin;
 import org.locationtech.udig.project.ui.ApplicationGIS;
 import org.locationtech.udig.project.ui.internal.LayersView;
-import org.locationtech.udig.project.ui.internal.MapEditorPart;
+import org.locationtech.udig.project.ui.internal.MapPart;
 import org.locationtech.udig.project.ui.internal.ProjectUIPlugin;
 import org.locationtech.udig.project.ui.render.displayAdapter.ViewportPane;
 import org.locationtech.udig.ui.IDropAction;
 
 /**
- * Opens a map when dropped on an IEditorPart
- * 
+ * Opens a map when dropped on an MapPart
+ *
  * @author jones
  * @since 1.1.0
  */
 public class DropMap extends IDropAction {
 
     @Override
-    public boolean accept( ) {
+    public boolean accept() {
 
         if (getDestination() instanceof ILayer) {
-        	if(getEvent()==null){
-        		return false;
-        	}
-            Control control = ((DropTarget)getEvent().widget).getControl();
-            if ( control != LayersView.getViewer().getControl() && !(control instanceof ViewportPane))
+            if (getEvent() == null) {
+                return false;
+            }
+            Control control = ((DropTarget) getEvent().widget).getControl();
+            if (control != LayersView.getViewer().getControl()
+                    && !(control instanceof ViewportPane))
                 return false;
         }
-        
+
         if (getData() instanceof IProjectElement) {
             return true;
         }
-       
+
         // we want to open the project element if one of it children is dragged
         if (getData() instanceof EObject && !(getDestination() instanceof ILayer)) {
             EObject eobj = (EObject) getData();
-            while (eobj!=null && !(eobj instanceof IProjectElement) ){
-                eobj=eobj.eContainer();
+            while (eobj != null && !(eobj instanceof IProjectElement)) {
+                eobj = eobj.eContainer();
             }
-            
+
             if (eobj == null)
                 return false;
-            
+
             // if layer dropped in layers view when map is open then we want use another action
-            if( getData() instanceof ILayer && getDestination() instanceof LayersView && ApplicationGIS.getActiveMap()!=ApplicationGIS.NO_MAP ){
+            if (getData() instanceof ILayer && getDestination() instanceof LayersView
+                    && ApplicationGIS.getActiveMap() != ApplicationGIS.NO_MAP) {
                 return false;
             }
             // layer droppe in map if special too
-            if( getData() instanceof ILayer && getDestination() instanceof MapEditorPart){
+            if (getData() instanceof ILayer && getDestination() instanceof MapPart) {
                 return false;
             }
-            
+
             return true;
         }
         URL url = null;
@@ -88,9 +90,9 @@ public class DropMap extends IDropAction {
             } catch (MalformedURLException e) {
                 // guess it is not a URL
             }
-            if( url == null ){
-                File file=new File(string);
-                if( !file.exists() ){
+            if (url == null) {
+                File file = new File(string);
+                if (!file.exists()) {
                     return false;
                 }
             }
@@ -117,17 +119,17 @@ public class DropMap extends IDropAction {
     }
 
     @Override
-    public void perform( IProgressMonitor monitor ) {
+    public void perform(IProgressMonitor monitor) {
         if (getData() instanceof EObject && !(getDestination() instanceof ILayer)) {
             EObject eobj = (EObject) getData();
-            while (eobj!=null && !(eobj instanceof IProjectElement) ){
-                eobj=eobj.eContainer();
+            while (eobj != null && !(eobj instanceof IProjectElement)) {
+                eobj = eobj.eContainer();
             }
-            ApplicationGIS.openProjectElement((IProjectElement) eobj,false);
+            ApplicationGIS.openProjectElement((IProjectElement) eobj, false);
         }
         URL url = null;
         if (getData() instanceof IProjectElement) {
-            ApplicationGIS.openProjectElement((IProjectElement) getData(),false);
+            ApplicationGIS.openProjectElement((IProjectElement) getData(), false);
         } else if (getData() instanceof String) {
             String string = (String) getData();
             try {
@@ -139,10 +141,10 @@ public class DropMap extends IDropAction {
                 url = new URL(string);
             } catch (MalformedURLException e) {
                 // guess it is not a URL
-                File file=new File(string);
-                if( file.exists() )
+                File file = new File(string);
+                if (file.exists())
                     try {
-                        url=file.toURL();
+                        url = file.toURL();
                     } catch (MalformedURLException e1) {
                         // oh well
                     }
@@ -158,7 +160,8 @@ public class DropMap extends IDropAction {
                 fileString = url.getFile();
                 File file = new File(fileString);
                 if (!file.exists() || !fileString.endsWith(".umap")) { //$NON-NLS-1$
-                    ProjectUIPlugin.log("Some how accept() accepted: " + getData(), new Exception()); //$NON-NLS-1$
+                    ProjectUIPlugin.log("Some how accept() accepted: " + getData(), //$NON-NLS-1$
+                            new Exception());
                     return;
                 }
             } catch (Exception e) {
@@ -170,8 +173,8 @@ public class DropMap extends IDropAction {
             IProjectElement elem;
             try {
                 elem = ApplicationGIS.loadProjectElement(url, ApplicationGIS.getActiveProject());
-                if ( elem instanceof IProjectElement )
-                    ApplicationGIS.openProjectElement((IProjectElement) elem,false);
+                if (elem instanceof IProjectElement)
+                    ApplicationGIS.openProjectElement(elem, false);
             } catch (IllegalArgumentException e) {
                 ProjectPlugin.log("", e); //$NON-NLS-1$
             } catch (IOException e) {
