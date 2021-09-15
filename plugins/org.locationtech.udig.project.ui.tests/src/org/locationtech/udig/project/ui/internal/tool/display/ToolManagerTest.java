e4cd4eb0cee7902b7a88cabcb7498e71ac42056d
Sebastian Schulz <bastie92_spam@gmx.de>
2021-09-15 15:11:48 +0200

    Update PR according to change-requests

--- plugins/org.locationtech.udig.project.ui.tests/src/org/locationtech/udig/project/ui/internal/tool/display/ToolManagerTest.java
+++ plugins/org.locationtech.udig.project.ui.tests/src/org/locationtech/udig/project/ui/internal/tool/display/ToolManagerTest.java
@@ -1,13 +1,13 @@
-/*
-*    uDig - User Friendly Desktop Internet GIS client
-*    http://udig.refractions.net
-*    (C) 2012, Refractions Research Inc.
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
-* License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
-*/
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2012, Refractions Research Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Refractions BSD
+ * License v1.0 (http://udig.refractions.net/files/bsd3-v10.html).
+ */
 package org.locationtech.udig.project.ui.internal.tool.display;
 
 import static org.junit.Assert.assertEquals;
