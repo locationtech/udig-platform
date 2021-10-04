c0ddac2736c26510d1c1e8442386cba741975064
Sebastian Schulz <bastie92_spam@gmx.de>
2021-10-04 10:41:24 +0200

    Remove unnessecary SuppressWarnings of type 'deprecated'

--- plugins/org.locationtech.udig.project/src/org/locationtech/udig/project/command/factory/NavigationCommandFactory.java
+++ plugins/org.locationtech.udig.project/src/org/locationtech/udig/project/command/factory/NavigationCommandFactory.java
@@ -1,4 +1,5 @@
-/* uDig - User Friendly Desktop Internet GIS client
+/**
+ * uDig - User Friendly Desktop Internet GIS client
  * http://udig.refractions.net
  * (C) 2004-2012, Refractions Research Inc.
  *
@@ -11,6 +12,8 @@ package org.locationtech.udig.project.command.factory;
 
 import java.util.Arrays;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Envelope;
 import org.locationtech.udig.project.command.NavCommand;
 import org.locationtech.udig.project.internal.command.navigation.NavComposite;
 import org.locationtech.udig.project.internal.command.navigation.PanCommand;
@@ -20,152 +23,161 @@ import org.locationtech.udig.project.internal.command.navigation.SetViewportHeig
 import org.locationtech.udig.project.internal.command.navigation.SetViewportWidth;
 import org.locationtech.udig.project.internal.command.navigation.ZoomCommand;
 import org.locationtech.udig.project.internal.command.navigation.ZoomExtentCommand;
-
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Envelope;
 
 /**
  * Factory providing default implementations of common navigation commands.
- * 
+ *
  * @author jeichar
  * @since 1.0
  * @deprecated Please use navigation commands directly
  */
-@SuppressWarnings("deprecation")
-public class NavigationCommandFactory /*extends org.locationtech.udig.project.command.NavigationCommandFactory */ {
+@Deprecated
+public class NavigationCommandFactory {
     /**
      * Creates a new NavigationCommandFactory object
-     * 
+     *
      * @return a new NavigationCommandFactory object
      */
     public static NavigationCommandFactory getInstance() {
         return instance;
     }
+
     private static final NavigationCommandFactory instance = new NavigationCommandFactory();
-    protected  NavigationCommandFactory() {
+
+    protected NavigationCommandFactory() {
         // no op
     }
 
     /**
      * Creates a new {@linkplain NavComposite}
-     * 
+     *
      * @param commands an array of commands to execute as a simgle command. The array will be
      *        executed from position 0 to position length-1 in order.
      * @return a new NavComposite object
      * @see NavCommand
      * @deprecated Please use new NavComposite(Arrays.asList(commands))
      */
-    public NavCommand createCompositeCommand( NavCommand[] commands ) {
+    @Deprecated
+    public NavCommand createCompositeCommand(NavCommand[] commands) {
         return new NavComposite(Arrays.asList(commands));
     }
 
     /**
      * Creates a new {@linkplain SetViewportBBoxCommand}
-     * 
+     *
      * @param newbbox the new bounding box to set in the viewport
      * @return a new SetViewportBBoxCommand object
      * @see NavCommand
      * @see Envelope
      * @deprecated Please use new SetViewportBBoxCommand( newbbox );
      */
-    public NavCommand createSetViewportBBoxCommand( Envelope newbbox ) {
+    @Deprecated
+    public NavCommand createSetViewportBBoxCommand(Envelope newbbox) {
         return new SetViewportBBoxCommand(newbbox);
     }
 
     /**
      * Creates a new {@linkplain ZoomCommand}
-     * 
+     *
      * @param zoomfactor the amount to zoom
      * @return a new ZoomCommand object
      * @see NavCommand
      * @deprecated Please use new ZoomCommand(zoomfactor)
      */
-    public NavCommand createZoomCommand( double zoomfactor ) {
+    @Deprecated
+    public NavCommand createZoomCommand(double zoomfactor) {
         return new ZoomCommand(zoomfactor);
     }
 
     /**
      * Creates a new {@linkplain ZoomExtentCommand}
-     * 
+     *
      * @return a new ZoomExtentCommand object
      * @see NavCommand
      * @deprecated Please use ZoomExtentCommand()
      */
+    @Deprecated
     public NavCommand createZoomExtentCommand() {
         return new ZoomExtentCommand();
     }
 
     /**
      * Creates a new {@linkplain SetViewportCenterCommand}
-     * 
+     *
      * @param center Sets the center of the viewport. The Coordinate must be in world coordinates.
      * @return a new SetViewportCenterCommand object
      * @see NavCommand
      * @see Coordinate
      * @deprecated Please use new SetViewportCenterCommand(center)
      */
-    public NavCommand createSetViewportCenterCommand( Coordinate center ) {
+    @Deprecated
+    public NavCommand createSetViewportCenterCommand(Coordinate center) {
         return new SetViewportCenterCommand(center);
     }
 
     /**
      * Creates a new {@linkplain SetViewportHeight}
-     * 
+     *
      * @param height The new viewport height
      * @return a new SetViewportHeight object
      * @see NavCommand
      * @deprecated Please use new SetViewportHeight(height)
      */
-    public NavCommand createSetViewportHeight( double height ) {
+    @Deprecated
+    public NavCommand createSetViewportHeight(double height) {
         return new SetViewportHeight(height);
     }
 
     /**
      * Creates a new {@linkplain SetViewportWidth}
-     * 
+     *
      * @param width the new viewport width
      * @return a new SetViewportWidth object
      * @see NavCommand
      * @deprecated Please use new SetViewportWidth(width)
      */
-    public NavCommand createSetViewportWidth( double width ) {
+    @Deprecated
+    public NavCommand createSetViewportWidth(double width) {
         return new SetViewportWidth(width);
     }
 
     /**
-     * Creates a new {@linkplain PanCommand}Pans the viewport in terms of pixels on the screen.
-     * Each pixel represents a distance in world coordinates, the x and y distances differ, so a pan
-     * of 8 pixels in the x direction will be translated to a pan of 8*xdistance in the world.
-     * 
+     * Creates a new {@linkplain PanCommand}Pans the viewport in terms of pixels on the screen. Each
+     * pixel represents a distance in world coordinates, the x and y distances differ, so a pan of 8
+     * pixels in the x direction will be translated to a pan of 8*xdistance in the world.
+     *
      * @param xpixels The amount, in pixels, to pan in the x direction
      * @param ypixels The amount, in pixels, to pan in the y direction
      * @return a new PanCommand object
      * @see NavCommand
      * @deprecated Please use new PanCommand(xpixels, ypixels)
      */
-    public NavCommand createPanCommandUsingScreenCoords( int xpixels, int ypixels ) {
+    @Deprecated
+    public NavCommand createPanCommandUsingScreenCoords(int xpixels, int ypixels) {
         return new PanCommand(xpixels, ypixels);
     }
 
     /**
      * Creates a new {@linkplain PanCommand}
-     * 
+     *
      * @param x The amount, in world coordinates, to pan in the x direction
      * @param y The amount, in world coordinates, to pan in the y direction
      * @return a new PanCommand object
      * @see NavCommand
      * @deprecated Please use new PanCommand(x, y)
      */
-    public NavCommand createPanCommandUsingWorldCoords( double x, double y ) {
+    @Deprecated
+    public NavCommand createPanCommandUsingWorldCoords(double x, double y) {
         return new PanCommand(x, y);
     }
+
     /**
-     * @deprecated Please use new SetViewportBBoxCommand(bounds, crs) 
+     * @deprecated Please use new SetViewportBBoxCommand(bounds, crs)
      */
-	public NavCommand createSetViewportBBoxCommand(Envelope bounds, CoordinateReferenceSystem crs) {
-		return new SetViewportBBoxCommand(bounds, crs);
-	}
+    @Deprecated
+    public NavCommand createSetViewportBBoxCommand(Envelope bounds, CoordinateReferenceSystem crs) {
+        return new SetViewportBBoxCommand(bounds, crs);
+    }
 
 }
