7ff3434fb289da043d0fe4982d25c99965f28e6f
Sebastian Schulz <bastie92_spam@gmx.de>
2021-10-06 10:03:45 +0200

    Apply code-formatter in module 'org.locationtech.udig.catalog'

--- plugins/org.locationtech.udig.catalog/src/org/locationtech/udig/catalog/moved/MovedService.java
+++ plugins/org.locationtech.udig.catalog/src/org/locationtech/udig/catalog/moved/MovedService.java
@@ -1,7 +1,7 @@
-/*
- *    uDig - User Friendly Desktop Internet GIS client
- *    http://udig.refractions.net
- *    (C) 2012, Refractions Research Inc.
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2012, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,59 +18,70 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.locationtech.udig.catalog.ID;
 import org.locationtech.udig.catalog.IForward;
 import org.locationtech.udig.catalog.IGeoResource;
 import org.locationtech.udig.catalog.IService;
 import org.locationtech.udig.catalog.IServiceInfo;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
 /**
  * This class represents a service that has been moved to a new location.
  * <p>
  * For details please see the IForward interface.
  * </p>
+ *
  * @author Jody Garnett
  */
-public class MovedService extends IService implements IForward  {
+public class MovedService extends IService implements IForward {
     ID identifier;
+
     ID forward;
 
-    public MovedService( ID id, ID forward ){
+    public MovedService(ID id, ID forward) {
         this.identifier = id;
         this.forward = forward;
     }
+
+    @Override
     public Map<String, Serializable> getConnectionParams() {
-        Map<String, Serializable> params = new HashMap<String, Serializable>();
-        params.put( MovedServiceExtention.ID_KEY, identifier );
-        params.put( MovedServiceExtention.FORWARD_KEY, forward );
+        Map<String, Serializable> params = new HashMap<>();
+        params.put(MovedServiceExtention.ID_KEY, identifier);
+        params.put(MovedServiceExtention.FORWARD_KEY, forward);
         return params;
     }
 
-    protected IServiceInfo createInfo( IProgressMonitor monitor ) throws IOException {
+    @Override
+    protected IServiceInfo createInfo(IProgressMonitor monitor) throws IOException {
         return null;
     }
 
     @Override
-    public List< ? extends IGeoResource> resources( IProgressMonitor monitor ) throws IOException {
+    public List<? extends IGeoResource> resources(IProgressMonitor monitor) throws IOException {
         return Collections.emptyList();
     }
-    
+
+    @Override
     public URL getIdentifier() {
         return identifier.toURL();
     }
+
+    @Override
     public ID getID() {
         return identifier;
     }
+
+    @Override
     public Throwable getMessage() {
-        return new IllegalStateException("Service has moved to "+forward );
+        return new IllegalStateException("Service has moved to " + forward); //$NON-NLS-1$
     }
 
+    @Override
     public Status getStatus() {
         return Status.BROKEN;
     }
 
+    @Override
     public ID getForward() {
         return forward;
     }
