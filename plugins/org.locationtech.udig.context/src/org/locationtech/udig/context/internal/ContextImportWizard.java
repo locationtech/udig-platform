6bdc1266439640fc21d9b1f07cf745c7256b3dae
Sebastian Schulz <bastie92_spam@gmx.de>
2021-11-02 09:28:18 +0100

    Use SubMonitor instead of SubProgressMonitor

--- plugins/org.locationtech.udig.context/src/org/locationtech/udig/context/internal/ContextImportWizard.java
+++ plugins/org.locationtech.udig.context/src/org/locationtech/udig/context/internal/ContextImportWizard.java
@@ -1,7 +1,7 @@
-/*
- *    uDig - User Friendly Desktop Internet GIS client
- *    http://udig.refractions.net
- *    (C) 2012, Refractions Research Inc.
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2012, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -24,6 +24,21 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+import org.geotools.ows.wms.WebMapServer;
+import org.geotools.referencing.CRS;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.locationtech.jts.geom.Envelope;
 import org.locationtech.udig.catalog.CatalogPlugin;
 import org.locationtech.udig.catalog.ICatalog;
 import org.locationtech.udig.catalog.IGeoResource;
@@ -40,28 +55,11 @@ import org.locationtech.udig.project.internal.ProjectPlugin;
 import org.locationtech.udig.project.internal.render.ViewportModel;
 import org.locationtech.udig.project.ui.ApplicationGIS;
 import org.locationtech.udig.ui.ProgressManager;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.IImportWizard;
-import org.eclipse.ui.IWorkbench;
-import org.geotools.ows.wms.WebMapServer;
-import org.geotools.referencing.CRS;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.Namespace;
-import org.jdom2.input.SAXBuilder;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-
-import org.locationtech.jts.geom.Envelope;
 
 /**
  * Import Context document as a new Map.
- * 
+ *
  * @author Jody Garnett
  * @since 1.0.0
  */
@@ -69,7 +67,9 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
     URLWizardPage page;
 
     static Namespace oc = Namespace.getNamespace("http://www.opengis.net/oc"); //$NON-NLS-1$
+
     static Namespace sld = Namespace.getNamespace("http://www.opengis.net/sld"); //$NON-NLS-1$
+
     static Namespace xlink = Namespace.getNamespace("http://www.w3.org/1999/xlink"); //$NON-NLS-1$
 
     public ContextImportWizard() {
@@ -83,9 +83,10 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         if (url == null)
             return false;
 
-        IRunnableWithProgress process = new IRunnableWithProgress(){
-            public void run( IProgressMonitor monitor ) throws InvocationTargetException,
-                    InterruptedException {
+        IRunnableWithProgress process = new IRunnableWithProgress() {
+            @Override
+            public void run(IProgressMonitor monitor)
+                    throws InvocationTargetException, InterruptedException {
                 try {
                     process(url, monitor);
                 } catch (Exception erp) {
@@ -106,24 +107,26 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         return true;
 
     }
+
     /**
      * Process a Context Document, a new IMap will be created if successful.
-     * 
+     *
      * @param context
      * @param monitor
      */
-    public static void process( final URL url, IProgressMonitor monitor ) throws Exception {
+    public static void process(final URL url, IProgressMonitor monitor) throws Exception {
         if (monitor == null)
             monitor = new NullProgressMonitor();
         try {
-            monitor.beginTask(Messages.ContextImportWizard_task_title, 100); 
-            monitor.subTask(MessageFormat.format(Messages.ContextImportWizard_task_connecting, url));
+            monitor.beginTask(Messages.ContextImportWizard_task_title, 100);
+            monitor.subTask(
+                    MessageFormat.format(Messages.ContextImportWizard_task_connecting, url));
             monitor.worked(5);
             InputStream input = null;
             try {
                 input = url.openStream();
                 monitor.worked(15);
-                SubProgressMonitor subMonitor = new SubProgressMonitor(monitor, 80);
+                SubMonitor subMonitor = SubMonitor.convert(monitor, 80);
                 try {
                     process(input, subMonitor);
                 } finally {
@@ -153,25 +156,25 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
      * presented as an error.
      * </p>
      */
-    static protected void process( InputStream input, IProgressMonitor monitor ) throws Exception {
+    static protected void process(InputStream input, IProgressMonitor monitor) throws Exception {
         if (monitor == null)
             monitor = new NullProgressMonitor();
 
-        monitor.beginTask(Messages.ContextImportWizard_parsingxml, 100); 
+        monitor.beginTask(Messages.ContextImportWizard_parsingxml, 100);
         monitor.worked(5);
         BufferedInputStream stream = new BufferedInputStream(input);
         Document document = new SAXBuilder().build(stream);
         monitor.worked(55);
-        process(document, new SubProgressMonitor(monitor, 40));
+        process(document, SubMonitor.convert(monitor, 40));
     }
 
     /**
      * Parse XML document using JDOM.
-     * 
+     *
      * @param document
      * @param monitor
      */
-    static public void process( Document document, IProgressMonitor monitor ) {
+    static public void process(Document document, IProgressMonitor monitor) {
         if (monitor == null)
             monitor = new NullProgressMonitor();
         Element root = document.getRootElement();
@@ -181,9 +184,8 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         Element resourceList = root.getChild("ResourceList", oc); //$NON-NLS-1$
 
         String title = general.getChildText("Title", oc); //$NON-NLS-1$
-        monitor
-                .beginTask(MessageFormat.format(
-                        Messages.ContextImportWizard_importing, title), resourceList.getContentSize() + 2);
+        monitor.beginTask(MessageFormat.format(Messages.ContextImportWizard_importing, title),
+                resourceList.getContentSize() + 2);
 
         // TODO: This is internal, why so hard? Commands? Thought I could make
         // a Map and then use a command to hand it over to uDig and rendering.
@@ -194,7 +196,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         processBoundingBox(general.getChild("BoundingBox", ows), map); //$NON-NLS-1$
         monitor.worked(1);
 
-        for( Iterator iter = resourceList.getContent().iterator(); iter.hasNext(); ) {
+        for (Iterator iter = resourceList.getContent().iterator(); iter.hasNext();) {
             Object child = iter.next();
             if (!(child instanceof Element))
                 continue;
@@ -222,7 +224,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
      * @param general
      * @param map
      */
-    static private void processBoundingBox( Element boundingBox, Map map ) {
+    static private void processBoundingBox(Element boundingBox, Map map) {
         Namespace ows = Namespace.getNamespace("http://www.opengis.net/ows"); //$NON-NLS-1$
 
         CoordinateReferenceSystem crs = null;
@@ -239,9 +241,9 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
             String lowerCorner[] = boundingBox.getChildTextTrim("LowerCorner", ows).split(" "); //$NON-NLS-1$ //$NON-NLS-2$
             String upperCorner[] = boundingBox.getChildTextTrim("UpperCorner", ows).split(" "); //$NON-NLS-1$ //$NON-NLS-2$
 
-            env = new Envelope(Double.parseDouble(lowerCorner[0]), Double
-                    .parseDouble(upperCorner[0]), Double.parseDouble(lowerCorner[1]), Double
-                    .parseDouble(upperCorner[1]));
+            env = new Envelope(Double.parseDouble(lowerCorner[0]),
+                    Double.parseDouble(upperCorner[0]), Double.parseDouble(lowerCorner[1]),
+                    Double.parseDouble(upperCorner[1]));
         } catch (Exception noBBox) {
             System.out.println("No BoundingBox" + boundingBox); //$NON-NLS-1$
         }
@@ -257,15 +259,15 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
      * <p>
      * I would really rather the renderers dragged things into the catalog as the need them...
      * </p>
-     * 
+     *
      * @param url URL used to start searching in the catalog
      * @param type The returned service must resolve to this type
      * @return URL of resulting service
      */
-    static final private URL service( URL url, Class<?> type ) {
+    static final private URL service(URL url, Class<?> type) {
         ICatalog local = CatalogPlugin.getDefault().getLocalCatalog();
         List<IResolve> services = local.find(url, ProgressManager.instance().get());
-        for( IResolve service : services ) {
+        for (IResolve service : services) {
             if (service.canResolve(type)) {
                 return service.getIdentifier();
             }
@@ -273,7 +275,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         IServiceFactory serviceFactory = CatalogPlugin.getDefault().getServiceFactory();
         List<IService> candidates = serviceFactory.createService(url);
         try {
-            for( Iterator<IService> i=candidates.iterator(); i.hasNext();){
+            for (Iterator<IService> i = candidates.iterator(); i.hasNext();) {
                 IService service = i.next();
                 if (service.canResolve(type)) {
                     IService registered = local.add(service);
@@ -281,20 +283,20 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
                     return registered.getIdentifier();
                 }
             }
-        }
-        finally {
-            serviceFactory.dispose( candidates, null );
+        } finally {
+            serviceFactory.dispose(candidates, null);
         }
         // It is okay if we don't find anything, the user interface
         // will let the user know - tempting to force a broken WMS
         // handle onto the catalog though
         return url;
     }
+
     /**
      * @param resource
      * @param map
      */
-    static private void processFeatureType( Element resource, Map map ) {
+    static private void processFeatureType(Element resource, Map map) {
         Layer layer;
         String hidden = resource.getAttributeValue("hidden"); //$NON-NLS-1$
         String name = resource.getChildTextTrim("Name", oc); //$NON-NLS-1$
@@ -319,8 +321,8 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         }
         String type = onlineResouce.getAttributeValue("type", xlink); // eg simple //$NON-NLS-1$
         try {
-            URL url = new URL(href
-                    + "?SERVICE=WFS&VERSION=" + serverversion + "&REQUEST=GetCapabilities"); //$NON-NLS-1$ //$NON-NLS-2$
+            URL url = new URL(
+                    href + "?SERVICE=WFS&VERSION=" + serverversion + "&REQUEST=GetCapabilities"); //$NON-NLS-1$ //$NON-NLS-2$
             IGeoResource georesource = findResource(url, name);
             if (georesource == null) {
                 layer = ProjectFactory.eINSTANCE.createLayer();
@@ -349,16 +351,17 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         map.getLayersInternal().add(layer);
     }
 
-    private static IGeoResource findResource( URL url, String name ) throws IOException {
+    private static IGeoResource findResource(URL url, String name) throws IOException {
         URL appended = new URL(url.toString() + "#" + name); //$NON-NLS-1$
         ICatalog localCatalog = CatalogPlugin.getDefault().getLocalCatalog();
         List<IResolve> results = localCatalog.find(appended, null);
         if (results.isEmpty()) {
-            List<IService> services = CatalogPlugin.getDefault().getServiceFactory().createService(url);
-            for( IService service : services ) {
+            List<IService> services = CatalogPlugin.getDefault().getServiceFactory()
+                    .createService(url);
+            for (IService service : services) {
                 localCatalog.add(service);
-                for( IGeoResource resource : service.resources(null) ) {
-                    if( resource.getIdentifier().getRef().equals(name) ){
+                for (IGeoResource resource : service.resources(null)) {
+                    if (resource.getIdentifier().getRef().equals(name)) {
                         return resource;
                     }
                 }
@@ -372,7 +375,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
      * @param resource
      * @param map
      */
-    static private void processLayer( Element resource, Map map ) {
+    static private void processLayer(Element resource, Map map) {
         Layer layer = ProjectFactory.eINSTANCE.createLayer();
         String hidden = resource.getAttributeValue("hidden"); //$NON-NLS-1$
         if (hidden != null) {
@@ -380,7 +383,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         }
         String queryable = resource.getAttributeValue("queryable"); //$NON-NLS-1$
         if (queryable != null) {
-            layer.setInteraction(Interaction.SELECT, !"0".equals(queryable)); //$NON-NLS-1$ //$NON-NLS-2$
+            layer.setInteraction(Interaction.SELECT, !"0".equals(queryable)); //$NON-NLS-1$
         }
 
         Namespace oc = Namespace.getNamespace("http://www.opengis.net/oc"); //$NON-NLS-1$
@@ -397,13 +400,13 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
             Element server = resource.getChild("Server", oc); //$NON-NLS-1$
 
             String service = server.getAttributeValue("service"); // should be OGC:WMS //$NON-NLS-1$
-                                                                    // //$NON-NLS-1$
+                                                                  // //$NON-NLS-1$
             String title = server.getAttributeValue("title"); //$NON-NLS-1$
             String version = server.getAttributeValue("version"); // eg 1.1.1 //$NON-NLS-1$
 
             Element onlineResouce = server.getChild("OnlineResource", oc); //$NON-NLS-1$
             String type = onlineResouce.getAttributeValue("type", xlink); // eg simple //$NON-NLS-1$
-                                                                            // //$NON-NLS-1$
+                                                                          // //$NON-NLS-1$
             String href = onlineResouce.getAttributeValue("href", xlink); // url //$NON-NLS-1$
             try {
                 href = URLDecoder.decode(href, "US-ASCII"); //$NON-NLS-1$
@@ -413,8 +416,8 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
             }
 
             try {
-                URL url = new URL(href
-                        + "?SERVICE=WMS&VERSION=" + version + "&REQUEST=GetCapabilities"); //$NON-NLS-1$ //$NON-NLS-2$
+                URL url = new URL(
+                        href + "?SERVICE=WMS&VERSION=" + version + "&REQUEST=GetCapabilities"); //$NON-NLS-1$ //$NON-NLS-2$
                 url = service(url, WebMapServer.class);
 
                 layer.setID(new URL(url + "#" + name)); //$NON-NLS-1$
@@ -445,14 +448,14 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
      * TODO: Move to a formal MapBuilder utility.
      * <p>
      * Intended workflow, MapBuilder looks a lot like a Map, calling create will issue the command
-     * to create the map based on the collected parameters. Builders are paramertized by a factory -
+     * to create the map based on the collected parameters. Builders are parameterized by a factory -
      * in this case a ProjectFactory.
      * </p>
-     * 
+     *
      * @param title Title of Map to be created
      * @return new IMap
      */
-    static private Map createMap( String title ) {
+    static private Map createMap(String title) {
         Project project = ProjectPlugin.getPlugin().getProjectRegistry().getCurrentProject();
         if (project == null) {
             project = ProjectPlugin.getPlugin().getProjectRegistry().getDefaultProject();
@@ -460,18 +463,22 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
         return ProjectFactory.eINSTANCE.createMap(project, title, new ArrayList<Layer>());
     }
 
-    public void init( IWorkbench workbench, IStructuredSelection selection ) {
+    @Override
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
         // grab dialog settings for context ...
-        // selction can be used to figure out project ...
+        // selection can be used to figure out project ...
     }
+
     @Override
     public void addPages() {
         super.addPages();
-        page = new URLWizardPage(
-                Messages.ContextImportWizard_wizard_name, Messages.ContextImportWizard_wizard_title, ContextPlugin.getImageDescriptor("icons/wizban/import_owscontext_wiz_gif")){    //$NON-NLS-1$
+        page = new URLWizardPage(Messages.ContextImportWizard_wizard_name,
+                Messages.ContextImportWizard_wizard_title,
+                ContextPlugin.getImageDescriptor("icons/wizban/import_owscontext_wiz_gif")) { //$NON-NLS-1$
 
-            /** Override to check extention */
-            protected boolean fileCheck( File file ) {
+            /** Override to check extension */
+            @Override
+            protected boolean fileCheck(File file) {
                 if (!super.fileCheck(file))
                     return false;
 
@@ -482,7 +489,7 @@ public class ContextImportWizard extends Wizard implements IImportWizard {
                 // if( file.getName().toUpperCase().endsWith(".XML") ){ //$NON-NLS-1$
                 // return true;
                 // }
-                // setErrorMessage("File required to have extention XML" );
+                // setErrorMessage("File required to have extension XML" );
                 // return false;
                 return true;
             }
