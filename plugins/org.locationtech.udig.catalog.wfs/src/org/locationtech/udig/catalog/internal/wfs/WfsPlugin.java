5633630497886ca96a9af1294acb29dc9c489456
Sebastian Schulz <bastie92_spam@gmx.de>
2022-01-04 15:02:19 +0100

    Remove unused imports from several Java classes

--- plugins/org.locationtech.udig.catalog.wfs/src/org/locationtech/udig/catalog/internal/wfs/WfsPlugin.java
+++ plugins/org.locationtech.udig.catalog.wfs/src/org/locationtech/udig/catalog/internal/wfs/WfsPlugin.java
@@ -1,7 +1,7 @@
-/*
- *    uDig - User Friendly Desktop Internet GIS client
- *    http://udig.refractions.net
- *    (C) 2012, Refractions Research Inc.
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2012, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,158 +12,142 @@ package org.locationtech.udig.catalog.internal.wfs;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.geotools.data.wfs.WFSDataStore;
-import org.geotools.xml.gml.GMLSchema;
 import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
  */
 public class WfsPlugin extends AbstractUIPlugin {
-	//The shared instance.
-	private static WfsPlugin plugin;
-	//Resource bundle.
-	private ResourceBundle resourceBundle;
-	public static final String ID = "org.locationtech.udig.catalog.internal.wfs"; //$NON-NLS-1$
-	
-	/**
-	 * The constructor.
-	 */
-	public WfsPlugin() {
-		super();
-		plugin = this;
-	}
+    // The shared instance.
+    private static WfsPlugin plugin;
 
-	/**
-     * This method is called upon plug-in activation
+    // Resource bundle.
+    private ResourceBundle resourceBundle;
+
+    public static final String ID = "org.locationtech.udig.catalog.internal.wfs"; //$NON-NLS-1$
+
+    /**
+     * The constructor.
      */
-    public void start( BundleContext context ) throws Exception {
-        super.start(context);
-        /*
-        ClassLoader current = Thread.currentThread().getContextClassLoader();
-        try {
-            Thread.currentThread().setContextClassLoader(WFSDataStore.class.getClassLoader());
-            setLoggerLevel("org.geotools.data.wfs", "org.locationtech.udig.catalog.wfs/debug/wfs"); //$NON-NLS-1$ //$NON-NLS-2$
-            setLoggerLevel("org.geotools.xml", "org.locationtech.udig.catalog.wfs/debug/wfs"); //$NON-NLS-1$ //$NON-NLS-2$
-            setLoggerLevel("org.geotools.xml.sax", "org.locationtech.udig.catalog.wfs/debug/wfs"); //$NON-NLS-1$ //$NON-NLS-2$
-            if (isDebugging("org.locationtech.udig.catalog.wfs/debug/gml")) //$NON-NLS-1$
-                GMLSchema.setLogLevel(Level.FINE);
-            else
-                GMLSchema.setLogLevel(Level.SEVERE);
-        } finally {
-            Thread.currentThread().setContextClassLoader(current);
-        }
-        */
+    public WfsPlugin() {
+        super();
+        plugin = this;
     }
 
-    /*
-    private void setLoggerLevel( String loggerID, String traceID ) {
-        Logger logger = Logger.getLogger(loggerID);
-        if( isDebugging(traceID) ) 
-            logger.setLevel(Level.FINE);
-        else
-            logger.setLevel(Level.SEVERE);
-        ConsoleHandler consoleHandler = new ConsoleHandler();
-        consoleHandler.setLevel(Level.ALL);
-        logger.addHandler(consoleHandler);
-    }*/
+    /**
+     * This method is called upon plug-in activation
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		resourceBundle = null;
-		super.stop(context);
-	}
+    /**
+     * This method is called when the plug-in is stopped
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        resourceBundle = null;
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static WfsPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance.
+     */
+    public static WfsPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		ResourceBundle bundle = WfsPlugin.getDefault().getResourceBundle();
-		try {
-			return (bundle != null) ? bundle.getString(key) : key;
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
+    /**
+     * Returns the string from the plugin's resource bundle, or 'key' if not found.
+     */
+    public static String getResourceString(String key) {
+        ResourceBundle bundle = WfsPlugin.getDefault().getResourceBundle();
+        try {
+            return (bundle != null) ? bundle.getString(key) : key;
+        } catch (MissingResourceException e) {
+            return key;
+        }
+    }
 
-	/**
-	 * Returns the plugin's resource bundle,
-	 */
-	public ResourceBundle getResourceBundle() {
-		try {
-			if (resourceBundle == null)
-				resourceBundle = ResourceBundle.getBundle("org.locationtech.udig.catalog.internal.wfs.WfsPluginResources"); //$NON-NLS-1$
-		} catch (MissingResourceException x) {
-			resourceBundle = null;
-		}
-		return resourceBundle;
-	}
+    /**
+     * Returns the plugin's resource bundle,
+     */
+    public ResourceBundle getResourceBundle() {
+        try {
+            if (resourceBundle == null)
+                resourceBundle = ResourceBundle
+                        .getBundle("org.locationtech.udig.catalog.internal.wfs.WfsPluginResources"); //$NON-NLS-1$
+        } catch (MissingResourceException x) {
+            resourceBundle = null;
+        }
+        return resourceBundle;
+    }
+
     /**
      * Logs the Throwable in the plugin's log.
      * <p>
-     * This will be a user visable ERROR iff:
+     * This will be a user visible ERROR iff:
      * <ul>
-     * <li>t is an Exception we are assuming it is human readable or if a message is provided
+     * <li>t is an Exception we are assuming it is human readable or if a message is provided</li>
      * </ul>
      * </p>
-     * @param message 
-     * @param t 
+     *
+     * @param message
+     * @param t
      */
-    public static void log( String message, Throwable t ) {
+    public static void log(String message, Throwable t) {
         int status = t instanceof Exception || message != null ? IStatus.ERROR : IStatus.WARNING;
         getDefault().getLog().log(new Status(status, ID, IStatus.OK, message, t));
     }
+
     /**
      * Messages that only engage if getDefault().isDebugging()
      * <p>
-     * It is much prefered to do this:<pre><code>
+     * It is much preferred to do this:
+     *
+     * <pre>
+     * <code>
      * private static final String RENDERING = "org.locationtech.udig.project/render/trace";
-     * if( ProjectUIPlugin.getDefault().isDebugging() && "true".equalsIgnoreCase( RENDERING ) ){
-     *      System.out.println( "your message here" );
+     * if (ProjectUIPlugin.getDefault().isDebugging() && "true".equalsIgnoreCase(RENDERING)){
+     *      System.out.println("your message here");
      * }
-     * </code></pre>
+     * </code>
+     * </pre>
      * </p>
-     * @param message 
-     * @param e 
+     *
+     * @param message
+     * @param e
      */
-    public static void trace( String message, Throwable e) {
-        if( getDefault().isDebugging() ) {
-            if( message != null ) System.out.println( message );
-            if( e != null ) e.printStackTrace();
+    public static void trace(String message, Throwable e) {
+        if (getDefault().isDebugging()) {
+            if (message != null)
+                System.out.println(message);
+            if (e != null)
+                e.printStackTrace();
         }
     }
+
     /**
      * Performs the Platform.getDebugOption true check on the provided trace
      * <p>
      * Note: ProjectUIPlugin.getDefault().isDebugging() must also be on.
      * <ul>
-     * <li>Trace.RENDER - trace rendering progress
+     * <li>Trace.RENDER - trace rendering progress</li>
      * </ul>
-     * </p> 
+     * </p>
+     *
      * @param trace currently only RENDER is defined
-     * @return true if -debug is on for this plugin 
+     * @return true if -debug is on for this plugin
      */
-    public static boolean isDebugging( final String trace ){
-        return getDefault().isDebugging() &&
-            "true".equalsIgnoreCase(Platform.getDebugOption(trace)); //$NON-NLS-1$    
-    }    
+    public static boolean isDebugging(final String trace) {
+        return getDefault().isDebugging()
+                && "true".equalsIgnoreCase(Platform.getDebugOption(trace)); //$NON-NLS-1$
+    }
 }
