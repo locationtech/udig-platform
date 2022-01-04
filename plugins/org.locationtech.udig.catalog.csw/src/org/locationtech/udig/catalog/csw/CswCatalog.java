4a67f4cc3beb9b91d3f39ba6a1f044642c982013
Sebastian <89982771+sschulz92@users.noreply.github.com>
2022-01-04 15:33:05 +0100

    Remove unused imports from several Java classes (#661)

--- plugins/org.locationtech.udig.catalog.csw/src/org/locationtech/udig/catalog/csw/CswCatalog.java
+++ plugins/org.locationtech.udig.catalog.csw/src/org/locationtech/udig/catalog/csw/CswCatalog.java
@@ -1,7 +1,7 @@
-/*
- *    uDig - User Friendly Desktop Internet GIS client
- *    http://udig.refractions.net
- *    (C) 2011, Refractions Research Inc.
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2011, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,116 +12,85 @@ package org.locationtech.udig.catalog.csw;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.locationtech.jts.geom.Envelope;
 import org.locationtech.udig.catalog.ICatalogInfo;
 import org.locationtech.udig.catalog.ID;
 import org.locationtech.udig.catalog.IResolve;
 import org.locationtech.udig.catalog.ISearch;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
-import org.locationtech.jts.geom.Envelope;
-
 public class CswCatalog extends ISearch {
 
-	private URL url;
-	private Throwable error = null;
-	
-	public CswCatalog(URL url) {
-		this.url = url;
-	}
+    private URL url;
 
-	@Override
-	public <T> boolean canResolve(Class<T> adaptee) {
-		return adaptee!=null && (adaptee.isAssignableFrom(ICatalogInfo.class) || adaptee.isAssignableFrom(List.class));
-	}
+    private Throwable error = null;
 
-	@Override
-	public Status getStatus() {
-		return error == null?Status.CONNECTED:Status.BROKEN;
-	}
+    public CswCatalog(URL url) {
+        this.url = url;
+    }
 
-	@Override
-	public Throwable getMessage() {
-		return error;
-	}
+    @Override
+    public <T> boolean canResolve(Class<T> adaptee) {
+        return adaptee != null && (adaptee.isAssignableFrom(ICatalogInfo.class)
+                || adaptee.isAssignableFrom(List.class));
+    }
 
-	@Override
-	public URL getIdentifier() {
-		return url;
-	}
+    @Override
+    public Status getStatus() {
+        return error == null ? Status.CONNECTED : Status.BROKEN;
+    }
 
-	@Override
-	public ID getID() {
-		return new ID(url);
-	}
+    @Override
+    public Throwable getMessage() {
+        return error;
+    }
 
-	@Override
-	public String getTitle() {
-		return url.getHost();
-	}
+    @Override
+    public URL getIdentifier() {
+        return url;
+    }
 
-	@Override
-	public <T> T resolve(Class<T> adaptee, IProgressMonitor monitor) throws IOException {
-        if(adaptee == null)
+    @Override
+    public ID getID() {
+        return new ID(url);
+    }
+
+    @Override
+    public String getTitle() {
+        return url.getHost();
+    }
+
+    @Override
+    public <T> T resolve(Class<T> adaptee, IProgressMonitor monitor) throws IOException {
+        if (adaptee == null)
             return null;
-        if(adaptee.isAssignableFrom(ICatalogInfo.class)){
+        if (adaptee.isAssignableFrom(ICatalogInfo.class)) {
             return adaptee.cast(getInfo(monitor));
         }
-        if(adaptee.isAssignableFrom(List.class)){
+        if (adaptee.isAssignableFrom(List.class)) {
             return adaptee.cast(members(monitor));
         }
         return null;
-	}
+    }
 
-	@Override
-	public List<IResolve> find(ID resourceId, IProgressMonitor monitor) {
-		return new LinkedList<IResolve>();
-	}
+    @Override
+    public List<IResolve> find(ID resourceId, IProgressMonitor monitor) {
+        return new LinkedList<>();
+    }
 
-	@Override
-	public <T extends IResolve> T getById(Class<T> type, ID id,
-			IProgressMonitor monitor) {
-		return null;
-	}
+    @Override
+    public <T extends IResolve> T getById(Class<T> type, ID id, IProgressMonitor monitor) {
+        return null;
+    }
 
-	@Override
-	public List<IResolve> search(String pattern, Envelope bbox,
-			IProgressMonitor monitor) throws IOException {
-	    return Collections.emptyList();
-	    /*
-		OgcFilter filter = new PropertyIsEqualTo("Type", "dataset");
-		PropertyIsEqualTo textSearch = new PropertyIsEqualTo("AnyText", pattern);
-		if(!pattern.trim().isEmpty()) {
-			filter = new And(filter, textSearch);
-		}
-		if(bbox != null) {
-			BBoxFilter bboxFilter = new BBoxFilter(bbox.getMinX(), bbox.getMinY(), bbox.getMaxX(), bbox.getMaxY());
-			filter = new And(filter, bboxFilter);
-		}
-		int maxRecords = 10;
-		GetRecordsParams params = new GetRecordsParams(filter,maxRecords);
-		GetDublinCoreRecords request = new GetDublinCoreRecords(params);
-		Context context = new Context(url.toExternalForm());
-		List<DublinCore> results = request.execute(context).java();
-		List<IResolve> resolves = new ArrayList<IResolve>();
-		for (DublinCore dublinCore : results) {
-			if(dublinCore.type() == dublinCore.WMS_TYPE() && !dublinCore.onlineResource().isEmpty()) {
-				URL url = new URL(dublinCore.onlineResource().get());
-				OGCLayer layer = new OGCLayer(dublinCore.title(), dublinCore.title(), 
-						dublinCore.description(), 
-						url, 
-						dublinCore.type(), 
-						url);
-				resolves.add(CswResource.getResource(layer));
-			}
-		}
-		return resolves;
-		*/
-	}
+    @Override
+    public List<IResolve> search(String pattern, Envelope bbox, IProgressMonitor monitor)
+            throws IOException {
+        return Collections.emptyList();
+    }
 
 }
