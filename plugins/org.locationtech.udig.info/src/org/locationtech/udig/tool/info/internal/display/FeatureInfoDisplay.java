734cb617c4626ca0cc8d652890e07f9c7379d80b
Sebastian <89982771+sschulz92@users.noreply.github.com>
2022-01-10 07:41:52 +0100

    Remove duplicated if-condition in 'FeatureInfoDisplay' (#670)

--- plugins/org.locationtech.udig.info/src/org/locationtech/udig/tool/info/internal/display/FeatureInfoDisplay.java
+++ plugins/org.locationtech.udig.info/src/org/locationtech/udig/tool/info/internal/display/FeatureInfoDisplay.java
@@ -1,7 +1,7 @@
-/*
- *    uDig - User Friendly Desktop Internet GIS client
- *    http://udig.refractions.net
- *    (C) 2004, Refractions Research Inc.
+/**
+ * uDig - User Friendly Desktop Internet GIS client
+ * http://udig.refractions.net
+ * (C) 2004, Refractions Research Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,72 +13,67 @@ package org.locationtech.udig.tool.info.internal.display;
 
 import java.io.IOException;
 
-import org.locationtech.udig.project.ui.controls.FeatureTableControl;
-import org.locationtech.udig.tool.info.InfoDisplay;
-import org.locationtech.udig.tool.info.LayerPointInfo;
-
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.geotools.data.DataUtilities;
 import org.geotools.data.FeatureReader;
 import org.geotools.feature.FeatureCollection;
+import org.locationtech.udig.project.ui.controls.FeatureTableControl;
+import org.locationtech.udig.tool.info.InfoDisplay;
+import org.locationtech.udig.tool.info.LayerPointInfo;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
 /**
  * Nested browser used to display LayerPointInfo.
- * 
+ *
  * @author Jody Garnett
  * @since 0.3
  */
 public class FeatureInfoDisplay extends InfoDisplay {
-    
-    /** <code>content</code> field */
+
     protected FeatureTableControl content;
-    
+
+    @Override
     public Control getControl() {
         return content.getControl();
     }
-    
-    public void createDisplay( Composite parent ) {        
-        // content = new Text( parent, SWT.DEFAULT );
+
+    @Override
+    public void createDisplay(Composite parent) {
         content = new FeatureTableControl();
-        content.createTableControl( parent );
+        content.createTableControl(parent);
     }
-    
+
     /**
      * Focus the browser onto LayerPointInfo.getRequestURL.
-     * 
+     *
      * @see org.locationtech.udig.tool.info.InfoDisplay#setInfo(org.locationtech.udig.project.render.LayerPointInfo)
      * @param info
      */
-    public void setInfo( LayerPointInfo info ) {
-        if( info == null ) {            
-            content.clear();            
-        }
-        else {            
+    @Override
+    public void setInfo(LayerPointInfo info) {
+        if (info == null) {
+            content.clear();
+        } else {
             try {
                 Object value = info.acquireValue();
-                if( value == null ){
+                if (value == null) {
                     // make empty ?
-                    content.clear();                    
+                    content.clear();
                 }
-                if( value instanceof FeatureCollection ){
-                    content.setFeatures( (FeatureCollection<SimpleFeatureType, SimpleFeature>) value );
-                }
-                else if( value instanceof FeatureReader){
+                if (value instanceof FeatureCollection) {
+                    content.setFeatures(
+                            (FeatureCollection<SimpleFeatureType, SimpleFeature>) value);
+                } else if (value instanceof FeatureReader) {
                     FeatureReader<SimpleFeatureType, SimpleFeature> reader = (FeatureReader<SimpleFeatureType, SimpleFeature>) value;
-                    content.setFeatures( DataUtilities.collection(reader) );                    
-                }
-                else if (value instanceof FeatureCollection ){
-                    content.setFeatures( (FeatureCollection<SimpleFeatureType, SimpleFeature>) value );
-                }
-                else {
-                    content.clear();                    
+                    content.setFeatures(DataUtilities.collection(reader));
+                } else {
+                    content.clear();
                 }
             } catch (IOException e) {
                 content.clear();
             }
         }
-    }        
+    }
 }
